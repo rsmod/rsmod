dcda4351902bce6d419b189f4ed61d43e9aa40ea
= <admin@indovaps.com>
2020-09-09 00:19:28 -0400

    Correct disabled RSA equality check

--- game/src/main/kotlin/gg/rsmod/game/config/RsaConfig.kt
+++ game/src/main/kotlin/gg/rsmod/game/config/RsaConfig.kt
@@ -7,7 +7,7 @@ data class RsaConfig(
     val modulus: BigInteger
 ) {
     val isEnabled: Boolean
-        get() = this != DISABLED_RSA
+        get() = this !== DISABLED_RSA
 
     companion object {
         val DISABLED_RSA = RsaConfig(BigInteger.ZERO, BigInteger.ZERO)
