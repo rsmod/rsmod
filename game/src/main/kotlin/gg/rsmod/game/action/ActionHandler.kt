078caeae31ee292226b0421fe54f11c8e06d4ec9
= <admin@indovaps.com>
2020-09-10 08:18:22 -0400

    Move ActionHandlerMap to ActionHandler file

--- game/src/main/kotlin/gg/rsmod/game/action/ActionHandler.kt
+++ game/src/main/kotlin/gg/rsmod/game/action/ActionHandler.kt
@@ -1,8 +1,37 @@
 package gg.rsmod.game.action
 
+import com.google.inject.Inject
 import gg.rsmod.game.model.client.Client
 import gg.rsmod.game.model.mob.Player
+import kotlin.reflect.KClass
 
 interface ActionHandler<T : Action> {
     fun handle(client: Client, player: Player, action: T)
+}
+
+class ActionHandlerMap(
+    val handlers: MutableMap<KClass<out Action>, ActionHandler<*>>
+) : Map<KClass<out Action>, ActionHandler<*>> by handlers {
+
+    @Inject
+    constructor() : this(mutableMapOf())
+
+    inline fun <reified T : Action> register(init: ActionHandlerBuilder<T>.() -> Unit) {
+        if (handlers.containsKey(T::class)) {
+            error("Action already has a handler (action=${T::class.simpleName}).")
+        }
+        val builder = ActionHandlerBuilder<T>().apply(init)
+        val handler = builder.handler ?: error("Action handler has not been set.")
+        handlers[T::class] = handler
+    }
+
+    inline operator fun <reified T : Action> get(action: T) = handlers[action::class]
+}
+
+@DslMarker
+private annotation class BuilderDslMarker
+
+@BuilderDslMarker
+class ActionHandlerBuilder<T : Action> {
+    var handler: ActionHandler<T>? = null
 }
