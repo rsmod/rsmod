26448eee4986e194d88e2cb6f2f57f6e0f6ba2e7
= <admin@indovaps.com>
2020-09-10 01:43:07 -0400

    Add max read attempts on GameSessionDecoder#readPayload

--- plugins/protocol/src/main/kotlin/gg/rsmod/plugins/protocol/codec/game/GameSessionDecoder.kt
+++ plugins/protocol/src/main/kotlin/gg/rsmod/plugins/protocol/codec/game/GameSessionDecoder.kt
@@ -5,6 +5,7 @@ import gg.rsmod.game.action.ActionHandlerMap
 import gg.rsmod.game.message.ClientPacketStructureMap
 import gg.rsmod.util.IsaacRandom
 import io.netty.buffer.ByteBuf
+import io.netty.channel.Channel
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.ByteToMessageDecoder
 
@@ -22,7 +23,8 @@ class GameSessionDecoder(
     private val handlers: ActionHandlerMap,
     private var stage: PacketDecodeStage = PacketDecodeStage.Opcode,
     private var opcode: Int = -1,
-    private var length: Int = 0
+    private var length: Int = 0,
+    private var readAttempts: Int = 0
 ) : ByteToMessageDecoder() {
 
     override fun decode(
@@ -31,13 +33,13 @@ class GameSessionDecoder(
         out: MutableList<Any>
     ) {
         when (stage) {
-            PacketDecodeStage.Opcode -> buf.readOpcode(out)
-            PacketDecodeStage.Length -> buf.readLength(out)
-            PacketDecodeStage.Payload -> buf.readPayload(out)
+            PacketDecodeStage.Opcode -> buf.readOpcode(ctx.channel(), out)
+            PacketDecodeStage.Length -> buf.readLength(ctx.channel(), out)
+            PacketDecodeStage.Payload -> buf.readPayload(ctx.channel(), out)
         }
     }
 
-    private fun ByteBuf.readOpcode(out: MutableList<Any>) {
+    private fun ByteBuf.readOpcode(channel: Channel, out: MutableList<Any>) {
         opcode = readModifiedOpcode()
         val structure = structures[opcode]
         if (structure == null) {
@@ -47,24 +49,24 @@ class GameSessionDecoder(
 
         length = structure.length
         if (length == 0) {
-            readPayload(out)
+            readPayload(channel ,out)
             return
         }
         stage = if (length < 0) PacketDecodeStage.Length else PacketDecodeStage.Payload
     }
 
-    private fun ByteBuf.readLength(out: MutableList<Any>) {
+    private fun ByteBuf.readLength(channel: Channel, out: MutableList<Any>) {
         length = readBytesRequired() ?: return
         if (length == 0) {
-            readPayload(out)
+            readPayload(channel ,out)
         } else {
             stage = PacketDecodeStage.Payload
         }
     }
 
-    private fun ByteBuf.readPayload(out: MutableList<Any>) {
+    private fun ByteBuf.readPayload(channel: Channel, out: MutableList<Any>) {
         if (readableBytes() < length) {
-            // TODO: max attempts before disconnecting player?
+            channel.incrementReadAttempts()
             return
         }
         try {
@@ -87,6 +89,16 @@ class GameSessionDecoder(
         }
     }
 
+    private fun Channel.incrementReadAttempts() {
+        readAttempts++
+        if (readAttempts >= MAX_READ_ATTEMPTS) {
+            logger.debug { "Read attempt limit reached... dropping connection (channel=$this)" }
+            close()
+        } else {
+            logger.debug { "Increment read attempts (attempts=${readAttempts}, channel=$this)" }
+        }
+    }
+
     private fun ByteBuf.readModifiedOpcode(): Int {
         return (readUnsignedByte().toInt() - isaacRandom.opcodeModifier()) and 0xFF
     }
@@ -100,5 +112,7 @@ class GameSessionDecoder(
     companion object {
         private const val BYTE_VARIABLE_LENGTH = -1
         private const val SHORT_VARIABLE_LENGTH = -2
+
+        private const val MAX_READ_ATTEMPTS = 10
     }
 }
