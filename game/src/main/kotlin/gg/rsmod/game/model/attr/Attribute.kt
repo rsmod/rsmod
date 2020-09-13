2866746d58fb9834224299277f125a73cfa2d4ae
= <admin@indovaps.com>
2020-09-13 10:45:02 -0400

    Add AttributeMap support

---
+++ game/src/main/kotlin/gg/rsmod/game/model/attr/Attribute.kt
@@ -0,0 +1,62 @@
+package gg.rsmod.game.model.attr
+
+interface AttributeFlag
+
+class AttributeKey<T>(
+    private val persistenceKey: String? = null,
+    private val flags: MutableSet<AttributeFlag> = mutableSetOf()
+) {
+
+    fun flag(flag: AttributeFlag): AttributeKey<T> {
+        flags.add(flag)
+        return this
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as AttributeKey<*>
+
+        return if (persistenceKey != null) {
+            other.persistenceKey == persistenceKey
+        } else {
+            super.equals(other)
+        }
+    }
+
+    override fun hashCode(): Int {
+        return persistenceKey?.hashCode() ?: super.hashCode()
+    }
+}
+
+class AttributeMap(
+    private val attributes: MutableMap<AttributeKey<*>, Any> = mutableMapOf()
+) : Iterable<Map.Entry<AttributeKey<*>, Any>> {
+
+    val size: Int
+        get() = attributes.size
+
+    fun contains(key: AttributeKey<*>): Boolean {
+        return attributes.contains(key)
+    }
+
+    fun isEmpty(): Boolean = attributes.isEmpty()
+
+    fun isNotEmpty(): Boolean = attributes.isNotEmpty()
+
+    operator fun set(key: AttributeKey<*>, value: Any?) {
+        if (value != null) {
+            attributes[key] = value
+        } else {
+            attributes.remove(key)
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    operator fun <T> get(key: AttributeKey<T>): T? {
+        return attributes[key] as? T
+    }
+
+    override fun iterator(): Iterator<Map.Entry<AttributeKey<*>, Any>> = attributes.iterator()
+}
---
+++ game/src/test/kotlin/gg/rsmod/game/model/attr/AttributeMapTests.kt
@@ -0,0 +1,96 @@
+package gg.rsmod.game.model.attr
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class AttributeMapTests {
+
+    @Test
+    fun `attribute map should not find a key when empty`() {
+        val key1 = AttributeKey<Int>("test_key")
+        val key2 = AttributeKey<Int>()
+
+        val map = AttributeMap()
+        Assertions.assertTrue(map.isEmpty())
+        Assertions.assertNull(map[key1])
+        Assertions.assertNull(map[key2])
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(AttributeDoubleValueProvider::class)
+    fun `attribute keys with same persistence key should overwrite`(value1: Int, value2: Int) {
+        val key1 = AttributeKey<Int>("test_key")
+        val key2 = AttributeKey<Int>("test_key")
+
+        val map = AttributeMap()
+
+        map[key1] = value1
+        Assertions.assertNotNull(map[key1])
+        Assertions.assertNotNull(map[key2])
+        Assertions.assertEquals(map[key1], map[key2])
+        Assertions.assertEquals(1, map.size)
+
+        map[key2] = value2
+        Assertions.assertEquals(value2, map[key1])
+        Assertions.assertEquals(1, map.size)
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(AttributeDoubleValueProvider::class)
+    fun `attribute keys with same persistence key and different flags should overwrite`(value1: Int, value2: Int) {
+        val flag = object : AttributeFlag {}
+        val key1 = AttributeKey<Int>("test_key").flag(flag)
+        val key2 = AttributeKey<Int>("test_key")
+
+        val map = AttributeMap()
+
+        map[key1] = value1
+        Assertions.assertNotNull(map[key1])
+        Assertions.assertNotNull(map[key2])
+        Assertions.assertEquals(map[key1], map[key2])
+        Assertions.assertEquals(1, map.size)
+
+        map[key2] = value2
+        Assertions.assertEquals(value2, map[key1])
+        Assertions.assertEquals(1, map.size)
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(AttributeDoubleValueProvider::class)
+    fun `attribute keys with different persistence key should not be equal`(value1: Int, value2: Int) {
+        val key1 = AttributeKey<Int>("test_key")
+        val key2 = AttributeKey<Int>("test_key2")
+
+        val map = AttributeMap()
+
+        map[key1] = value1
+        Assertions.assertNotNull(map[key1])
+        Assertions.assertNull(map[key2])
+        Assertions.assertEquals(1, map.size)
+
+        map[key2] = value2
+        Assertions.assertNotNull(map[key2])
+        Assertions.assertNotEquals(map[key1], map[key2])
+        Assertions.assertEquals(2, map.size)
+    }
+}
+
+private class AttributeDoubleValueProvider : ArgumentsProvider {
+
+    override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
+        return Stream.of(
+            Arguments.of(1, 2),
+            Arguments.of(8, 16),
+            Arguments.of(32, 64),
+            Arguments.of(128, 256)
+        )
+    }
+}
