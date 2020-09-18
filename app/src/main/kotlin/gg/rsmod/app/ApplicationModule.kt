56dc6e93ce7ccf4dedf23dadf5c1d8cbaf31244a
= <admin@indovaps.com>
2020-09-18 17:17:11 -0400

    Bind PasswordEncryption implementation

--- app/src/main/kotlin/gg/rsmod/app/ApplicationModule.kt
+++ app/src/main/kotlin/gg/rsmod/app/ApplicationModule.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.inject.Scope
 import dev.misfitlabs.kotlinguice4.KotlinModule
 import gg.rsmod.util.config.ObjectMapperProvider
+import gg.rsmod.util.security.BCryptEncryption
+import gg.rsmod.util.security.PasswordEncryption
 
 class ApplicationModule(
     private val scope: Scope
@@ -12,6 +14,10 @@ class ApplicationModule(
     override fun configure() {
         bind<ObjectMapper>()
             .toProvider<ObjectMapperProvider>()
+            .`in`(scope)
+
+        bind<PasswordEncryption>()
+            .to<BCryptEncryption>()
             .`in`(scope)
     }
 }
