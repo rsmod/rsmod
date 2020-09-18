54ab23229d5d75c7be1eacb470e8d4ea98603cac
= <admin@indovaps.com>
2020-09-18 17:48:54 -0400

    Replace LoginDispatcher with AccountDispatcher

--- plugins/protocol/src/main/kotlin/gg/rsmod/plugins/protocol/codec/account/AccountDispatcher.kt
+++ plugins/protocol/src/main/kotlin/gg/rsmod/plugins/protocol/codec/account/AccountDispatcher.kt
@@ -2,21 +2,35 @@ package gg.rsmod.plugins.protocol.codec.account
 
 import com.github.michaelbull.logging.InlineLogger
 import com.google.inject.Inject
+import gg.rsmod.game.action.ActionHandlerMap
 import gg.rsmod.game.config.InternalConfig
 import gg.rsmod.game.config.RsaConfig
 import gg.rsmod.game.coroutine.IoCoroutineScope
 import gg.rsmod.game.dispatch.GameJobDispatcher
+import gg.rsmod.game.event.EventBus
 import gg.rsmod.game.model.client.Client
+import gg.rsmod.game.model.domain.repo.XteaRepository
 import gg.rsmod.game.model.domain.serializer.ClientDeserializeRequest
 import gg.rsmod.game.model.domain.serializer.ClientDeserializeResponse
 import gg.rsmod.game.model.domain.serializer.ClientSerializer
+import gg.rsmod.game.model.mob.Player
 import gg.rsmod.game.model.mob.PlayerList
+import gg.rsmod.plugins.protocol.DesktopPacketStructure
+import gg.rsmod.plugins.protocol.Device
+import gg.rsmod.plugins.protocol.codec.HandshakeConstants
 import gg.rsmod.plugins.protocol.codec.ResponseType
 import gg.rsmod.plugins.protocol.codec.game.ChannelMessageListener
-import gg.rsmod.plugins.protocol.codec.login.LoginDispatcher
+import gg.rsmod.plugins.protocol.codec.game.GameSessionDecoder
+import gg.rsmod.plugins.protocol.codec.game.GameSessionEncoder
+import gg.rsmod.plugins.protocol.codec.game.GameSessionHandler
 import gg.rsmod.plugins.protocol.codec.login.LoginRequest
+import gg.rsmod.plugins.protocol.codec.login.LoginResponse
 import gg.rsmod.plugins.protocol.codec.writeErrResponse
+import gg.rsmod.plugins.protocol.packet.server.PlayerInfo
+import gg.rsmod.plugins.protocol.packet.server.RebuildNormal
 import gg.rsmod.util.IsaacRandom
+import io.netty.channel.Channel
+import io.netty.channel.ChannelPipeline
 import java.util.concurrent.ConcurrentLinkedQueue
 import kotlinx.coroutines.launch
 
@@ -27,9 +41,12 @@ class AccountDispatcher @Inject constructor(
     private val internalConfig: InternalConfig,
     private val ioCoroutineScope: IoCoroutineScope,
     private val gameJobDispatcher: GameJobDispatcher,
-    private val loginDispatcher: LoginDispatcher,
     private val serializer: ClientSerializer,
-    private val playerList: PlayerList
+    private val playerList: PlayerList,
+    private val actionHandlers: ActionHandlerMap,
+    private val desktopStructures: DesktopPacketStructure,
+    private val xteas: XteaRepository,
+    private val eventBus: EventBus
 ) {
 
     private val registerQueue = ConcurrentLinkedQueue<Account>()
@@ -49,18 +66,25 @@ class AccountDispatcher @Inject constructor(
     }
 
     fun unregister(client: Client) {
-        unregisterQueue.add(client)
+        // TODO: back-off strategy to keep trying to serialize
+        // if failure occurs
+        ioCoroutineScope.launch {
+            serializer.serialize(client)
+            unregisterQueue.add(client)
+        }
     }
 
     private fun gameCycle() {
         for (i in 0 until internalConfig.logoutsPerCycle) {
             val logout = unregisterQueue.poll() ?: break
+            logger.debug { "Unregister player from game (player=${logout.player})" }
             playerList.remove(logout.player)
         }
 
         for (i in 0 until internalConfig.loginsPerCycle) {
             val account = registerQueue.poll() ?: break
-            loginDispatcher.login(account)
+            logger.debug { "Register account to game (account=$account)" }
+            login(account)
         }
     }
 
@@ -105,4 +129,131 @@ class AccountDispatcher @Inject constructor(
             }
         }
     }
+
+    private fun login(account: Account) {
+        val channel = account.channel
+        val client = account.client
+        val device = account.device
+        val decodeIsaac = account.decodeIsaac
+        val encodeIsaac = account.encodeIsaac
+
+        val online = playerList.any { it?.id?.value == client.player.id.value }
+        if (online) {
+            channel.writeErrResponse(ResponseType.ACCOUNT_ONLINE)
+            return
+        }
+
+        val registered = playerList.register(client.player)
+        if (!registered) {
+            channel.writeErrResponse(ResponseType.WORLD_FULL)
+            return
+        }
+        client.register(channel, device, decodeIsaac, encodeIsaac)
+    }
+
+    private fun Client.register(
+        channel: Channel,
+        device: Device,
+        decodeIsaac: IsaacRandom,
+        encodeIsaac: IsaacRandom
+    ) {
+        val gpi = player.gpi()
+        val reconnect = false
+        writeResponse(channel, encodeIsaac, reconnect, gpi)
+        channel.pipeline().applyGameCodec(
+            this,
+            device,
+            decodeIsaac,
+            encodeIsaac
+        )
+        player.login(reconnect, gpi)
+    }
+
+    private fun Client.writeResponse(
+        channel: Channel,
+        encodeIsaac: IsaacRandom,
+        reconnect: Boolean,
+        gpi: PlayerInfo
+    ) {
+        val response = if (reconnect) {
+            LoginResponse.Reconnect(gpi)
+        } else {
+            LoginResponse.Normal(
+                playerIndex = player.index,
+                privilege = player.entity.privilege,
+                moderator = true,
+                rememberDevice = false,
+                encodeIsaac = encodeIsaac,
+                members = true
+            )
+        }
+        channel.write(response)
+        channel.flush()
+    }
+
+    private fun ChannelPipeline.applyGameCodec(
+        client: Client,
+        device: Device,
+        decodeIsaac: IsaacRandom,
+        encodeIsaac: IsaacRandom
+    ) {
+        val structures = when (device) {
+            Device.Desktop -> desktopStructures
+            else -> TODO()
+        }
+        val decoder = GameSessionDecoder(decodeIsaac, structures.client, actionHandlers)
+        val encoder = GameSessionEncoder(encodeIsaac, structures.server)
+        val handler = GameSessionHandler(client, this@AccountDispatcher)
+
+        remove(HandshakeConstants.RESPONSE_PIPELINE)
+
+        replace(
+            HandshakeConstants.DECODER_PIPELINE,
+            HandshakeConstants.DECODER_PIPELINE,
+            decoder
+        )
+        replace(
+            HandshakeConstants.ENCODER_PIPELINE,
+            HandshakeConstants.ENCODER_PIPELINE,
+            encoder
+        )
+        replace(
+            HandshakeConstants.ADAPTER_PIPELINE,
+            HandshakeConstants.ADAPTER_PIPELINE,
+            handler
+        )
+    }
+
+    private fun Player.login(reconnect: Boolean, gpi: PlayerInfo) {
+        if (!reconnect) {
+            val rebuildNormal = RebuildNormal(
+                gpi = gpi,
+                zoneX = coords.x shr 3,
+                zoneY = coords.y shr 3,
+                xteas = xteas
+            )
+            write(rebuildNormal)
+            flush()
+        }
+        login(eventBus)
+    }
+
+    private fun PlayerList.playerCoords(excludeIndex: Int): IntArray {
+        var index = 0
+        val coordinates = IntArray(capacity - 1)
+        for (i in indices) {
+            if (i == excludeIndex) {
+                continue
+            }
+            val player = this[i]
+            val coords = player?.coords?.packed18Bits ?: 0
+            coordinates[index++] = coords
+        }
+        return coordinates
+    }
+
+    private fun Player.gpi() = PlayerInfo(
+        playerCoordsAs30Bits = coords.packed30Bits,
+        otherPlayerCoords = playerList.playerCoords(index)
+    )
 }
--- plugins/protocol/src/main/kotlin/gg/rsmod/plugins/protocol/codec/game/GameSessionHandler.kt
+++ plugins/protocol/src/main/kotlin/gg/rsmod/plugins/protocol/codec/game/GameSessionHandler.kt
@@ -3,6 +3,7 @@ package gg.rsmod.plugins.protocol.codec.game
 import com.github.michaelbull.logging.InlineLogger
 import gg.rsmod.game.action.ActionHandler
 import gg.rsmod.game.model.client.Client
+import gg.rsmod.plugins.protocol.codec.account.AccountDispatcher
 import gg.rsmod.plugins.protocol.codec.exceptionCaught
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInboundHandlerAdapter
@@ -10,16 +11,18 @@ import io.netty.channel.ChannelInboundHandlerAdapter
 private val logger = InlineLogger()
 
 class GameSessionHandler(
-    private val client: Client
+    private val client: Client,
+    private val dispatcher: AccountDispatcher
 ) : ChannelInboundHandlerAdapter() {
 
     override fun handlerAdded(ctx: ChannelHandlerContext) {
-        logger.debug { "Channel registered (username=${client.player.username}, channel=${ctx.channel()})" }
+        logger.trace { "Channel registered (username=${client.player.username}, channel=${ctx.channel()})" }
         super.handlerAdded(ctx)
     }
 
     override fun channelUnregistered(ctx: ChannelHandlerContext) {
-        logger.debug { "Channel unregistered (username=${client.player.username}, channel=${ctx.channel()})" }
+        logger.trace { "Channel unregistered (username=${client.player.username}, channel=${ctx.channel()})" }
+        dispatcher.unregister(client)
         super.channelUnregistered(ctx)
     }
 
