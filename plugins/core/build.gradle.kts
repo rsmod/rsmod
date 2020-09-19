9073bf1e47fcf58221d67734bf8aca4b731c0270
= <admin@indovaps.com>
2020-09-18 20:50:04 -0400

    Add core plugin subprojects default dependencies

--- plugins/core/build.gradle.kts
+++ plugins/core/build.gradle.kts
@@ -1,3 +1,9 @@
-dependencies {
-    implementation(project(":util"))
+subprojects {
+    group = "gg.rsmod.plugins.core"
+
+    dependencies {
+        implementation(project(":util"))
+        implementation(project(":plugins:api"))
+        implementation(project(":plugins:protocol"))
+    }
 }
