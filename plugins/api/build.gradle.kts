db9c7846385f16e31303d2a28c301932cf067344
= <admin@indovaps.com>
2020-09-26 03:55:20 -0400

    Swap dependencies for api and protocol projects

--- plugins/api/build.gradle.kts
+++ plugins/api/build.gradle.kts
@@ -1,1 +1,5 @@
 version = "1.0.0-SNAPSHOT"
+
+dependencies {
+    implementation(project(":plugins:protocol"))
+}
--- plugins/protocol/build.gradle.kts
+++ plugins/protocol/build.gradle.kts
@@ -4,7 +4,6 @@ dependencies {
     implementation(project(":util"))
     implementation(project(":cache"))
     implementation(project(":net"))
-    implementation(project(":plugins:api"))
     implementation("io.netty:netty-all:${NetVersions.NETTY}")
     api("io.guthix:jagex-bytebuf:${ProjectVersions.GUTHIX_BYTEBUF}")
     api("io.guthix:jagex-store-5:${ProjectVersions.JS5_STORE}")
