ceeba7d64e7f89d2352c09a695c9df34591ec807
= <admin@indovaps.com>
2020-09-28 06:21:26 -0400

    Add game project tests

---
+++ game/src/test/kotlin/gg/rsmod/game/dispatch/GameJobDispatcherTests.kt
@@ -0,0 +1,43 @@
+package gg.rsmod.game.dispatch
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class GameJobDispatcherTests {
+
+    @Test
+    fun `add continuous game job`() {
+        val dispatcher = GameJobDispatcher()
+        dispatcher.schedule {}
+        Assertions.assertEquals(1, dispatcher.size)
+    }
+
+    @Test
+    fun `add one-time game job`() {
+        val dispatcher = GameJobDispatcher()
+        dispatcher.execute {}
+        Assertions.assertEquals(1, dispatcher.size)
+    }
+
+    @Test
+    fun `keep continuous game job after executing all jobs`() {
+        val dispatcher = GameJobDispatcher()
+        dispatcher.schedule {}
+        Assertions.assertEquals(1, dispatcher.size)
+
+        dispatcher.executeAll()
+        Assertions.assertEquals(1, dispatcher.size)
+    }
+
+    @Test
+    fun `discard one-time game job after executing all jobs`() {
+        val dispatcher = GameJobDispatcher()
+        dispatcher.execute {}
+        Assertions.assertEquals(1, dispatcher.size)
+
+        dispatcher.executeAll()
+        Assertions.assertEquals(0, dispatcher.size)
+    }
+}
---
+++ game/src/test/kotlin/gg/rsmod/game/model/map/MapTests.kt
@@ -0,0 +1,78 @@
+package gg.rsmod.game.model.map
+
+import java.util.stream.Stream
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MapTests {
+
+    @ParameterizedTest
+    @ArgumentsSource(CoordinateProvider::class)
+    fun `create coordinates`(x: Int, y: Int, plane: Int) {
+        val coords = Coordinates(x, y, plane)
+        Assertions.assertEquals(x, coords.x)
+        Assertions.assertEquals(y, coords.y)
+        Assertions.assertEquals(plane, coords.plane)
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(TranslationProvider::class)
+    fun `translate coordinates`(offX: Int, offY: Int, offPlane: Int) {
+        val coords = Coordinates(3200, 3200, 0)
+        val translation = coords.translate(offX, offY, offPlane)
+        Assertions.assertEquals(coords.x + offX, translation.x)
+        Assertions.assertEquals(coords.y + offY, translation.y)
+        Assertions.assertEquals(coords.plane + offPlane, translation.plane)
+    }
+}
+
+private object CoordinateProvider : ArgumentsProvider {
+
+    override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
+        return Stream.of(
+            Arguments.of(0, 0, 0),
+            Arguments.of(0, 0, 1),
+            Arguments.of(0, 0, 2),
+            Arguments.of(3200, 3200, 0),
+            Arguments.of(3200, 3200, 1),
+            Arguments.of(3200, 3200, 2),
+            Arguments.of(9600, 6400, 0),
+            Arguments.of(9600, 6400, 1),
+            Arguments.of(9600, 6400, 2),
+            Arguments.of(6400, 9600, 0),
+            Arguments.of(6400, 9600, 1),
+            Arguments.of(6400, 9600, 2),
+            Arguments.of(16383, 16383, 0),
+            Arguments.of(16383, 16383, 1),
+            Arguments.of(16383, 16383, 2)
+        )
+    }
+}
+
+private object TranslationProvider : ArgumentsProvider {
+
+    override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
+        return Stream.of(
+            Arguments.of(0, 0, 0),
+            Arguments.of(1, 0, 0),
+            Arguments.of(2, 0, 0),
+            Arguments.of(0, 1, 0),
+            Arguments.of(0, 2, 0),
+            Arguments.of(0, 0, 1),
+            Arguments.of(0, 0, 2),
+            Arguments.of(1, 1, 1),
+            Arguments.of(1, 2, 1),
+            Arguments.of(1, 3, 1),
+            Arguments.of(2, 1, 1),
+            Arguments.of(3, 1, 1),
+            Arguments.of(1, 1, 2),
+            Arguments.of(1, 1, 3)
+        )
+    }
+}
