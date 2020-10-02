1a4f4b9d21d5fe7316002516870bd84ada94fe65
= <admin@indovaps.com>
2020-10-01 21:18:40 -0400

    Format PluginExt newlines

--- plugins/api/src/main/kotlin/gg/rsmod/plugins/api/PluginExt.kt
+++ plugins/api/src/main/kotlin/gg/rsmod/plugins/api/PluginExt.kt
@@ -3,25 +3,19 @@ package gg.rsmod.plugins.api
 import gg.rsmod.game.event.impl.LoginEvent
 import gg.rsmod.game.plugin.Plugin
 
-fun Plugin.onEarlyLogin(
-    block: LoginEvent.() -> Unit
-) {
+fun Plugin.onEarlyLogin(block: LoginEvent.() -> Unit) {
     onEvent<LoginEvent>()
         .where { priority == LoginEvent.Priority.High }
         .then(block)
 }
 
-fun Plugin.onLogin(
-    block: LoginEvent.() -> Unit
-) {
+fun Plugin.onLogin(block: LoginEvent.() -> Unit) {
     onEvent<LoginEvent>()
         .where { priority == LoginEvent.Priority.Normal }
         .then(block)
 }
 
-fun Plugin.onPostLogin(
-    block: LoginEvent.() -> Unit
-) {
+fun Plugin.onPostLogin(block: LoginEvent.() -> Unit) {
     onEvent<LoginEvent>()
         .where { priority == LoginEvent.Priority.Low }
         .then(block)
