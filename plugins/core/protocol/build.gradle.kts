08b99dc061a0e911c82ca3da65b97512b5585ae1
= <admin@indovaps.com>
2020-10-23 16:24:37 -0400

    Change protocol module api dependency to implementation

--- plugins/core/protocol/build.gradle.kts
+++ plugins/core/protocol/build.gradle.kts
@@ -6,6 +6,6 @@ dependencies {
     implementation(project(":net"))
     implementation("io.netty:netty-all:${NetVersions.NETTY}")
     implementation("com.michael-bull.kotlin-retry:kotlin-retry:${ProjectVersions.KOTLIN_RETRY}")
-    api("io.guthix:jagex-bytebuf:${ProjectVersions.GUTHIX_BYTEBUF}")
-    api("io.guthix:jagex-store-5:${ProjectVersions.JS5_STORE}")
+    implementation("io.guthix:jagex-bytebuf:${ProjectVersions.GUTHIX_BYTEBUF}")
+    implementation("io.guthix:jagex-store-5:${ProjectVersions.JS5_STORE}")
 }
