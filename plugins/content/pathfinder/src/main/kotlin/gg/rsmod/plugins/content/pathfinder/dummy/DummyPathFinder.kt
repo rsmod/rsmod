f9b4b5e347c9be6f6028fcd2d28024573cd0d34d
= <admin@indovaps.com>
2020-10-31 10:00:53 -0400

    Move Coordinates::rayCast

--- plugins/content/pathfinder/src/main/kotlin/gg/rsmod/plugins/content/pathfinder/dummy/DummyPathFinder.kt
+++ plugins/content/pathfinder/src/main/kotlin/gg/rsmod/plugins/content/pathfinder/dummy/DummyPathFinder.kt
@@ -1,8 +1,8 @@
 package gg.rsmod.plugins.content.pathfinder.dummy
 
-import gg.rsmod.game.model.domain.rayCast
 import gg.rsmod.game.model.map.Coordinates
 import gg.rsmod.game.path.PathFinder
+import kotlin.math.abs
 
 class DummyPathFinder : PathFinder {
 
@@ -14,4 +14,42 @@ class DummyPathFinder : PathFinder {
     ): List<Coordinates> {
         return start.rayCast(destination)
     }
+}
+
+private fun Coordinates.rayCast(destination: Coordinates): List<Coordinates> {
+    var diffX = x - destination.x
+    var diffY = y - destination.y
+
+    val steps = abs(diffX).coerceAtLeast(abs(diffY))
+    if (steps == 0) {
+        return emptyList()
+    }
+
+    val coordinates = mutableListOf<Coordinates>()
+    var prev: Coordinates = this
+    repeat(steps) {
+        var translateX = 0
+        var translateY = 0
+
+        if (diffX < 0) {
+            diffX++
+            translateX = 1
+        } else if (diffX > 0) {
+            diffX--
+            translateX = -1
+        }
+
+        if (diffY < 0) {
+            diffY++
+            translateY = 1
+        } else if (diffY > 0) {
+            diffY--
+            translateY = -1
+        }
+
+        val next = prev.translate(translateX, translateY)
+        coordinates.add(next)
+        prev = next
+    }
+    return coordinates
 }
