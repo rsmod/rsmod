7485d2ac6be2b42db80198c317ca07d44b68ab92
= <admin@indovaps.com>
2020-10-31 06:59:13 -0400

    Fix up default item containers

--- plugins/api/src/main/kotlin/gg/rsmod/plugins/api/container/DefaultContainer.kt
+++ plugins/api/src/main/kotlin/gg/rsmod/plugins/api/container/DefaultContainer.kt
@@ -5,10 +5,6 @@ import gg.rsmod.game.model.item.ItemContainer
 import gg.rsmod.game.model.mob.Player
 import gg.rsmod.game.model.ui.Component
 
-private val inventoryKey = ItemContainerKey()
-private val equipmentKey = ItemContainerKey()
-private val bankKey = ItemContainerKey()
-
 private const val INVENTORY_SIZE = 28
 private const val EQUIPMENT_SIZE = 12
 private const val BANK_SIZE = 800
@@ -17,17 +13,29 @@ private const val INVENTORY_ID = 93
 private const val EQUIPMENT_ID = 94
 private const val BANK_ID = 95
 
+private val inventoryKey = ItemContainerKey()
+private val equipmentKey = ItemContainerKey()
+private val bankKey = ItemContainerKey()
+
 private val inventoryComponent = Component(149, 0)
 
-internal fun Player.setContainers() {
-    setContainer(inventoryKey, inventory, INVENTORY_SIZE)
-    setContainer(equipmentKey, equipment, EQUIPMENT_SIZE)
+internal fun Player.setDefaultContainers() {
+    setContainer(inventoryKey, inventory, INVENTORY_SIZE, loginUpdate = true)
+    setContainer(equipmentKey, equipment, EQUIPMENT_SIZE, loginUpdate = true)
     setContainer(bankKey, bank, BANK_SIZE)
 }
 
-internal fun Player.setContainer(key: ItemContainerKey, container: ItemContainer, size: Int) {
+internal fun Player.setContainer(
+    key: ItemContainerKey,
+    container: ItemContainer,
+    size: Int,
+    loginUpdate: Boolean = false
+) {
     containers[key] = container
     container.grow(size)
+    if (loginUpdate) {
+        container.update = true
+    }
 }
 
 internal fun Player.updateDefaultContainer(key: ItemContainerKey, container: ItemContainer) {
--- plugins/api/src/main/kotlin/gg/rsmod/plugins/api/container/default-containers.plugin.kts
+++ plugins/api/src/main/kotlin/gg/rsmod/plugins/api/container/default-containers.plugin.kts
@@ -4,7 +4,7 @@ import gg.rsmod.game.event.impl.ClientRegister
 import gg.rsmod.game.event.impl.ItemContainerUpdate
 
 onEvent<ClientRegister>()
-    .then { client.player.setContainers() }
+    .then { client.player.setDefaultContainers() }
 
 onEvent<ItemContainerUpdate>()
     .then { player.updateDefaultContainer(key, container) }
