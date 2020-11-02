138b0da13db25144850b45d30884ecbdb035d6a1
= <admin@indovaps.com>
2020-11-02 04:21:08 -0500

    Add Serializer plugin netty buffer dependency

--- plugins/content/serializer/build.gradle.kts
+++ plugins/content/serializer/build.gradle.kts
@@ -3,4 +3,5 @@ version = "1.0.0-SNAPSHOT"
 dependencies {
     implementation(project(":util"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${JacksonVersions.JACKSON}")
+    implementation("io.netty:netty-buffer:${NetVersions.NETTY}")
 }
