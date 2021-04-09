086db845a256c8582f2c4458c131e7b817c81f99
= <admin@indovaps.com>
2021-04-09 18:51:32 -0400

    Move PrivilegeMap::register function

--- game/src/main/kotlin/org/rsmod/game/privilege/PrivilegeMap.kt
+++ game/src/main/kotlin/org/rsmod/game/privilege/PrivilegeMap.kt
@@ -2,4 +2,9 @@ package org.rsmod.game.privilege
 
 import org.rsmod.game.name.TypeNamedMap
 
-class PrivilegeMap : TypeNamedMap<Privilege>()
+class PrivilegeMap : TypeNamedMap<Privilege>() {
+
+    fun register(privilege: Privilege) {
+        this[privilege.nameId] = privilege
+    }
+}
--- plugins/api/src/main/kotlin/org/rsmod/plugins/api/privilege/default-privilege.plugin.kts
+++ plugins/api/src/main/kotlin/org/rsmod/plugins/api/privilege/default-privilege.plugin.kts
@@ -1,13 +1,8 @@
 package org.rsmod.plugins.api.privilege
 
-import org.rsmod.game.privilege.Privilege
 import org.rsmod.game.privilege.PrivilegeMap
 
 val privileges: PrivilegeMap by inject()
 
 privileges.register(Privileges.Mod)
 privileges.register(Privileges.Admin)
-
-fun PrivilegeMap.register(privilege: Privilege) {
-    this[privilege.nameId] = privilege
-}
