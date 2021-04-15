11c38a26c1bbb3a55ac9c825bd76179d8b10864d
= <admin@indovaps.com>
2021-04-15 13:53:26 -0400

    Reformat GameModule

--- game/src/main/kotlin/org/rsmod/game/GameModule.kt
+++ game/src/main/kotlin/org/rsmod/game/GameModule.kt
@@ -5,13 +5,13 @@ import dev.misfitlabs.kotlinguice4.KotlinModule
 import org.rsmod.game.action.ActionBus
 import org.rsmod.game.cmd.CommandMap
 import org.rsmod.game.collision.CollisionMap
-import org.rsmod.game.model.obj.GameObjectMap
 import org.rsmod.game.event.EventBus
 import org.rsmod.game.model.client.ClientList
 import org.rsmod.game.model.map.MapIsolation
 import org.rsmod.game.model.mob.NpcList
 import org.rsmod.game.model.mob.PlayerList
 import org.rsmod.game.model.obj.GameObjectApSet
+import org.rsmod.game.model.obj.GameObjectMap
 import org.rsmod.game.model.world.World
 import org.rsmod.game.privilege.PrivilegeMap
 import org.rsmod.game.task.StartupTaskList
@@ -20,49 +20,20 @@ import org.rsmod.game.update.task.UpdateTaskList
 class GameModule(private val scope: Scope) : KotlinModule() {
 
     override fun configure() {
-        bind<Game>()
-            .`in`(scope)
-
-        bind<World>()
-            .`in`(scope)
-
-        bind<EventBus>()
-            .`in`(scope)
-
-        bind<ActionBus>()
-            .`in`(scope)
-
-        bind<StartupTaskList>()
-            .`in`(scope)
-
-        bind<CommandMap>()
-            .`in`(scope)
-
-        bind<PlayerList>()
-            .`in`(scope)
-
-        bind<ClientList>()
-            .`in`(scope)
-
-        bind<NpcList>()
-            .`in`(scope)
-
-        bind<UpdateTaskList>()
-            .`in`(scope)
-
-        bind<MapIsolation>()
-            .`in`(scope)
-
-        bind<CollisionMap>()
-            .`in`(scope)
-
-        bind<GameObjectMap>()
-            .`in`(scope)
-
-        bind<GameObjectApSet>()
-            .`in`(scope)
-
-        bind<PrivilegeMap>()
-            .`in`(scope)
+        bind<Game>().`in`(scope)
+        bind<World>().`in`(scope)
+        bind<EventBus>().`in`(scope)
+        bind<ActionBus>().`in`(scope)
+        bind<StartupTaskList>().`in`(scope)
+        bind<CommandMap>().`in`(scope)
+        bind<PlayerList>().`in`(scope)
+        bind<ClientList>().`in`(scope)
+        bind<NpcList>().`in`(scope)
+        bind<UpdateTaskList>().`in`(scope)
+        bind<MapIsolation>().`in`(scope)
+        bind<CollisionMap>().`in`(scope)
+        bind<GameObjectMap>().`in`(scope)
+        bind<GameObjectApSet>().`in`(scope)
+        bind<PrivilegeMap>().`in`(scope)
     }
 }
