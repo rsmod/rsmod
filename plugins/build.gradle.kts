2965917431ebe91aad926d0359b1baf3908a6087
= <admin@indovaps.com>
2021-04-15 18:11:55 -0400

    Add task to copy plugin resources

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -12,17 +12,45 @@ subprojects {
     buildDir = rootPluginBuildDir.resolve(relative)
     group = "org.rsmod.plugins"
 
-    sourceSets {
-        main {
-            output.setResourcesDir(pluginConfigDir.resolve(relative))
-        }
-    }
-
     dependencies {
         implementation(kotlin("stdlib"))
         implementation(projectsAlias.game)
         implementation(libsAlias.kotlinScriptRuntime)
         implementation(libsAlias.kotlinCoroutinesCore)
         implementation(libsAlias.rsmodPathfinder)
+    }
+
+    tasks.register("install") {
+        copyResources(project)
+    }
+}
+
+tasks.register("install-plugins") {
+    subprojects.forEach { project ->
+        copyResources(project)
+    }
+}
+
+tasks.register("install-plugins-overwrite") {
+    subprojects.forEach { project ->
+        copyResources(project, overwriteFiles = true)
+    }
+}
+
+fun copyResources(project: Project, overwriteFiles: Boolean = false) {
+    val relativePluginDir = project.projectDir.relativeTo(rootPluginDir)
+    val pluginResourceFiles = project.sourceSets.main.get().resources.asFileTree
+    if (pluginResourceFiles.isEmpty) return
+    val configDirectory = pluginConfigDir.resolve(relativePluginDir)
+    pluginResourceFiles.forEach { file ->
+        val existingFile = configDirectory.resolve(file.name)
+        if (existingFile.exists() && !overwriteFiles) {
+            /* do not overwrite existing config files */
+            return@forEach
+        }
+        copy {
+            from(file)
+            into(configDirectory)
+        }
     }
 }
