8d06db10dc937d36adcffd2fb67255fca828bd5b
= <admin@indovaps.com>
2021-04-19 09:32:51 -0400

    Add run state varp

--- plugins/content/gameframe/src/main/kotlin/org/rsmod/plugins/content/gameframe/buttons.plugin.kts
+++ plugins/content/gameframe/src/main/kotlin/org/rsmod/plugins/content/gameframe/buttons.plugin.kts
@@ -2,19 +2,16 @@ package org.rsmod.plugins.content.gameframe
 
 import org.rsmod.game.model.mob.Player
 import org.rsmod.game.model.move.MovementSpeed
-import org.rsmod.game.model.vars.type.VarpTypeList
 import org.rsmod.plugins.api.model.mob.player.getVarp
 import org.rsmod.plugins.api.model.mob.player.sendMessage
 import org.rsmod.plugins.api.model.mob.player.setVarp
 import org.rsmod.plugins.api.model.mob.player.toggleVarp
 import org.rsmod.plugins.api.onButton
 
-val varps: VarpTypeList by inject()
+val runVarp = varp("run_state")
 
 val logout = component("logout_button")
 val runOrb = component("run_orb_button")
-
-val runVarp = varps[173]
 
 onButton(logout) { player.logout() }
 onButton(runOrb) { player.toggleRun() }
