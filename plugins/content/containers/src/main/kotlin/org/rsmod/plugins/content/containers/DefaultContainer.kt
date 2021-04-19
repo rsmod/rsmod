18bcdebb0c049055ea917af34cc299a62912a017
= <admin@indovaps.com>
2021-04-19 12:04:41 -0400

    Use named component for inventory component

--- plugins/content/containers/src/main/kotlin/org/rsmod/plugins/content/containers/DefaultContainer.kt
+++ plugins/content/containers/src/main/kotlin/org/rsmod/plugins/content/containers/DefaultContainer.kt
@@ -1,53 +1,12 @@
 package org.rsmod.plugins.content.containers
 
-import org.rsmod.game.model.item.container.ItemContainerKey
-import org.rsmod.game.model.item.container.ItemContainerStackMode
-import org.rsmod.game.model.mob.Player
-import org.rsmod.game.model.ui.Component
-import org.rsmod.plugins.api.model.mob.player.setContainer
+object DefaultContainer {
 
-private const val INVENTORY_SIZE = 28
-private const val EQUIPMENT_SIZE = 12
-private const val BANK_SIZE = 800
+    const val INVENTORY_SIZE = 28
+    const val EQUIPMENT_SIZE = 12
+    const val BANK_SIZE = 800
 
-private const val INVENTORY_ID = 93
-private const val EQUIPMENT_ID = 94
-private const val BANK_ID = 95
-
-private val inventoryComponent = Component(149, 0)
-
-/*
- * These "default containers" are cached for each player
- * and do not need to be exposed publicly.
- */
-internal val inventoryKey = ItemContainerKey.of(
-    "inventory",
-    ItemContainerStackMode.Default,
-    INVENTORY_SIZE,
-    INVENTORY_ID,
-    inventoryComponent
-)
-
-internal val equipmentKey = ItemContainerKey.ofClientId(
-    "equipment",
-    ItemContainerStackMode.Default,
-    EQUIPMENT_SIZE,
-    EQUIPMENT_ID
-)
-
-internal val bankKey = ItemContainerKey.ofClientId(
-    "bank",
-    ItemContainerStackMode.Always,
-    BANK_SIZE,
-    BANK_ID
-)
-
-internal fun Player.setDefaultContainers() {
-    setContainer(inventoryKey, inventory)
-    setContainer(equipmentKey, equipment)
-    setContainer(bankKey, bank)
-
-    /* enable auto-updates for necessary containers */
-    inventory.autoUpdate = true
-    equipment.autoUpdate = true
+    const val INVENTORY_CLIENT_ID = 93
+    const val EQUIPMENT_CLIENT_ID = 94
+    const val BANK_CLIENT_ID = 95
 }
--- plugins/content/containers/src/main/kotlin/org/rsmod/plugins/content/containers/default-containers.plugin.kts
+++ plugins/content/containers/src/main/kotlin/org/rsmod/plugins/content/containers/default-containers.plugin.kts
@@ -1,13 +1,57 @@
 package org.rsmod.plugins.content.containers
 
 import org.rsmod.game.event.impl.ClientRegister
+import org.rsmod.game.event.impl.ServerStartup
+import org.rsmod.game.model.item.container.ItemContainerKey
 import org.rsmod.game.model.item.container.ItemContainerKeyMap
+import org.rsmod.game.model.item.container.ItemContainerStackMode
+import org.rsmod.game.model.mob.Player
+import org.rsmod.plugins.api.model.mob.player.setContainer
 
 val keys: ItemContainerKeyMap by inject()
 
-keys.register(inventoryKey)
-keys.register(equipmentKey)
-keys.register(bankKey)
+val inventoryComponent = component("inventory_container").toComponent()
+
+val inventoryKey = ItemContainerKey.of(
+    "inventory",
+    ItemContainerStackMode.Default,
+    DefaultContainer.INVENTORY_SIZE,
+    DefaultContainer.INVENTORY_CLIENT_ID,
+    inventoryComponent
+)
+
+val equipmentKey = ItemContainerKey.ofClientId(
+    "equipment",
+    ItemContainerStackMode.Default,
+    DefaultContainer.EQUIPMENT_SIZE,
+    DefaultContainer.EQUIPMENT_CLIENT_ID
+)
+
+val bankKey = ItemContainerKey.ofClientId(
+    "bank",
+    ItemContainerStackMode.Always,
+    DefaultContainer.BANK_SIZE,
+    DefaultContainer.BANK_CLIENT_ID
+)
 
 onEvent<ClientRegister>()
     .then { client.player.setDefaultContainers() }
+
+onEvent<ServerStartup>()
+    .then { registerContainerKeys() }
+
+fun Player.setDefaultContainers() {
+    setContainer(inventoryKey, inventory)
+    setContainer(equipmentKey, equipment)
+    setContainer(bankKey, bank)
+
+    /* enable auto-updates for necessary containers */
+    inventory.autoUpdate = true
+    equipment.autoUpdate = true
+}
+
+fun registerContainerKeys() {
+    keys.register(inventoryKey)
+    keys.register(equipmentKey)
+    keys.register(bankKey)
+}
