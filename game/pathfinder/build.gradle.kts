0c0881a7ea608047aece3d0313528df248ec7735
Tomm0017 <rspsmods@gmail.com>
2022-12-20 01:23:06 -0500

    Add basic PathFinder benchmarks

--- game/pathfinder/build.gradle.kts
+++ game/pathfinder/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     kotlin("jvm")
+    id("me.champeau.gradle.jmh")
 }
 
 dependencies {
+    jmh("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.0")
     testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.0")
+}
+
+jmh {
+    profilers = listOf("stack")
 }
