eb1f3a8033ae3a9add81ebb147e1dee6d301324f
Tomm0017 <rspsmods@gmail.com>
2022-12-20 01:28:35 -0500

    Replace CollisionFlagMap::flags Map with Array

--- game/pathfinder/src/main/kotlin/org/rsmod/game/pathfinder/collision/CollisionFlagMap.kt
+++ game/pathfinder/src/main/kotlin/org/rsmod/game/pathfinder/collision/CollisionFlagMap.kt
@@ -1,8 +1,24 @@
 package org.rsmod.game.pathfinder.collision
 
-public class CollisionFlagMap(
-    private val tiles: MutableMap<Int, Int> = mutableMapOf()
-) {
+@Suppress("NOTHING_TO_INLINE", "MemberVisibilityCanBePrivate")
+public class CollisionFlagMap(private val flags: Array<IntArray?> = arrayOfNulls(TOTAL_ZONES)) {
+
+    public fun getOrAlloc(x: Int, y: Int, level: Int): IntArray {
+        val zoneIndex = coordsToZoneIndex(x, y, level)
+        val curr = flags[zoneIndex]
+        if (curr != null) return curr
+        val alloc = IntArray(ZONE_SIZE)
+        flags[zoneIndex] = alloc
+        return alloc
+    }
+
+    /**
+     * Deallocates the whole 8x8 zone to which [x], [y] and [level] belong.
+     */
+    public fun deallocate(x: Int, y: Int, level: Int) {
+        val zoneIndex = coordsToZoneIndex(x, y, level)
+        flags[zoneIndex] = null
+    }
 
     public fun add(x: Int, y: Int, level: Int, mask: Int) {
         val old = this[x, y, level]
@@ -15,20 +31,27 @@ public class CollisionFlagMap(
     }
 
     public operator fun get(x: Int, y: Int, level: Int): Int {
-        val packed = asPackedCoords(x, y, level)
-        return tiles[packed] ?: 0
+        val zoneIndex = coordsToZoneIndex(x, y, level)
+        val localIndex = coordsToZoneLocalIndex(x, y)
+        return flags[zoneIndex]?.get(localIndex) ?: 0
     }
 
     public operator fun set(x: Int, y: Int, level: Int, flags: Int) {
-        val packed = asPackedCoords(x, y, level)
-        tiles[packed] = flags
+        val zoneFlags = getOrAlloc(x, y, level)
+        val localIndex = coordsToZoneLocalIndex(x, y)
+        zoneFlags[localIndex] = flags
     }
 
     public companion object {
 
-        @Suppress("NOTHING_TO_INLINE")
-        public inline fun asPackedCoords(x: Int, y: Int, level: Int): Int {
-            return (x and 0x7FFF) or ((y and 0x7FFF) shl 15) or ((level and 0x3) shl 30)
+        public const val TOTAL_ZONES: Int = 2048 * 2048 * 4
+
+        public const val ZONE_SIZE: Int = 8 * 8
+
+        private inline fun coordsToZoneLocalIndex(x: Int, y: Int): Int = (x and 0x7) or ((y and 0x7) shl 3)
+
+        private inline fun coordsToZoneIndex(x: Int, y: Int, level: Int): Int {
+            return (x and 0x7FF) or ((y and 0x7FF) shl 11) or ((level and 0x3) shl 22)
         }
     }
 }
