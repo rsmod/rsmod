d546ee500554f365342680fb9131106c03f7644a
Tomm0017 <rspsmods@gmail.com>
2023-01-18 15:51:42 -0500

    Add StepValidator::naiveDestination
    Calculates the naive target/destination for NPC -> X pathfinding. Credits to https://github.com/GregHib (Greg#8550) for code

--- game/pathfinder/src/main/kotlin/org/rsmod/game/pathfinder/RouteCoordinates.kt
+++ game/pathfinder/src/main/kotlin/org/rsmod/game/pathfinder/RouteCoordinates.kt
@@ -16,6 +16,12 @@ public value class RouteCoordinates(public val packed: Int) {
         (x and 0x7FFF) or ((y and 0x7FFF) shl 15) or ((level and 0x3) shl 30)
     )
 
+    public fun translate(xOffset: Int, yOffset: Int, levelOffset: Int = 0): RouteCoordinates = RouteCoordinates(
+        x = x + xOffset,
+        y = y + yOffset,
+        level = level + levelOffset
+    )
+
     override fun toString(): String {
         return "RouteCoordinates(x=$x, y=$y, level=$level)"
     }
