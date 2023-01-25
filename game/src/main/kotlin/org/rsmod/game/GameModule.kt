4eb6d9861d21f9efb9d99d11d84b5aabf7d709e6
Tomm0017 <rspsmods@gmail.com>
2023-01-25 04:48:11 -0500

    Move cache related binding to CacheModule

--- game/src/main/kotlin/org/rsmod/game/GameModule.kt
+++ game/src/main/kotlin/org/rsmod/game/GameModule.kt
@@ -1,14 +1,8 @@
 package org.rsmod.game
 
 import com.google.inject.AbstractModule
-import com.google.inject.Scopes
-import org.openrs2.cache.Cache
-import org.openrs2.cache.Js5MasterIndex
-import org.openrs2.cache.Store
 import org.rsmod.buffer.BufferModule
-import org.rsmod.game.cache.CacheProvider
-import org.rsmod.game.cache.Js5MasterIndexProvider
-import org.rsmod.game.cache.StoreProvider
+import org.rsmod.game.cache.CacheModule
 import org.rsmod.game.config.ConfigModule
 import org.rsmod.game.coroutine.CoroutineModule
 import org.rsmod.game.net.NetworkModule
@@ -18,21 +12,10 @@ public object GameModule : AbstractModule() {
 
     override fun configure() {
         install(BufferModule)
+        install(CacheModule)
         install(ConfigModule)
         install(CoroutineModule)
         install(NetworkModule)
         install(ServiceModule)
-
-        bind(Store::class.java)
-            .toProvider(StoreProvider::class.java)
-            .`in`(Scopes.SINGLETON)
-
-        bind(Js5MasterIndex::class.java)
-            .toProvider(Js5MasterIndexProvider::class.java)
-            .`in`(Scopes.SINGLETON)
-
-        bind(Cache::class.java)
-            .toProvider(CacheProvider::class.java)
-            .`in`(Scopes.SINGLETON)
     }
 }
---
+++ game/src/main/kotlin/org/rsmod/game/cache/CacheModule.kt
@@ -0,0 +1,24 @@
+package org.rsmod.game.cache
+
+import com.google.inject.AbstractModule
+import com.google.inject.Scopes
+import org.openrs2.cache.Cache
+import org.openrs2.cache.Js5MasterIndex
+import org.openrs2.cache.Store
+
+public object CacheModule : AbstractModule() {
+
+    override fun configure() {
+        bind(Store::class.java)
+            .toProvider(StoreProvider::class.java)
+            .`in`(Scopes.SINGLETON)
+
+        bind(Js5MasterIndex::class.java)
+            .toProvider(Js5MasterIndexProvider::class.java)
+            .`in`(Scopes.SINGLETON)
+
+        bind(Cache::class.java)
+            .toProvider(CacheProvider::class.java)
+            .`in`(Scopes.SINGLETON)
+    }
+}
