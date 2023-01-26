acfc8f121a0bb09919ae699e439badded90a273e
Tomm0017 <rspsmods@gmail.com>
2023-01-26 02:11:59 -0500

    Move data-related paths to `GameConfig`

--- game/src/main/kotlin/org/rsmod/game/cache/StoreProvider.kt
+++ game/src/main/kotlin/org/rsmod/game/cache/StoreProvider.kt
@@ -3,7 +3,6 @@ package org.rsmod.game.cache
 import io.netty.buffer.ByteBufAllocator
 import org.openrs2.cache.Store
 import org.rsmod.game.config.GameConfig
-import java.nio.file.Path
 import javax.inject.Inject
 import javax.inject.Provider
 
@@ -12,10 +11,7 @@ public class StoreProvider @Inject constructor(
     private val config: GameConfig
 ) : Provider<Store> {
 
-    private val path: Path
-        get() = config.dataPath.resolve("cache/packed")
-
     override fun get(): Store {
-        return Store.open(path, alloc)
+        return Store.open(config.cachePath, alloc)
     }
 }
--- game/src/main/kotlin/org/rsmod/game/config/GameConfig.kt
+++ game/src/main/kotlin/org/rsmod/game/config/GameConfig.kt
@@ -6,4 +6,11 @@ public data class GameConfig(
     val name: String,
     val world: Int,
     val dataPath: Path
-)
+) {
+
+    val cachePath: Path
+        get() = dataPath.resolve("cache/packed")
+
+    val rsaPath: Path
+        get() = dataPath.resolve("rsa/game.key")
+}
