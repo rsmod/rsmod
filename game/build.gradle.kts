7cdc86556b7ead194124a5b038162a19b3ba4292
Tomm0017 <rspsmods@gmail.com>
2023-01-26 01:57:58 -0500

    Remove unused `game` module deps

--- game/build.gradle.kts
+++ game/build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
 @Suppress("UnstableApiUsage")
 dependencies {
     api(libs.clikt)
-    api(libs.nettyTransport)
-    api(libs.openrs2Buffer)
     api(libs.openrs2Cache)
     implementation(projects.buffer)
     implementation(projects.log)
--- plugins/net/build.gradle.kts
+++ plugins/net/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     api(libs.nettyHandler)
     api(libs.openrs2Crypto)
     api(libs.openrs2Cache)
+    api(libs.openrs2Buffer)
     implementation(libs.kotlinCoroutinesCore)
     implementation(projects.game.protocol)
     implementation(projects.plugins.api)
