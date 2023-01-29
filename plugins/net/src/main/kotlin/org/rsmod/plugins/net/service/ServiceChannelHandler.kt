15ce8fd36ff3acc683066e938a1c7fb11792e925
Tomm0017 <rspsmods@gmail.com>
2023-01-28 19:49:55 -0500

    Naive `ServiceChannelHandler` player codec implementation

--- plugins/net/src/main/kotlin/org/rsmod/plugins/net/service/ServiceChannelHandler.kt
+++ plugins/net/src/main/kotlin/org/rsmod/plugins/net/service/ServiceChannelHandler.kt
@@ -1,5 +1,6 @@
 package org.rsmod.plugins.net.service
 
+import com.github.michaelbull.logging.InlineLogger
 import com.google.common.hash.Hashing
 import io.netty.channel.ChannelFutureListener
 import io.netty.channel.ChannelHandlerContext
@@ -9,14 +10,16 @@ import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import org.openrs2.crypto.IsaacRandom
 import org.openrs2.crypto.secureRandom
 import org.rsmod.game.client.Client
 import org.rsmod.game.client.ClientList
 import org.rsmod.game.events.EventBus
-import org.rsmod.game.model.client.PlayerEntity
-import org.rsmod.game.model.map.Coordinates
-import org.rsmod.game.model.mob.Player
+import org.rsmod.game.model.mob.list.PlayerList
+import org.rsmod.game.store.player.PlayerCodec
+import org.rsmod.game.store.player.PlayerDataRequest
+import org.rsmod.game.store.player.PlayerDataResponse
 import org.rsmod.plugins.api.event.ClientSession
 import org.rsmod.plugins.net.game.client.Platform
 import org.rsmod.plugins.net.game.clientAttr
@@ -42,12 +45,16 @@ import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Provider
 
+private val logger = InlineLogger()
+
 class ServiceChannelHandler @Inject constructor(
-    private val js5HandlerProvider: Provider<Js5ChannelHandler>,
     @Js5RemoteDownstream private val js5RemoteDownstream: Protocol,
     @LoginDownstream private val loginDownstream: Protocol,
+    private val playerCodec: PlayerCodec,
+    private val js5HandlerProvider: Provider<Js5ChannelHandler>,
     private val gamePackets: GamePlatformPacketMaps,
     private val events: EventBus,
+    private val players: PlayerList,
     private val clients: ClientList
 ) : SimpleChannelInboundHandler<ServiceRequest>(ServiceRequest::class.java) {
 
@@ -127,47 +134,72 @@ class ServiceChannelHandler @Inject constructor(
             ctx.write(LoginResponse.ClientProtocolOutOfDate).addListener(ChannelFutureListener.CLOSE)
             return
         }
-        // TODO: add profile/account load request (launched in [scope])
-        // TODO: use load-request response to create player in game-thread dispatcher.
-        val decodeCipher = IsaacRandom(encrypted.xtea.toIntArray())
-        val encodeCipher = IsaacRandom(encrypted.xtea.toIntArray().map { it + 50 }.toIntArray())
-        val accountHash = Hashing.sha256().hashString(username.lowercase(Locale.US), StandardCharsets.UTF_8)
-        val player = Player(
-            PlayerEntity().apply {
-                index = 1
-                coords = Coordinates(3200, 3200)
-            }
-        )
-        val deviceLinkIdentifier = if (encrypted.authType == LoginPacketRequest.AuthType.TwoFactorInputTrustDevice) {
-            // TODO: use player_id or account_id as identifier
-            69
-        } else {
-            null
-        }
-        val response = LoginResponse.ConnectOk(
-            deviceLinkIdentifier = deviceLinkIdentifier,
-            playerModLevel = 2,
-            playerMember = true,
-            playerMod = true,
-            playerIndex = player.index,
-            accountHash = accountHash.asLong(),
-            cipher = encodeCipher
-        )
-        ctx.writeAndFlush(response).addListener { future ->
-            if (!future.isSuccess) return@addListener
-            val decoder = ctx.pipeline().get(ProtocolDecoder::class.java)
-            when (platform) {
-                Platform.Desktop -> {
-                    encoder.protocol = gamePackets.desktopDownstream.getOrCreateProtocol()
-                    decoder.protocol = gamePackets.desktopUpstream.getOrCreateProtocol()
+        scope.launch {
+            // TODO: account dispatcher to hold all this boilerplate
+            val request = PlayerDataRequest(
+                username = username,
+                plaintTextPass = encrypted.password,
+                loginXtea = encrypted.xtea.toIntArray()
+            )
+            when (val deserialize = playerCodec.deserialize(request)) {
+                PlayerDataResponse.InvalidCredentials -> {
+                    // TODO: invalid credentials response
+                    ctx.write(LoginResponse.ClientProtocolOutOfDate)
+                        .addListener(ChannelFutureListener.CLOSE)
+                }
+                is PlayerDataResponse.Exception -> {
+                    logger.error(deserialize.t) { "Exception thrown when deserializing player: $username" }
+                }
+                is PlayerDataResponse.Success -> {
+                    val player = deserialize.player
+                    /* instantly save new players */
+                    if (deserialize is PlayerDataResponse.Success.NewPlayer) {
+                        launch { playerCodec.serialize(player) }
+                    }
+                    val decodeCipher = IsaacRandom(encrypted.xtea.toIntArray())
+                    val encodeCipher = IsaacRandom(encrypted.xtea.toIntArray().map { it + 50 }.toIntArray())
+                    val accountHash = Hashing.sha256().hashString(username.lowercase(Locale.US), StandardCharsets.UTF_8)
+                    // TODO: use load-request response to create player in game-thread dispatcher.
+                    val playerIndex = players.nextAvailableIndex()
+                    if (playerIndex == null) {
+                        // TODO: world full response
+                        ctx.write(LoginResponse.ClientProtocolOutOfDate).addListener(ChannelFutureListener.CLOSE)
+                        return@launch
+                    }
+                    players[playerIndex] = player
+                    player.index = playerIndex
+                    val deviceLinkIdentifier = when (encrypted.authType) {
+                        // TODO: use player_id or account_id as identifier
+                        LoginPacketRequest.AuthType.TwoFactorInputTrustDevice -> 69
+                        else -> null
+                    }
+                    val response = LoginResponse.ConnectOk(
+                        deviceLinkIdentifier = deviceLinkIdentifier,
+                        playerModLevel = 2,
+                        playerMember = true,
+                        playerMod = true,
+                        playerIndex = player.index,
+                        accountHash = accountHash.asLong(),
+                        cipher = encodeCipher
+                    )
+                    ctx.writeAndFlush(response).addListener { future ->
+                        if (!future.isSuccess) return@addListener
+                        val decoder = ctx.pipeline().get(ProtocolDecoder::class.java)
+                        when (platform) {
+                            Platform.Desktop -> {
+                                encoder.protocol = gamePackets.desktopDownstream.getOrCreateProtocol()
+                                decoder.protocol = gamePackets.desktopUpstream.getOrCreateProtocol()
+                            }
+                        }
+                        val client = Client(player, ctx.channel())
+                        ctx.channel().setClientAttr(client)
+                        encoder.cipher = encodeCipher
+                        decoder.cipher = decodeCipher
+                        clients += client
+                        events += ClientSession.Connect(client)
+                    }
                 }
             }
-            val client = Client(player, ctx.channel())
-            ctx.channel().setClientAttr(client)
-            encoder.cipher = encodeCipher
-            decoder.cipher = decodeCipher
-            clients += client
-            events += ClientSession.Connect(client)
         }
         return@with
     }
