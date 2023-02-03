cb51487a7a706ee3af41be338ff9d65104f6e713
Tomm0017 <rspsmods@gmail.com>
2023-02-03 03:18:17 -0500

    Add `NamedInterface::child` helper function

--- game/types/src/main/kotlin/org/rsmod/game/types/NamedInterface.kt
+++ game/types/src/main/kotlin/org/rsmod/game/types/NamedInterface.kt
@@ -1,4 +1,10 @@
 package org.rsmod.game.types
 
 @JvmInline
-public value class NamedInterface(public val id: Int)
+public value class NamedInterface(public val id: Int) {
+
+    @Suppress("NOTHING_TO_INLINE")
+    public inline fun child(child: Int): NamedComponent {
+        return NamedComponent(id, child)
+    }
+}
