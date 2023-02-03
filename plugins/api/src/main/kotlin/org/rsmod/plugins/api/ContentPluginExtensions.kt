81d4cd079baca6eb3a53c47e978fe1b1d7c277f6
Tomm0017 <rspsmods@gmail.com>
2023-02-03 12:57:34 -0500

    Remove `ContentPlugin.enums`
    Though the idea of having an easier way to access the values is nice - it is fragile and can be abused easily. `Injector::getInstance` should be avoided at runtime as much as possible. If plugins call this property within methods that will happen at game runtime, then we may get performance hits.

--- plugins/api/src/main/kotlin/org/rsmod/plugins/api/ContentPluginExtensions.kt
+++
@@ -1,7 +0,0 @@
-package org.rsmod.plugins.api
-
-import org.rsmod.game.plugins.content.ContentPlugin
-import org.rsmod.plugins.api.cache.type.enums.EnumTypeList
-
-public val ContentPlugin.enums: EnumTypeList
-    get() = injector.getInstance(EnumTypeList::class.java)
