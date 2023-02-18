e1dc7231f6d6af22d6425e07492af79b39727f93
Tomm0017 <rspsmods@gmail.com>
2023-02-17 21:55:16 -0500

    Add `info` module publish support

--- plugins/info/build.gradle.kts
+++ plugins/info/build.gradle.kts
@@ -1,10 +1,32 @@
 version = "1.0.0"
 
 plugins {
+    `maven-publish`
+    signing
     kotlin("jvm")
     id("me.champeau.gradle.jmh") apply true
 }
 
 jmh {
     profilers = listOf("stack")
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+        pom {
+            packaging = "jar"
+            name.set("RS Mod Avatar-Info")
+            description.set(
+                """
+                A plugin to handle avatar world information and convert into a format
+                the client can comprehend.
+                """.trimIndent()
+            )
+        }
+        signing {
+            useGpgCmd()
+            sign(publishing.publications["maven"])
+        }
+    }
 }
