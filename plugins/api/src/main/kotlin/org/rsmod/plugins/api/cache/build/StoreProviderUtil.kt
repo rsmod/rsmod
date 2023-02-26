e618533141e130e1ebf6ac724e849c44ebc9b16e
Tomm0017 <rspsmods@gmail.com>
2023-02-26 13:28:55 -0500

    Mark util class as internal

--- plugins/api/src/main/kotlin/org/rsmod/plugins/api/cache/build/StoreProviderUtil.kt
+++ plugins/api/src/main/kotlin/org/rsmod/plugins/api/cache/build/StoreProviderUtil.kt
@@ -2,9 +2,9 @@ package org.rsmod.plugins.api.cache.build
 
 import java.io.File
 
-public object StoreProviderUtil {
+internal object StoreProviderUtil {
 
-    public fun createOrCopyStore(createPath: File, copyPath: File) {
+    fun createOrCopyStore(createPath: File, copyPath: File) {
         if (createPath.exists() && createPath.walk().count() > 1) return
         createPath.mkdirs()
         if (copyPath.exists()) {
