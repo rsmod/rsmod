92f360436d50b197907cf7c38a13266c455a756f
Tomm0017 <rspsmods@gmail.com>
2023-02-26 15:46:36 -0500

    Add item type codec test

--- plugins/api/src/main/kotlin/org/rsmod/plugins/api/cache/type/item/ItemTypePacker.kt
+++ plugins/api/src/main/kotlin/org/rsmod/plugins/api/cache/type/item/ItemTypePacker.kt
@@ -22,7 +22,7 @@ public object ItemTypePacker {
         val buf = Unpooled.buffer()
         val packed = mutableListOf<ItemType>()
         types.forEach { type ->
-            buf.clear().writeType(type, params, isJs5)
+            writeType(buf.clear(), type, params, isJs5)
             val oldData = if (cache.exists(CONFIG_ARCHIVE, ITEM_GROUP, type.id)) {
                 cache.read(CONFIG_ARCHIVE, ITEM_GROUP, type.id)
             } else {
@@ -35,210 +35,210 @@ public object ItemTypePacker {
         return packed
     }
 
-    private fun ByteBuf.writeType(type: ItemType, params: ParamTypeList, isJs5: Boolean) {
+    public fun writeType(buf: ByteBuf, type: ItemType, params: ParamTypeList, isJs5: Boolean) {
         if (type.model != 0) {
-            writeByte(1)
-            writeShort(type.model)
+            buf.writeByte(1)
+            buf.writeShort(type.model)
         }
         if (type.name != "null") {
-            writeByte(2)
-            writeString(type.name)
+            buf.writeByte(2)
+            buf.writeString(type.name)
         }
         if (type.zoom2d != 2000) {
-            writeByte(4)
-            writeShort(type.zoom2d)
+            buf.writeByte(4)
+            buf.writeShort(type.zoom2d)
         }
         if (type.xan2d != 0) {
-            writeByte(5)
-            writeShort(type.xan2d)
+            buf.writeByte(5)
+            buf.writeShort(type.xan2d)
         }
         if (type.yan2d != 0) {
-            writeByte(6)
-            writeShort(type.yan2d)
+            buf.writeByte(6)
+            buf.writeShort(type.yan2d)
         }
         if (type.xoff2d != 0) {
-            writeByte(7)
-            writeShort(type.xoff2d)
+            buf.writeByte(7)
+            buf.writeShort(type.xoff2d)
         }
         if (type.yoff2d != 0) {
-            writeByte(8)
-            writeShort(type.yoff2d)
+            buf.writeByte(8)
+            buf.writeShort(type.yoff2d)
         }
         if (type.stacks) {
-            writeByte(11)
+            buf.writeByte(11)
         }
         if (type.cost != 1) {
-            writeByte(12)
-            writeInt(type.cost)
+            buf.writeByte(12)
+            buf.writeInt(type.cost)
         }
         if (type.wearPos1 != -1) {
-            writeByte(13)
-            writeByte(type.wearPos1)
+            buf.writeByte(13)
+            buf.writeByte(type.wearPos1)
         }
         if (type.wearPos2 != -1) {
-            writeByte(14)
-            writeByte(type.wearPos2)
+            buf.writeByte(14)
+            buf.writeByte(type.wearPos2)
         }
         if (type.members) {
-            writeByte(16)
+            buf.writeByte(16)
         }
         if (type.maleModel0 != 0) {
-            writeByte(23)
-            writeShort(type.maleModel0)
-            writeByte(type.maleModelOffset)
+            buf.writeByte(23)
+            buf.writeShort(type.maleModel0)
+            buf.writeByte(type.maleModelOffset)
         }
         if (type.maleModel1 != 0) {
-            writeByte(24)
-            writeShort(type.maleModel1)
+            buf.writeByte(24)
+            buf.writeShort(type.maleModel1)
         }
         if (type.femaleModel0 != 0) {
-            writeByte(25)
-            writeShort(type.femaleModel0)
-            writeByte(type.femaleModelOffset)
+            buf.writeByte(25)
+            buf.writeShort(type.femaleModel0)
+            buf.writeByte(type.femaleModelOffset)
         }
         if (type.femaleModel1 != 0) {
-            writeByte(26)
-            writeShort(type.femaleModel1)
+            buf.writeByte(26)
+            buf.writeShort(type.femaleModel1)
         }
         if (type.wearPos3 != -1) {
-            writeByte(27)
-            writeByte(type.wearPos3)
+            buf.writeByte(27)
+            buf.writeByte(type.wearPos3)
         }
         type.groundOptions.forEachIndexed { i, str ->
             if (str != null && str != "Hidden" && str != "Take") {
-                writeByte(30 + i)
-                writeString(str)
+                buf.writeByte(30 + i)
+                buf.writeString(str)
             }
         }
         type.inventoryOptions.forEachIndexed { i, str ->
             if (str != null && str != "Hidden" && str != "Drop") {
-                writeByte(35 + i)
-                writeString(str)
+                buf.writeByte(35 + i)
+                buf.writeString(str)
             }
         }
         if (type.recolorSrc.isNotEmpty()) {
-            writeByte(40)
-            writeByte(type.recolorSrc.size)
+            buf.writeByte(40)
+            buf.writeByte(type.recolorSrc.size)
             for (i in type.recolorSrc.indices) {
-                writeShort(type.recolorSrc[i])
-                writeShort(type.recolorDest[i])
+                buf.writeShort(type.recolorSrc[i])
+                buf.writeShort(type.recolorDest[i])
             }
         }
         if (type.retextureSrc.isNotEmpty()) {
-            writeByte(41)
-            writeByte(type.retextureSrc.size)
+            buf.writeByte(41)
+            buf.writeByte(type.retextureSrc.size)
             for (i in type.retextureSrc.indices) {
-                writeShort(type.retextureSrc[i])
-                writeShort(type.retextureDest[i])
+                buf.writeShort(type.retextureSrc[i])
+                buf.writeShort(type.retextureDest[i])
             }
         }
         if (type.dropOptionIndex != -2) {
-            writeByte(42)
-            writeByte(type.dropOptionIndex)
+            buf.writeByte(42)
+            buf.writeByte(type.dropOptionIndex)
         }
         if (type.exchangeable) {
-            writeByte(65)
+            buf.writeByte(65)
         }
         if (type.weight != 0) {
-            writeByte(75)
-            writeShort(type.weight)
+            buf.writeByte(75)
+            buf.writeShort(type.weight)
         }
         if (type.maleModel2 != 0) {
-            writeByte(78)
-            writeShort(type.maleModel2)
+            buf.writeByte(78)
+            buf.writeShort(type.maleModel2)
         }
         if (type.femaleModel2 != 0) {
-            writeByte(79)
-            writeShort(type.femaleModel2)
+            buf.writeByte(79)
+            buf.writeShort(type.femaleModel2)
         }
         if (type.maleHeadModel0 != 0) {
-            writeByte(90)
-            writeShort(type.maleHeadModel0)
+            buf.writeByte(90)
+            buf.writeShort(type.maleHeadModel0)
         }
         if (type.femaleHeadModel0 != 0) {
-            writeByte(91)
-            writeShort(type.femaleHeadModel0)
+            buf.writeByte(91)
+            buf.writeShort(type.femaleHeadModel0)
         }
         if (type.maleHeadModel1 != 0) {
-            writeByte(92)
-            writeShort(type.maleHeadModel1)
+            buf.writeByte(92)
+            buf.writeShort(type.maleHeadModel1)
         }
         if (type.femaleHeadModel1 != 0) {
-            writeByte(93)
-            writeShort(type.femaleHeadModel1)
+            buf.writeByte(93)
+            buf.writeShort(type.femaleHeadModel1)
         }
         type.categories.forEach { category ->
-            writeByte(94)
-            writeShort(category)
+            buf.writeByte(94)
+            buf.writeShort(category)
         }
         if (type.zan2d != 0) {
-            writeByte(95)
-            writeShort(type.zan2d)
+            buf.writeByte(95)
+            buf.writeShort(type.zan2d)
         }
         if (type.noteLink != 0) {
-            writeByte(97)
-            writeShort(type.noteLink)
+            buf.writeByte(97)
+            buf.writeShort(type.noteLink)
         }
         if (type.noteModel != 0) {
-            writeByte(98)
-            writeShort(type.noteModel)
+            buf.writeByte(98)
+            buf.writeShort(type.noteModel)
         }
         type.countItem.forEachIndexed { i, obj ->
             val countCo = type.countCo[i]
-            writeByte(100 + i)
-            writeShort(obj)
-            writeShort(countCo)
+            buf.writeByte(100 + i)
+            buf.writeShort(obj)
+            buf.writeShort(countCo)
         }
         if (type.resizeX != 128) {
-            writeByte(110)
-            writeShort(type.resizeX)
+            buf.writeByte(110)
+            buf.writeShort(type.resizeX)
         }
         if (type.resizeY != 128) {
-            writeByte(111)
-            writeShort(type.resizeY)
+            buf.writeByte(111)
+            buf.writeShort(type.resizeY)
         }
         if (type.resizeZ != 128) {
-            writeByte(112)
-            writeShort(type.resizeZ)
+            buf.writeByte(112)
+            buf.writeShort(type.resizeZ)
         }
         if (type.ambient != 0) {
-            writeByte(113)
-            writeByte(type.ambient)
+            buf.writeByte(113)
+            buf.writeByte(type.ambient)
         }
         if (type.contrast != 0) {
-            writeByte(114)
-            writeByte(type.contrast)
+            buf.writeByte(114)
+            buf.writeByte(type.contrast)
         }
         if (type.team != 0) {
-            writeByte(115)
-            writeByte(type.team)
+            buf.writeByte(115)
+            buf.writeByte(type.team)
         }
         if (type.boughtLink != 0) {
-            writeByte(139)
-            writeShort(type.boughtLink)
+            buf.writeByte(139)
+            buf.writeShort(type.boughtLink)
         }
         if (type.boughtValue != 0) {
-            writeByte(140)
-            writeShort(type.boughtValue)
+            buf.writeByte(140)
+            buf.writeShort(type.boughtValue)
         }
         if (type.placeholderLink != 0) {
-            writeByte(148)
-            writeShort(type.placeholderLink)
+            buf.writeByte(148)
+            buf.writeShort(type.placeholderLink)
         }
         if (type.placeholderModel != 0) {
-            writeByte(149)
-            writeShort(type.placeholderModel)
+            buf.writeByte(149)
+            buf.writeShort(type.placeholderModel)
         }
         if (type.params != null) {
-            writeByte(249)
-            writeParams(type.params, params)
+            buf.writeByte(249)
+            buf.writeParams(type.params, params)
         }
         if (!isJs5) {
             type.internalName?.let {
-                writeByte(ConfigType.INTERNAL_NAME_OPCODE)
-                writeString(it)
+                buf.writeByte(ConfigType.INTERNAL_NAME_OPCODE)
+                buf.writeString(it)
             }
         }
-        writeByte(0)
+        buf.writeByte(0)
     }
 }
