6eb0ed2aaae24178f99c2aef8c89de862b08c098
Tomm0017 <rspsmods@gmail.com>
2023-02-27 06:06:11 -0500

    Refactor StandaloneModule injection

--- game/src/main/kotlin/org/rsmod/game/GameCommand.kt
+++ game/src/main/kotlin/org/rsmod/game/GameCommand.kt
@@ -4,23 +4,22 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.michaelbull.logging.InlineLogger
 import com.google.inject.AbstractModule
 import com.google.inject.Guice
-import com.google.inject.Provides
 
 public fun main(args: Array<String>): Unit = GameCommand().main(args)
 
 public class GameCommand : CliktCommand(name = "game") {
 
     override fun run() {
-        val injector = Guice.createInjector(GameModule, StandaloneModule)
+        val injector = Guice.createInjector(GameStandaloneModule)
         val bootstrap = injector.getInstance(GameBootstrap::class.java)
         bootstrap.startUp()
     }
 
-    private object StandaloneModule : AbstractModule() {
+    private object GameStandaloneModule : AbstractModule() {
 
-        @Provides
-        fun provideGameProcess(): GameProcess {
-            return process
+        override fun configure() {
+            install(GameModule)
+            bind(GameProcess::class.java).toInstance(process)
         }
 
         private val process = object : GameProcess {
