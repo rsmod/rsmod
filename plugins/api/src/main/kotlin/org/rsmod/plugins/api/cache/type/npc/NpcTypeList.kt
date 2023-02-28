763c49959cb5c88f9627b326dc45addb3e339582
Tomm0017 <rspsmods@gmail.com>
2023-02-28 06:52:36 -0500

    Fix lint error

--- plugins/api/src/main/kotlin/org/rsmod/plugins/api/cache/type/npc/NpcTypeList.kt
+++ plugins/api/src/main/kotlin/org/rsmod/plugins/api/cache/type/npc/NpcTypeList.kt
@@ -1,8 +1,6 @@
 package org.rsmod.plugins.api.cache.type.npc
 
-import org.rsmod.plugins.api.cache.type.varp.VarpType
 import org.rsmod.plugins.types.NamedNpc
-import org.rsmod.plugins.types.NamedVarp
 
 public class NpcTypeList(private val elements: Map<Int, NpcType>) : Map<Int, NpcType> by elements {
 
