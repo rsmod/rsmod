577980a8bfe25d78f2b7b3bfac6a9a191ed914e2
Tomm0017 <rspsmods@gmail.com>
2023-02-28 11:02:03 -0500

    Add login plugin

--- plugins/playground/login/src/main/kotlin/org/rsmod/plugins/playground/login/gameframe.plugin.kts
+++ plugins/playground/login/src/main/kotlin/org/rsmod/plugins/playground/login/gameframe.plugin.kts
@@ -2,19 +2,13 @@ package org.rsmod.plugins.playground.login
 
 import org.rsmod.game.events.GameEventBus
 import org.rsmod.game.events.subscribe
-import org.rsmod.plugins.api.cache.type.varbit.VarbitTypeList
-import org.rsmod.plugins.api.chatbox_unlocked
 import org.rsmod.plugins.api.model.event.PlayerSession
 import org.rsmod.plugins.api.openGameframe
-import org.rsmod.plugins.api.setVarbit
-import org.rsmod.plugins.api.varbit
 import org.rsmod.plugins.content.gameframe.GameframePlugin
 
 private val gameframe: GameframePlugin by inject()
-private val varbits: VarbitTypeList by inject()
 private val events: GameEventBus by inject()
 
 events.subscribe<PlayerSession.Initialize> {
     player.openGameframe(gameframe.fixed)
-    player.setVarbit(true, varbits[varbit.chatbox_unlocked])
 }
---
+++ plugins/playground/login/src/main/kotlin/org/rsmod/plugins/playground/login/login.plugin.kts
@@ -0,0 +1,25 @@
+package org.rsmod.plugins.playground.login
+
+import org.rsmod.game.events.GameEventBus
+import org.rsmod.game.events.subscribe
+import org.rsmod.plugins.api.cache.type.varbit.VarbitTypeList
+import org.rsmod.plugins.api.chatbox_unlocked
+import org.rsmod.plugins.api.clientScript
+import org.rsmod.plugins.api.message
+import org.rsmod.plugins.api.model.event.PlayerSession
+import org.rsmod.plugins.api.setVarbit
+import org.rsmod.plugins.api.varbit
+
+private val varbits: VarbitTypeList by inject()
+private val events: GameEventBus by inject()
+
+events.subscribe<PlayerSession.LogIn> {
+    player.setVarbit(true, varbits[varbit.chatbox_unlocked])
+    player.clientScript(2498, 1, 0, 0)
+    player.clientScript(72, 26148868, 26148869, 0)
+    player.clientScript(2358, 0)
+    player.clientScript(385, 1)
+    player.clientScript(828, 1)
+    player.clientScript(876, 28404, 0, player.displayName, "REGULAR")
+    player.message("Welcome to RS Mod.")
+}
