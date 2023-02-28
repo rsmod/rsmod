adbb56b508f3d79daa8c6bf7f7369299fb13bf44
Tomm0017 <rspsmods@gmail.com>
2023-02-28 09:46:19 -0500

    Bind `GameframeFixed`
    Mainly for convention and "showing-intent" purposes

--- plugins/content/gameframe/src/main/kotlin/org/rsmod/plugins/content/gameframe/GameframeModule.kt
+++ plugins/content/gameframe/src/main/kotlin/org/rsmod/plugins/content/gameframe/GameframeModule.kt
@@ -2,11 +2,13 @@ package org.rsmod.plugins.content.gameframe
 
 import com.google.inject.AbstractModule
 import com.google.inject.Scopes
+import org.rsmod.plugins.content.gameframe.build.GameframeFixed
 import org.rsmod.plugins.content.gameframe.build.GameframeResizeList
 
 public object GameframeModule : AbstractModule() {
 
     override fun configure() {
+        bind(GameframeFixed::class.java).`in`(Scopes.SINGLETON)
         bind(GameframeResizeList::class.java).`in`(Scopes.SINGLETON)
     }
 }
