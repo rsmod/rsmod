e544f3fcea63e319c217e3f7a63e1c12e0ca8d6d
Tomm0017 <rspsmods@gmail.com>
2023-03-22 12:35:50 -0400

    Change `keyed` collection type

--- game/src/main/kotlin/org/rsmod/game/model/EventList.kt
+++ game/src/main/kotlin/org/rsmod/game/model/EventList.kt
@@ -4,10 +4,10 @@ import org.rsmod.game.events.Event
 import org.rsmod.game.events.EventBus
 import org.rsmod.game.events.KeyedEvent
 
-public class EventList<T>(
-    private val events: MutableList<Event<T>> = mutableListOf(),
-    private val keyed: MutableMap<Long, KeyedEvent<T>> = mutableMapOf()
-) {
+public class EventList<T> {
+
+    private val events: MutableList<Event<T>> = mutableListOf()
+    private val keyed: MutableList<CachedKeyedEvent<T>> = mutableListOf()
 
     public fun publishAll(parameter: T, eventBus: EventBus) {
         events.forEach { event -> eventBus.publish(parameter, event) }
@@ -19,11 +19,20 @@ public class EventList<T>(
         keyed.clear()
     }
 
+    public fun add(id: Long, event: KeyedEvent<T>) {
+        keyed += CachedKeyedEvent(id, event)
+    }
+
     public operator fun plusAssign(event: Event<T>) {
         events += event
     }
 
-    public operator fun set(id: Long, event: KeyedEvent<T>) {
-        keyed[id] = event
-    }
+    public fun getUnbound(): List<Event<T>> = events
+
+    public fun getKeyed(): List<CachedKeyedEvent<T>> = keyed
+
+    public data class CachedKeyedEvent<T>(
+        public val id: Long,
+        public val event: KeyedEvent<T>
+    )
 }
