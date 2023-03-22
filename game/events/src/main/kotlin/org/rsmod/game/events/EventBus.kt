68dcda066fd45a6fa8c52794de4abc52acf052a7
Tomm0017 <rspsmods@gmail.com>
2023-03-22 14:17:45 -0400

    Change `EventBus::getOrNull` to `get` operator

--- game/events/src/main/kotlin/org/rsmod/game/events/EventBus.kt
+++ game/events/src/main/kotlin/org/rsmod/game/events/EventBus.kt
@@ -26,28 +26,28 @@ public class EventBus {
     }
 
     public fun <T : Event<Unit>> publish(event: T) {
-        getOrNull(event::class.java)?.forEach { it.invoke(Unit, event) }
+        get(event::class.java)?.forEach { it.invoke(Unit, event) }
     }
 
     public fun <K, T : Event<K>> publish(parameter: K, event: T) {
-        getOrNull(event::class.java)?.forEach { it.invoke(parameter, event) }
+        get(event::class.java)?.forEach { it.invoke(parameter, event) }
     }
 
     public fun <K, T : KeyedEvent<K>> publish(id: Long, parameter: K, event: T) {
-        getOrNull(event::class.java)?.get(id)?.invoke(parameter, event)
+        get(event::class.java)?.get(id)?.invoke(parameter, event)
     }
-
-    public operator fun <T : Event<*>> contains(type: Class<T>): Boolean =
-        unboundEvents.containsKey(type)
 
     public fun <T : KeyedEvent<*>> contains(id: Long, type: Class<T>): Boolean =
         keyedEvents[type]?.containsKey(id) ?: false
 
+    public operator fun <T : Event<*>> contains(type: Class<T>): Boolean =
+        unboundEvents.containsKey(type)
+
     @Suppress("UNCHECKED_CAST")
-    public fun <K, T : Event<K>> getOrNull(type: Class<out T>): List<K.(T) -> Unit>? =
+    public operator fun <K, T : Event<K>> get(type: Class<out T>): List<K.(T) -> Unit>? =
         unboundEvents[type] as? List<K.(T) -> Unit>
 
     @Suppress("UNCHECKED_CAST")
-    public fun <K, T : KeyedEvent<K>> getOrNull(type: Class<out T>): Map<Long, K.(T) -> Unit>? =
+    public operator fun <K, T : KeyedEvent<K>> get(type: Class<out T>): Map<Long, K.(T) -> Unit>? =
         keyedEvents[type] as? Map<Long, K.(T) -> Unit>
 }
