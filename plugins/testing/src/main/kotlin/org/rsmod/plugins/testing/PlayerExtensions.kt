57dcf8279ca5912f7494156127eb5f248b42de6b
Tomm0017 <rspsmods@gmail.com>
2023-04-04 20:02:59 -0400

    Remove explicit assertions class scope

--- plugins/testing/src/main/kotlin/org/rsmod/plugins/testing/PlayerExtensions.kt
+++ plugins/testing/src/main/kotlin/org/rsmod/plugins/testing/PlayerExtensions.kt
@@ -1,14 +1,15 @@
 package org.rsmod.plugins.testing
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.rsmod.game.model.mob.Player
 import org.rsmod.game.model.mob.info.ExtendedInfo
 
 public inline fun <reified T : ExtendedInfo> Player.verify(predicate: (T) -> Boolean) {
     val found = extendedInfo.pendingInfo[T::class.java] as? T
-    Assertions.assertNotNull(found) { "`${T::class.simpleName}` not found in extended-info pending map." }
-    Assertions.assertTrue(found?.let { predicate(it) } ?: false)
+    assertNotNull(found) { "`${T::class.simpleName}` not found in extended-info pending map." }
+    assertTrue(found?.let { predicate(it) } ?: false)
 }
 
 public inline fun <reified T : ExtendedInfo> Player.verifyNull() {
