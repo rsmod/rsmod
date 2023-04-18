23338751be56f986c0e395e82402e5135a7551f4
Tomm0017 <rspsmods@gmail.com>
2023-04-18 00:24:15 -0400

    Mark Route as data class

--- game/pathfinder/src/main/kotlin/org/rsmod/game/pathfinder/Route.kt
+++ game/pathfinder/src/main/kotlin/org/rsmod/game/pathfinder/Route.kt
@@ -1,16 +1,12 @@
 package org.rsmod.game.pathfinder
 
-public class Route(
+public data class Route(
     public val waypoints: List<RouteCoordinates>,
     public val alternative: Boolean,
     public val success: Boolean
 ) : List<RouteCoordinates> by waypoints {
 
     public val failed: Boolean get() = !success
-
-    override fun toString(): String {
-        return "Route(success=$success, alternative=$alternative, waypoints=$waypoints)"
-    }
 
     public companion object {
 
