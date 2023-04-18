4372e72f721a7d3755421b2147fd29997a950e47
Tomm0017 <rspsmods@gmail.com>
2023-04-18 08:07:14 -0400

    Clarify `REBUILD_BOUNDARY` value

--- plugins/api/src/main/kotlin/org/rsmod/plugins/api/util/BuildAreaUtils.kt
+++ plugins/api/src/main/kotlin/org/rsmod/plugins/api/util/BuildAreaUtils.kt
@@ -1,10 +1,14 @@
 package org.rsmod.plugins.api.util
 
+import org.rsmod.game.map.zone.Zone
+
+@Suppress("MemberVisibilityCanBePrivate")
 public object BuildAreaUtils {
 
     public const val SIZE: Int = 104
 
-    public const val REBUILD_BOUNDARY: Int = 16
+    // Rebuilds when you step into 2-zone boundary
+    public const val REBUILD_BOUNDARY: Int = Zone.SIZE * 2
 
     public const val ZONE_VIEW_RADIUS: Int = SIZE / REBUILD_BOUNDARY
 }
