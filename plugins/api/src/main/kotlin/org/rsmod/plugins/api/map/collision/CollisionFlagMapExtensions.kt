0dba0e4d60ec24f5c38acc865dcec703a3f1cedd
Tomm0017 <rspsmods@gmail.com>
2023-04-18 01:59:40 -0400

    Rename toggle functions
    "Toggle" makes more sense than "change" in this scenario.

--- plugins/api/src/main/kotlin/org/rsmod/plugins/api/map/collision/CollisionFlagMapExtensions.kt
+++ plugins/api/src/main/kotlin/org/rsmod/plugins/api/map/collision/CollisionFlagMapExtensions.kt
@@ -12,14 +12,14 @@ public fun CollisionFlagMap.get(coords: Coordinates): Int {
 }
 
 public fun CollisionFlagMap.addObject(obj: GameObject) {
-    changeObject(obj, add = true)
+    toggleObject(obj, add = true)
 }
 
 public fun CollisionFlagMap.removeObject(obj: GameObject) {
-    changeObject(obj, add = false)
+    toggleObject(obj, add = false)
 }
 
-private fun CollisionFlagMap.changeObject(obj: GameObject, add: Boolean) {
+private fun CollisionFlagMap.toggleObject(obj: GameObject, add: Boolean) {
     val shape = obj.shape() ?: error("Could not convert object shape. (id=${obj.shape})")
     val slot = obj.slot()
     val type = obj.type
@@ -30,7 +30,7 @@ private fun CollisionFlagMap.changeObject(obj: GameObject, add: Boolean) {
     val blockProjectile = type.blockProjectile
 
     if (slot == ObjectSlot.Wall && clipType != 0) {
-        changeWall(coords, rotation, shape, blockProjectile, add)
+        toggleWall(coords, rotation, shape, blockProjectile, add)
     } else if (slot == ObjectSlot.Main && clipType != 0) {
         var width = type.width
         var length = type.height
@@ -38,13 +38,13 @@ private fun CollisionFlagMap.changeObject(obj: GameObject, add: Boolean) {
             width = type.height
             length = type.width
         }
-        changeNormal(coords, width, length, blockPath, blockProjectile, add)
+        toggleMain(coords, width, length, blockPath, blockProjectile, add)
     } else if (slot == ObjectSlot.GroundDetail && clipType == 1) {
-        changeFloorDecor(coords, add)
+        toggleGroundDetail(coords, add)
     }
 }
 
-private fun CollisionFlagMap.changeNormal(
+private fun CollisionFlagMap.toggleMain(
     coords: Coordinates,
     width: Int,
     length: Int,
@@ -55,19 +55,19 @@ private fun CollisionFlagMap.changeNormal(
     for (x in 0 until width) {
         for (z in 0 until length) {
             val translate = coords.translate(x, z)
-            change(translate, CollisionFlag.OBJECT, add)
+            toggle(translate, CollisionFlag.OBJECT, add)
             if (blockProjectile) {
-                change(translate, CollisionFlag.OBJECT_PROJECTILE_BLOCKER, add)
+                toggle(translate, CollisionFlag.OBJECT_PROJECTILE_BLOCKER, add)
             }
             if (blockPath) {
-                change(translate, CollisionFlag.OBJECT_ROUTE_BLOCKER, add)
+                toggle(translate, CollisionFlag.OBJECT_ROUTE_BLOCKER, add)
             }
         }
     }
 }
 
 @Suppress("CascadeIf")
-private fun CollisionFlagMap.changeWall(
+private fun CollisionFlagMap.toggleWall(
     coords: Coordinates,
     rotation: Int,
     shape: ObjectShape,
@@ -77,132 +77,132 @@ private fun CollisionFlagMap.changeWall(
     if (shape == ObjectShape.WallStraight) {
         when (rotation) {
             0 -> {
-                change(coords, CollisionFlag.WALL_WEST, add)
-                change(coords.translate(-1, 0), CollisionFlag.WALL_EAST, add)
+                toggle(coords, CollisionFlag.WALL_WEST, add)
+                toggle(coords.translate(-1, 0), CollisionFlag.WALL_EAST, add)
                 if (blockProjectile) {
-                    change(coords, CollisionFlag.WALL_WEST_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(-1, 0), CollisionFlag.WALL_EAST_PROJECTILE_BLOCKER, add)
+                    toggle(coords, CollisionFlag.WALL_WEST_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(-1, 0), CollisionFlag.WALL_EAST_PROJECTILE_BLOCKER, add)
                 }
             }
             1 -> {
-                change(coords, CollisionFlag.WALL_NORTH, add)
-                change(coords.translate(0, 1), CollisionFlag.WALL_SOUTH, add)
+                toggle(coords, CollisionFlag.WALL_NORTH, add)
+                toggle(coords.translate(0, 1), CollisionFlag.WALL_SOUTH, add)
                 if (blockProjectile) {
-                    change(coords, CollisionFlag.WALL_NORTH_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(0, 1), CollisionFlag.WALL_SOUTH_PROJECTILE_BLOCKER, add)
+                    toggle(coords, CollisionFlag.WALL_NORTH_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(0, 1), CollisionFlag.WALL_SOUTH_PROJECTILE_BLOCKER, add)
                 }
             }
             2 -> {
-                change(coords, CollisionFlag.WALL_EAST, add)
-                change(coords.translate(1, 0), CollisionFlag.WALL_WEST, add)
+                toggle(coords, CollisionFlag.WALL_EAST, add)
+                toggle(coords.translate(1, 0), CollisionFlag.WALL_WEST, add)
                 if (blockProjectile) {
-                    change(coords, CollisionFlag.WALL_EAST_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(1, 0), CollisionFlag.WALL_WEST_PROJECTILE_BLOCKER, add)
+                    toggle(coords, CollisionFlag.WALL_EAST_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(1, 0), CollisionFlag.WALL_WEST_PROJECTILE_BLOCKER, add)
                 }
             }
             3 -> {
-                change(coords, CollisionFlag.WALL_SOUTH, add)
-                change(coords.translate(0, -1), CollisionFlag.WALL_NORTH, add)
+                toggle(coords, CollisionFlag.WALL_SOUTH, add)
+                toggle(coords.translate(0, -1), CollisionFlag.WALL_NORTH, add)
                 if (blockProjectile) {
-                    change(coords, CollisionFlag.WALL_SOUTH_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(0, -1), CollisionFlag.WALL_NORTH_PROJECTILE_BLOCKER, add)
+                    toggle(coords, CollisionFlag.WALL_SOUTH_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(0, -1), CollisionFlag.WALL_NORTH_PROJECTILE_BLOCKER, add)
                 }
             }
         }
     } else if (shape == ObjectShape.WallDiagonalCorner || shape == ObjectShape.WallSquareCorner) {
         when (rotation) {
             0 -> {
-                change(coords, CollisionFlag.WALL_NORTH_WEST, add)
-                change(coords.translate(-1, 1), CollisionFlag.WALL_SOUTH_EAST, add)
+                toggle(coords, CollisionFlag.WALL_NORTH_WEST, add)
+                toggle(coords.translate(-1, 1), CollisionFlag.WALL_SOUTH_EAST, add)
                 if (blockProjectile) {
-                    change(coords, CollisionFlag.WALL_NORTH_WEST_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(-1, 1), CollisionFlag.WALL_SOUTH_EAST_PROJECTILE_BLOCKER, add)
+                    toggle(coords, CollisionFlag.WALL_NORTH_WEST_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(-1, 1), CollisionFlag.WALL_SOUTH_EAST_PROJECTILE_BLOCKER, add)
                 }
             }
             1 -> {
-                change(coords, CollisionFlag.WALL_NORTH_EAST, add)
-                change(coords.translate(1, 1), CollisionFlag.WALL_SOUTH_WEST, add)
+                toggle(coords, CollisionFlag.WALL_NORTH_EAST, add)
+                toggle(coords.translate(1, 1), CollisionFlag.WALL_SOUTH_WEST, add)
                 if (blockProjectile) {
-                    change(coords, CollisionFlag.WALL_NORTH_EAST_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(1, 1), CollisionFlag.WALL_SOUTH_WEST_PROJECTILE_BLOCKER, add)
+                    toggle(coords, CollisionFlag.WALL_NORTH_EAST_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(1, 1), CollisionFlag.WALL_SOUTH_WEST_PROJECTILE_BLOCKER, add)
                 }
             }
             2 -> {
-                change(coords, CollisionFlag.WALL_SOUTH_EAST, add)
-                change(coords.translate(1, -1), CollisionFlag.WALL_NORTH_WEST, add)
+                toggle(coords, CollisionFlag.WALL_SOUTH_EAST, add)
+                toggle(coords.translate(1, -1), CollisionFlag.WALL_NORTH_WEST, add)
                 if (blockProjectile) {
-                    change(coords, CollisionFlag.WALL_SOUTH_EAST_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(1, -1), CollisionFlag.WALL_NORTH_WEST_PROJECTILE_BLOCKER, add)
+                    toggle(coords, CollisionFlag.WALL_SOUTH_EAST_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(1, -1), CollisionFlag.WALL_NORTH_WEST_PROJECTILE_BLOCKER, add)
                 }
             }
             3 -> {
-                change(coords, CollisionFlag.WALL_SOUTH_WEST, add)
-                change(coords.translate(-1, -1), CollisionFlag.WALL_NORTH_EAST, add)
+                toggle(coords, CollisionFlag.WALL_SOUTH_WEST, add)
+                toggle(coords.translate(-1, -1), CollisionFlag.WALL_NORTH_EAST, add)
                 if (blockProjectile) {
-                    change(coords, CollisionFlag.WALL_SOUTH_WEST_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(-1, -1), CollisionFlag.WALL_NORTH_EAST_PROJECTILE_BLOCKER, add)
+                    toggle(coords, CollisionFlag.WALL_SOUTH_WEST_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(-1, -1), CollisionFlag.WALL_NORTH_EAST_PROJECTILE_BLOCKER, add)
                 }
             }
         }
     } else if (shape == ObjectShape.WallL) {
         when (rotation) {
             0 -> {
-                change(coords, CollisionFlag.WALL_WEST or CollisionFlag.WALL_NORTH, add)
-                change(coords.translate(-1, 0), CollisionFlag.WALL_EAST, add)
-                change(coords.translate(0, 1), CollisionFlag.WALL_SOUTH, add)
+                toggle(coords, CollisionFlag.WALL_WEST or CollisionFlag.WALL_NORTH, add)
+                toggle(coords.translate(-1, 0), CollisionFlag.WALL_EAST, add)
+                toggle(coords.translate(0, 1), CollisionFlag.WALL_SOUTH, add)
                 if (blockProjectile) {
                     val flag = CollisionFlag.WALL_WEST_PROJECTILE_BLOCKER or
                         CollisionFlag.WALL_NORTH_PROJECTILE_BLOCKER
-                    change(coords, flag, add)
-                    change(coords.translate(-1, 0), CollisionFlag.WALL_EAST_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(0, 1), CollisionFlag.WALL_SOUTH_PROJECTILE_BLOCKER, add)
+                    toggle(coords, flag, add)
+                    toggle(coords.translate(-1, 0), CollisionFlag.WALL_EAST_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(0, 1), CollisionFlag.WALL_SOUTH_PROJECTILE_BLOCKER, add)
                 }
             }
             1 -> {
-                change(coords, CollisionFlag.WALL_NORTH or CollisionFlag.WALL_EAST, add)
-                change(coords.translate(0, 1), CollisionFlag.WALL_SOUTH, add)
-                change(coords.translate(1, 0), CollisionFlag.WALL_WEST, add)
+                toggle(coords, CollisionFlag.WALL_NORTH or CollisionFlag.WALL_EAST, add)
+                toggle(coords.translate(0, 1), CollisionFlag.WALL_SOUTH, add)
+                toggle(coords.translate(1, 0), CollisionFlag.WALL_WEST, add)
                 if (blockProjectile) {
                     val flag = CollisionFlag.WALL_NORTH_PROJECTILE_BLOCKER or
                         CollisionFlag.WALL_EAST_PROJECTILE_BLOCKER
-                    change(coords, flag, add)
-                    change(coords.translate(0, 1), CollisionFlag.WALL_SOUTH_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(1, 0), CollisionFlag.WALL_WEST_PROJECTILE_BLOCKER, add)
+                    toggle(coords, flag, add)
+                    toggle(coords.translate(0, 1), CollisionFlag.WALL_SOUTH_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(1, 0), CollisionFlag.WALL_WEST_PROJECTILE_BLOCKER, add)
                 }
             }
             2 -> {
-                change(coords, CollisionFlag.WALL_EAST or CollisionFlag.WALL_SOUTH, add)
-                change(coords.translate(1, 0), CollisionFlag.WALL_WEST, add)
-                change(coords.translate(0, -1), CollisionFlag.WALL_NORTH, add)
+                toggle(coords, CollisionFlag.WALL_EAST or CollisionFlag.WALL_SOUTH, add)
+                toggle(coords.translate(1, 0), CollisionFlag.WALL_WEST, add)
+                toggle(coords.translate(0, -1), CollisionFlag.WALL_NORTH, add)
                 if (blockProjectile) {
                     val flag = CollisionFlag.WALL_EAST_PROJECTILE_BLOCKER or
                         CollisionFlag.WALL_SOUTH_PROJECTILE_BLOCKER
-                    change(coords, flag, add)
-                    change(coords.translate(1, 0), CollisionFlag.WALL_WEST_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(0, -1), CollisionFlag.WALL_NORTH_PROJECTILE_BLOCKER, add)
+                    toggle(coords, flag, add)
+                    toggle(coords.translate(1, 0), CollisionFlag.WALL_WEST_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(0, -1), CollisionFlag.WALL_NORTH_PROJECTILE_BLOCKER, add)
                 }
             }
             3 -> {
-                change(coords, CollisionFlag.WALL_SOUTH or CollisionFlag.WALL_WEST, add)
-                change(coords.translate(0, -1), CollisionFlag.WALL_NORTH, add)
-                change(coords.translate(-1, 0), CollisionFlag.WALL_EAST, add)
+                toggle(coords, CollisionFlag.WALL_SOUTH or CollisionFlag.WALL_WEST, add)
+                toggle(coords.translate(0, -1), CollisionFlag.WALL_NORTH, add)
+                toggle(coords.translate(-1, 0), CollisionFlag.WALL_EAST, add)
                 if (blockProjectile) {
                     val flag = CollisionFlag.WALL_SOUTH_PROJECTILE_BLOCKER or
                         CollisionFlag.WALL_WEST_PROJECTILE_BLOCKER
-                    change(coords, flag, add)
-                    change(coords.translate(0, -1), CollisionFlag.WALL_NORTH_PROJECTILE_BLOCKER, add)
-                    change(coords.translate(-1, 0), CollisionFlag.WALL_EAST_PROJECTILE_BLOCKER, add)
+                    toggle(coords, flag, add)
+                    toggle(coords.translate(0, -1), CollisionFlag.WALL_NORTH_PROJECTILE_BLOCKER, add)
+                    toggle(coords.translate(-1, 0), CollisionFlag.WALL_EAST_PROJECTILE_BLOCKER, add)
                 }
             }
         }
     }
 }
 
-private fun CollisionFlagMap.changeFloorDecor(coords: Coordinates, add: Boolean) {
-    change(coords, CollisionFlag.FLOOR_DECORATION, add)
+private fun CollisionFlagMap.toggleGroundDetail(coords: Coordinates, add: Boolean) {
+    toggle(coords, CollisionFlag.FLOOR_DECORATION, add)
 }
 
-private fun CollisionFlagMap.change(coords: Coordinates, mask: Int, add: Boolean) {
+private fun CollisionFlagMap.toggle(coords: Coordinates, mask: Int, add: Boolean) {
     if (add) {
         add(coords.x, coords.z, coords.level, mask)
     } else {
