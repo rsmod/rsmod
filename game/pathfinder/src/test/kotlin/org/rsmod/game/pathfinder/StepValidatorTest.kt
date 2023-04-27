c23f3e7d982edb25879caa9565d75a97a5406783
Tomm0017 <rspsmods@gmail.com>
2023-04-27 10:08:52 -0400

    Revise `StepValidatorTest`

--- game/pathfinder/src/test/kotlin/org/rsmod/game/pathfinder/StepValidatorTest.kt
+++ game/pathfinder/src/test/kotlin/org/rsmod/game/pathfinder/StepValidatorTest.kt
@@ -1,128 +1,239 @@
 package org.rsmod.game.pathfinder
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.ArgumentsProvider
 import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.provider.EnumSource
 import org.rsmod.game.pathfinder.collision.CollisionFlagMap
+import org.rsmod.game.pathfinder.collision.CollisionStrategies
 import org.rsmod.game.pathfinder.flag.CollisionFlag
-import org.rsmod.game.pathfinder.flag.CollisionFlag.BLOCK_NPCS
-import org.rsmod.game.pathfinder.flag.CollisionFlag.BLOCK_PLAYERS
-import java.util.stream.Stream
+import org.rsmod.game.pathfinder.flag.CollisionFlag.OBJECT
+import org.rsmod.game.pathfinder.flag.CollisionFlag.OBJECT_PROJECTILE_BLOCKER
+import kotlin.math.max
+import kotlin.math.min
 
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class StepValidatorTest {
 
-    private val flags = CollisionFlagMap()
-    private val validator = StepValidator(flags)
-
     @ParameterizedTest
-    @ArgumentsSource(DirectionProvider::class)
-    internal fun `validate unblocked direction (size 1)`(dir: Direction) {
-        val src = RouteCoordinates(3200, 3200)
-        flags.allocateIfAbsent(src.x, src.z, src.level)
-        flags.allocateIfAbsent(src.x, src.z + dir.offZ, src.level)
-        flags.allocateIfAbsent(src.x + dir.offX, src.z, src.level)
-        flags.allocateIfAbsent(src.x + dir.offX, src.z + dir.offZ, src.level)
-        val valid = validator.canTravel(
-            level = src.level,
-            x = src.x,
-            z = src.z,
-            offsetX = dir.offX,
-            offsetZ = dir.offZ,
-            size = 1,
-            extraFlag = 0
-        )
-        Assertions.assertTrue(valid)
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(BlockedDirectionProvider::class)
-    internal fun `invalidate blocked direction (size 1)`(dir: Direction, blockFlags: Int) {
-        val src = RouteCoordinates(3200, 3200)
-        val dest = RouteCoordinates(src.x + dir.offX, src.z + dir.offZ)
-        flags[dest.x, dest.z, dest.level] = blockFlags
-        val valid = validator.canTravel(
-            level = src.level,
-            x = src.x,
-            z = src.z,
-            offsetX = dir.offX,
-            offsetZ = dir.offZ,
-            size = 1,
-            extraFlag = 0
-        )
-        Assertions.assertFalse(valid)
-    }
-
-    @ParameterizedTest
-    @ArgumentsSource(BlockedCharacterProvider::class)
-    internal fun `invalidate blocked character direction (size 1)`(dir: Direction, blockFlags: Int) {
-        val src = RouteCoordinates(3200, 3200)
-        val dest = RouteCoordinates(src.x + dir.offX, src.z + dir.offZ)
-        flags[dest.x, dest.z, dest.level] = blockFlags
-        val valid = validator.canTravel(
-            level = src.level,
-            x = src.x,
-            z = src.z,
-            offsetX = dir.offX,
-            offsetZ = dir.offZ,
-            size = 1,
-            extraFlag = BLOCK_PLAYERS or BLOCK_NPCS
-        )
-        Assertions.assertFalse(valid)
-    }
-
-    private companion object {
-
-        private object DirectionProvider : ArgumentsProvider {
-
-            override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
-                return Stream.of(
-                    Arguments.of(Direction.South),
-                    Arguments.of(Direction.North),
-                    Arguments.of(Direction.West),
-                    Arguments.of(Direction.East),
-                    Arguments.of(Direction.SouthWest),
-                    Arguments.of(Direction.NorthWest),
-                    Arguments.of(Direction.SouthEast),
-                    Arguments.of(Direction.NorthEast)
-                )
+    @EnumSource(Direction::class)
+    internal fun testClearPath(dir: Direction) {
+        val map = CollisionFlagMap()
+        val srcX = 3200
+        val srcZ = 3200
+        val destX = srcX + dir.offX
+        val destZ = srcZ + dir.offZ
+        // Make sure to allocate every zone in between the source
+        // and destination coordinates.
+        for (level in 0 until 4) {
+            for (z in min(srcZ, destZ)..max(srcZ, destZ)) {
+                for (x in min(srcX, destX)..max(srcX, destX)) {
+                    map.allocateIfAbsent(x, z, level)
+                }
             }
         }
+        with(StepValidator(map)) {
+            assertTrue(canTravel(level = 0, srcX, srcZ, dir.offX, dir.offZ))
+            assertTrue(canTravel(level = 1, srcX, srcZ, dir.offX, dir.offZ))
+            assertTrue(canTravel(level = 2, srcX, srcZ, dir.offX, dir.offZ))
+            assertTrue(canTravel(level = 3, srcX, srcZ, dir.offX, dir.offZ))
+        }
+    }
 
-        private object BlockedDirectionProvider : ArgumentsProvider {
-
-            override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
-                return Stream.of(
-                    Arguments.of(Direction.South, CollisionFlag.BLOCK_SOUTH),
-                    Arguments.of(Direction.North, CollisionFlag.BLOCK_NORTH),
-                    Arguments.of(Direction.West, CollisionFlag.BLOCK_WEST),
-                    Arguments.of(Direction.East, CollisionFlag.BLOCK_EAST),
-                    Arguments.of(Direction.SouthWest, CollisionFlag.BLOCK_SOUTH_WEST),
-                    Arguments.of(Direction.NorthWest, CollisionFlag.BLOCK_NORTH_WEST),
-                    Arguments.of(Direction.SouthEast, CollisionFlag.BLOCK_SOUTH_EAST),
-                    Arguments.of(Direction.NorthEast, CollisionFlag.BLOCK_NORTH_EAST)
-                )
+    @ParameterizedTest
+    @EnumSource(Direction::class)
+    internal fun testObjectBlocking(dir: Direction) {
+        val map = CollisionFlagMap()
+        val srcX = 3200
+        val srcZ = 3200
+        val destX = srcX + dir.offX
+        val destZ = srcZ + dir.offZ
+        // Make sure to allocate every zone in between the source
+        // and destination coordinates.
+        for (level in 0 until 4) {
+            for (z in min(srcZ, destZ)..max(srcZ, destZ)) {
+                for (x in min(srcX, destX)..max(srcX, destX)) {
+                    map.allocateIfAbsent(x, z, level)
+                }
             }
         }
+        for (level in 0 until 4) {
+            map[destX, destZ, level] = OBJECT
+        }
+        with(StepValidator(map)) {
+            assertFalse(canTravel(level = 0, srcX, srcZ, dir.offX, dir.offZ))
+            assertFalse(canTravel(level = 1, srcX, srcZ, dir.offX, dir.offZ))
+            assertFalse(canTravel(level = 2, srcX, srcZ, dir.offX, dir.offZ))
+            assertFalse(canTravel(level = 3, srcX, srcZ, dir.offX, dir.offZ))
+        }
+    }
 
-        private object BlockedCharacterProvider : ArgumentsProvider {
-
-            override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
-                return Stream.of(
-                    Arguments.of(Direction.South, BLOCK_PLAYERS),
-                    Arguments.of(Direction.North, BLOCK_NPCS),
-                    Arguments.of(Direction.West, BLOCK_PLAYERS),
-                    Arguments.of(Direction.East, BLOCK_NPCS),
-                    Arguments.of(Direction.SouthWest, BLOCK_PLAYERS),
-                    Arguments.of(Direction.NorthWest, BLOCK_NPCS),
-                    Arguments.of(Direction.SouthEast, BLOCK_PLAYERS),
-                    Arguments.of(Direction.NorthEast, BLOCK_NPCS)
-                )
+    @ParameterizedTest
+    @ArgumentsSource(DirectionalExtraFlagProvider::class)
+    internal fun testExtraFlagBlocking(dir: Direction, extraFlag: Int) {
+        val map = CollisionFlagMap()
+        val srcX = 3200
+        val srcZ = 3200
+        val destX = srcX + dir.offX
+        val destZ = srcZ + dir.offZ
+        // Make sure to allocate every zone in between the source
+        // and destination coordinates.
+        for (level in 0 until 4) {
+            for (z in min(srcZ, destZ)..max(srcZ, destZ)) {
+                for (x in min(srcX, destX)..max(srcX, destX)) {
+                    map.allocateIfAbsent(x, z, level)
+                }
             }
+        }
+        for (level in 0 until 4) {
+            map[destX, destZ, level] = extraFlag
+        }
+        with(StepValidator(map)) {
+            assertFalse(canTravel(level = 0, srcX, srcZ, dir.offX, dir.offZ, extraFlag = extraFlag))
+            assertFalse(canTravel(level = 1, srcX, srcZ, dir.offX, dir.offZ, extraFlag = extraFlag))
+            assertFalse(canTravel(level = 2, srcX, srcZ, dir.offX, dir.offZ, extraFlag = extraFlag))
+            assertFalse(canTravel(level = 3, srcX, srcZ, dir.offX, dir.offZ, extraFlag = extraFlag))
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(Direction::class)
+    internal fun testBlockedFlagStrategy(dir: Direction) {
+        val map = CollisionFlagMap()
+        val srcX = 3200
+        val srcZ = 3200
+        val destX = srcX + dir.offX
+        val destZ = srcZ + dir.offZ
+        // Need to make sure every tile in between the two coordinates
+        // is marked properly, otherwise the collision strategy will not
+        // allow diagonal movement.
+        for (level in 0 until 4) {
+            for (z in min(srcZ, destZ)..max(srcZ, destZ)) {
+                for (x in min(srcX, destX)..max(srcX, destX)) {
+                    map[x, z, level] = CollisionFlag.FLOOR
+                }
+            }
+        }
+        with(StepValidator(map)) {
+            val strategy = CollisionStrategies.Blocked
+            assertTrue(canTravel(level = 0, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 1, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 2, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 3, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(Direction::class)
+    internal fun testIndoorsFlagStrategy(dir: Direction) {
+        val map = CollisionFlagMap()
+        val srcX = 3200
+        val srcZ = 3200
+        val destX = srcX + dir.offX
+        val destZ = srcZ + dir.offZ
+        val outdoorsX = destX + dir.offX
+        val outdoorsZ = destZ + dir.offZ
+        // Need to make sure every tile in between the two coordinates
+        // is marked properly, otherwise the collision strategy will not
+        // allow diagonal movement.
+        for (level in 0 until 4) {
+            for (z in min(srcZ, min(destZ, outdoorsZ))..max(srcZ, max(destZ, outdoorsZ))) {
+                for (x in min(srcX, min(destX, outdoorsX))..max(srcX, max(destX, outdoorsX))) {
+                    map[x, z, level] = CollisionFlag.ROOF
+                }
+            }
+        }
+        // Overwrite the outdoors tiles to remove indoor flag.
+        for (level in 0 until 4) {
+            map[outdoorsX, outdoorsZ, level] = 0
+        }
+        with(StepValidator(map)) {
+            val strategy = CollisionStrategies.Indoors
+            // Test step is valid if destination is also flagged as indoors.
+            assertTrue(canTravel(level = 0, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 1, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 2, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 3, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            // Test step is invalid if destination is not flagged as indoors.
+            assertFalse(canTravel(level = 0, destX, destZ, dir.offX, dir.offZ, collision = strategy))
+            assertFalse(canTravel(level = 1, destX, destZ, dir.offX, dir.offZ, collision = strategy))
+            assertFalse(canTravel(level = 2, destX, destZ, dir.offX, dir.offZ, collision = strategy))
+            assertFalse(canTravel(level = 3, destX, destZ, dir.offX, dir.offZ, collision = strategy))
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(Direction::class)
+    internal fun testOutdoorsFlagStrategy(dir: Direction) {
+        val map = CollisionFlagMap()
+        val srcX = 3200
+        val srcZ = 3200
+        val destX = srcX + dir.offX
+        val destZ = srcZ + dir.offZ
+        val indoorsX = destX + dir.offX
+        val indoorsZ = destZ + dir.offZ
+        // Make sure to allocate every zone in between the source
+        // and destination coordinates.
+        for (level in 0 until 4) {
+            for (z in min(srcZ, min(destZ, indoorsZ))..max(srcZ, max(destZ, indoorsZ))) {
+                for (x in min(srcX, min(destX, indoorsX))..max(srcX, max(destX, indoorsX))) {
+                    map.allocateIfAbsent(x, z, level)
+                }
+            }
+        }
+        // Set the indoor tile flags.
+        for (level in 0 until 4) {
+            map[indoorsX, indoorsZ, level] = CollisionFlag.ROOF
+        }
+        with(StepValidator(map)) {
+            val strategy = CollisionStrategies.Outdoors
+            // Test step is valid if destination _is not_ flagged as indoors.
+            assertTrue(canTravel(level = 0, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 1, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 2, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 3, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            // Test step is invalid if destination _is_ flagged as indoors.
+            assertFalse(canTravel(level = 0, destX, destZ, dir.offX, dir.offZ, collision = strategy))
+            assertFalse(canTravel(level = 1, destX, destZ, dir.offX, dir.offZ, collision = strategy))
+            assertFalse(canTravel(level = 2, destX, destZ, dir.offX, dir.offZ, collision = strategy))
+            assertFalse(canTravel(level = 3, destX, destZ, dir.offX, dir.offZ, collision = strategy))
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(Direction::class)
+    internal fun testLineOfSightFlagStrategy(dir: Direction) {
+        val map = CollisionFlagMap()
+        val srcX = 3200
+        val srcZ = 3200
+        val destX = srcX + dir.offX
+        val destZ = srcZ + dir.offZ
+        val blockedX = destX + dir.offX
+        val blockedZ = destZ + dir.offZ
+        // Make sure to allocate every zone in between the source
+        // and destination coordinates.
+        for (level in 0 until 4) {
+            for (z in min(srcZ, min(destZ, blockedZ))..max(srcZ, max(destZ, blockedZ))) {
+                for (x in min(srcX, min(destX, blockedX))..max(srcX, max(destX, blockedX))) {
+                    map.allocateIfAbsent(x, z, level)
+                }
+            }
+        }
+        for (level in 0 until 4) {
+            map[blockedX, blockedZ, level] = OBJECT_PROJECTILE_BLOCKER
+        }
+        with(StepValidator(map)) {
+            val strategy = CollisionStrategies.Fly
+            // Test step is valid if destination _is not_ flagged with projectile block flag.
+            assertTrue(canTravel(level = 0, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 1, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 2, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            assertTrue(canTravel(level = 3, srcX, srcZ, dir.offX, dir.offZ, collision = strategy))
+            // Test step is invalid if destination _is_ flagged with projectile block flag.
+            assertFalse(canTravel(level = 0, destX, destZ, dir.offX, dir.offZ, collision = strategy))
+            assertFalse(canTravel(level = 1, destX, destZ, dir.offX, dir.offZ, collision = strategy))
+            assertFalse(canTravel(level = 2, destX, destZ, dir.offX, dir.offZ, collision = strategy))
+            assertFalse(canTravel(level = 3, destX, destZ, dir.offX, dir.offZ, collision = strategy))
         }
     }
 }
