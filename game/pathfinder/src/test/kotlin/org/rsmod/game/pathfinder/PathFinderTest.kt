78dbb2162372f0c69d01893cb925786dc223ac44
Tomm0017 <rspsmods@gmail.com>
2023-05-02 10:50:16 -0400

    Add PathFinder tests
    Valid directional paths don't correlate directly with "level"; moved route coordinates level assertions to their own test. Added a new test to verify route fails when surrounded by object collision flags. Also started using [Pair] deconstructor for coordinate declarations in the tests.

--- game/pathfinder/src/test/kotlin/org/rsmod/game/pathfinder/PathFinderTest.kt
+++ game/pathfinder/src/test/kotlin/org/rsmod/game/pathfinder/PathFinderTest.kt
@@ -3,6 +3,7 @@ package org.rsmod.game.pathfinder
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 import org.rsmod.game.pathfinder.collision.buildCollisionMap
@@ -11,36 +12,73 @@ import org.rsmod.game.pathfinder.flag.CollisionFlag
 
 class PathFinderTest {
 
+    @Test
+    fun testSurroundedByBoxes() {
+        val (srcX, srcZ) = 3200 to 3200
+        val (destX, destZ) = 3205 to 3200
+        val map = buildCollisionMap(srcX, srcZ, destX, destZ)
+        // Surround source tile with object collision flag.
+        for (z in -1..1) {
+            for (x in -1..1) {
+                if (x == 0 && z == 0) continue // Skip center (source) tile.
+                map[srcX + x, srcZ + z, 0] = CollisionFlag.OBJECT
+            }
+        }
+        val pathFinder = PathFinder(map)
+        pathFinder.findPath(level = 0, srcX, srcZ, destX, destZ).let { route ->
+            assertTrue(route.failed)
+            assertTrue(route.isEmpty())
+        }
+    }
+
+    @Test
+    fun testRouteCoordinatesMatchLevelInput() {
+        val (srcX, srcZ) = 3200 to 3200
+        val (destX, destZ) = 3201 to 3200
+        val map = buildCollisionMap(srcX, srcZ, destX, destZ)
+        val pathFinder = PathFinder(map)
+        pathFinder.findPath(level = 0, srcX, srcZ, destX, destZ).let { route ->
+            check(route.success)
+            assertTrue(route.all { it.level == 0 })
+        }
+        pathFinder.findPath(level = 1, srcX, srcZ, destX, destZ).let { route ->
+            check(route.success)
+            assertTrue(route.all { it.level == 1 })
+        }
+        pathFinder.findPath(level = 2, srcX, srcZ, destX, destZ).let { route ->
+            check(route.success)
+            assertTrue(route.all { it.level == 2 })
+        }
+        pathFinder.findPath(level = 3, srcX, srcZ, destX, destZ).let { route ->
+            check(route.success)
+            assertTrue(route.all { it.level == 3 })
+        }
+    }
+
     @ParameterizedTest
     @EnumSource(Direction::class)
     fun testValidDirectionalPath(dir: Direction) {
-        val srcX = 3200
-        val srcZ = 3200
-        val destX = srcX + dir.offX
-        val destZ = srcZ + dir.offZ
+        val (srcX, srcZ) = 3200 to 3200
+        val (destX, destZ) = (srcX + dir.offX) to (srcZ + dir.offZ)
         val map = buildCollisionMap(srcX, srcZ, destX, destZ)
         val pathFinder = PathFinder(map)
         pathFinder.findPath(level = 0, srcX, srcZ, destX, destZ).let { route ->
             assertEquals(1, route.size)
-            assertEquals(0, route.last().level)
             assertEquals(destX, route.last().x)
             assertEquals(destZ, route.last().z)
         }
         pathFinder.findPath(level = 1, srcX, srcZ, destX, destZ).let { route ->
             assertEquals(1, route.size)
-            assertEquals(1, route.last().level)
             assertEquals(destX, route.last().x)
             assertEquals(destZ, route.last().z)
         }
         pathFinder.findPath(level = 2, srcX, srcZ, destX, destZ).let { route ->
             assertEquals(1, route.size)
-            assertEquals(2, route.last().level)
             assertEquals(destX, route.last().x)
             assertEquals(destZ, route.last().z)
         }
         pathFinder.findPath(level = 3, srcX, srcZ, destX, destZ).let { route ->
             assertEquals(1, route.size)
-            assertEquals(3, route.last().level)
             assertEquals(destX, route.last().x)
             assertEquals(destZ, route.last().z)
         }
@@ -49,10 +87,8 @@ class PathFinderTest {
     @ParameterizedTest
     @EnumSource(Direction::class)
     fun testInvalidDirectionalPath(dir: Direction) {
-        val srcX = 3200
-        val srcZ = 3200
-        val destX = srcX + dir.offX
-        val destZ = srcZ + dir.offZ
+        val (srcX, srcZ) = 3200 to 3200
+        val (destX, destZ) = (srcX + dir.offX) to (srcZ + dir.offZ)
         val map = buildCollisionMap(srcX, srcZ, destX, destZ)
             .flag(destX, destZ, width = 1, height = 1, mask = CollisionFlag.FLOOR)
         val pathFinder = PathFinder(map)
