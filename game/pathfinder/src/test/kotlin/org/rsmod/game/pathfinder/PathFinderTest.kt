948968edb054a33aeb98ba9a4b48420b11609b8f
Tomm0017 <rspsmods@gmail.com>
2023-05-02 00:44:57 -0400

    Use `buildCollisionMap` in [PathFinderTest]

--- game/pathfinder/src/test/kotlin/org/rsmod/game/pathfinder/PathFinderTest.kt
+++ game/pathfinder/src/test/kotlin/org/rsmod/game/pathfinder/PathFinderTest.kt
@@ -5,63 +5,42 @@ import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
-import org.rsmod.game.pathfinder.collision.CollisionFlagMap
+import org.rsmod.game.pathfinder.collision.buildCollisionMap
+import org.rsmod.game.pathfinder.collision.flag
 import org.rsmod.game.pathfinder.flag.CollisionFlag
-import kotlin.math.max
-import kotlin.math.min
 
 class PathFinderTest {
 
     @ParameterizedTest
     @EnumSource(Direction::class)
     fun testValidDirectionalPath(dir: Direction) {
-        val map = CollisionFlagMap()
-        val pathFinder = PathFinder(map)
         val srcX = 3200
         val srcZ = 3200
         val destX = srcX + dir.offX
         val destZ = srcZ + dir.offZ
-        // Allocate every zone in between the source and
-        // destination coordinates.
-        for (level in 0 until 4) {
-            for (z in min(srcZ, destZ)..max(srcZ, destZ)) {
-                for (x in min(srcX, destX)..max(srcX, destX)) {
-                    map.allocateIfAbsent(x, z, level)
-                }
-            }
-        }
-        with(pathFinder.findPath(level = 0, srcX, srcZ, destX, destZ)) {
-            assertEquals(1, waypoints.size)
-            assertEquals(destX, waypoints.last().x)
-            assertEquals(destZ, waypoints.last().z)
+        val map = buildCollisionMap(srcX, srcZ, destX, destZ)
+        val pathFinder = PathFinder(map)
+        pathFinder.findPath(level = 0, srcX, srcZ, destX, destZ).let { route ->
+            assertEquals(1, route.size)
+            assertEquals(destX, route.last().x)
+            assertEquals(destZ, route.last().z)
         }
     }
 
     @ParameterizedTest
     @EnumSource(Direction::class)
     fun testInvalidDirectionalPath(dir: Direction) {
-        val map = CollisionFlagMap()
-        val pathFinder = PathFinder(map)
         val srcX = 3200
         val srcZ = 3200
         val destX = srcX + dir.offX
         val destZ = srcZ + dir.offZ
-        // Allocate every zone in between the source and
-        // destination coordinates.
-        for (level in 0 until 4) {
-            for (z in min(srcZ, destZ)..max(srcZ, destZ)) {
-                for (x in min(srcX, destX)..max(srcX, destX)) {
-                    map.allocateIfAbsent(x, z, level)
-                }
-            }
-        }
-        for (level in 0 until 4) {
-            map[destX, destZ, level] = CollisionFlag.FLOOR
-        }
-        with(pathFinder.findPath(level = 0, srcX, srcZ, destX, destZ, moveNear = false)) {
-            assertTrue(failed)
-            assertTrue(waypoints.isEmpty())
-            assertFalse(alternative)
+        val map = buildCollisionMap(srcX, srcZ, destX, destZ)
+            .flag(destX, destZ, width = 1, height = 1, mask = CollisionFlag.FLOOR)
+        val pathFinder = PathFinder(map)
+        pathFinder.findPath(level = 0, srcX, srcZ, destX, destZ, moveNear = false).let { route ->
+            assertTrue(route.failed)
+            assertTrue(route.isEmpty())
+            assertFalse(route.alternative)
         }
     }
 }
