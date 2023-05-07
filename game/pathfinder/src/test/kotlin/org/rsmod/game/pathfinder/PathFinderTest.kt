41a1c37034c81d2bf1902fc5f8f03ab9959e94b7
Tomm0017 <rspsmods@gmail.com>
2023-05-06 20:49:18 -0400

    Add object rotation [PathFinder] tests

--- game/pathfinder/src/test/kotlin/org/rsmod/game/pathfinder/PathFinderTest.kt
+++ game/pathfinder/src/test/kotlin/org/rsmod/game/pathfinder/PathFinderTest.kt
@@ -2,12 +2,18 @@ package org.rsmod.game.pathfinder
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ArgumentsSource
 import org.junit.jupiter.params.provider.EnumSource
 import org.rsmod.game.pathfinder.collision.buildCollisionMap
 import org.rsmod.game.pathfinder.collision.flag
+import org.rsmod.game.pathfinder.flag.BlockAccessFlag.BLOCK_EAST
+import org.rsmod.game.pathfinder.flag.BlockAccessFlag.BLOCK_NORTH
+import org.rsmod.game.pathfinder.flag.BlockAccessFlag.BLOCK_SOUTH
+import org.rsmod.game.pathfinder.flag.BlockAccessFlag.BLOCK_WEST
 import org.rsmod.game.pathfinder.flag.CollisionFlag
 
 class PathFinderTest {
@@ -135,6 +141,286 @@ class PathFinderTest {
             assertTrue(route.failed)
             assertTrue(route.isEmpty())
             assertFalse(route.alternative)
+        }
+    }
+
+    /**
+     * Test that object rotations are taken into account within [PathFinder.findPath]
+     * and do not rely on external modifications. For example, given the parameters of
+     * an object in coordinates (3203, 3203) with a dimension of 3 x 1 (width x height),
+     * the following test should pass:
+     *
+     * Object rotation of [0] or [2]. (normal)
+     * ```
+     * --------
+     * --------
+     * --------
+     * ---ooo--
+     * --o   o-
+     * ---ooo--
+     * --------
+     * --------
+     * ```
+     * Where:
+     * - Area starts from bottom-left and makes its way to top-right. (3200,3200 - 3207,3207)
+     * - ' ' (whitespace) are the tiles occupied by the rotated object.
+     * - 'o' are the valid tiles that the tail of [Route.waypoints] may return.
+     * - '-' represents every other tile in the area. (in this case a zone, or 8x8 tile area)
+     */
+    @ParameterizedTest
+    @ArgumentsSource(RotatedObjectProvider::class)
+    fun testRotatedObjectPathNormal(objX: Int, objZ: Int, dimension: Dimension) {
+        val (width, height) = dimension
+        val (minX, minZ) = RouteCoordinates(objX - 16, objZ - 16)
+        val (maxX, maxZ) = RouteCoordinates(objX + 16, objZ + 16)
+        val map = buildCollisionMap(minX, minZ, maxX, maxZ)
+            .flag(objX, objZ, width = width, height = height, mask = CollisionFlag.OBJECT)
+        val pathFinder = PathFinder(map)
+        fun route(srcX: Int, srcZ: Int, rot: Int, blockAccessFlags: Int = 0): Route {
+            return pathFinder.findPath(
+                level = 0,
+                srcX = srcX,
+                srcZ = srcZ,
+                destX = objX,
+                destZ = objZ,
+                destWidth = width,
+                destHeight = height,
+                objRot = rot,
+                objShape = -2, //  Use rectangular exclusive strategy
+                blockAccessFlags = blockAccessFlags
+            )
+        }
+        for (x in 0 until width) {
+            // Test coming from south tiles.
+            route(srcX = objX + x, srcZ = objZ - 3, rot = 0).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + x, objZ - 1), route.last())
+            }
+            route(srcX = objX + x, srcZ = objZ - 3, rot = 2).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + x, objZ - 1), route.last())
+            }
+            // Test coming from north tiles.
+            route(srcX = objX + x, srcZ = objZ + height + 3, rot = 0).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + x, objZ + height), route.last())
+            }
+            route(srcX = objX + x, srcZ = objZ + height + 3, rot = 2).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + x, objZ + height), route.last())
+            }
+            // Test coming from south tiles with access blocked.
+            route(srcX = objX + x, srcZ = objZ - 3, rot = 0, blockAccessFlags = BLOCK_SOUTH).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + x, objZ - 1), route.last())
+            }
+            route(srcX = objX + x, srcZ = objZ - 3, rot = 2, blockAccessFlags = BLOCK_NORTH).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + x, objZ - 1), route.last())
+            }
+            // Test coming from north tiles with access blocked.
+            route(srcX = objX + x, srcZ = objZ + height + 3, rot = 0, blockAccessFlags = BLOCK_NORTH).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + x, objZ + height), route.last())
+            }
+            route(srcX = objX + x, srcZ = objZ + height + 3, rot = 2, blockAccessFlags = BLOCK_SOUTH).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + x, objZ + height), route.last())
+            }
+        }
+        for (z in 0 until height) {
+            // Test coming from west tiles.
+            route(srcX = objX - 3, srcZ = objZ + z, rot = 0).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX - 1, objZ + z), route.last())
+            }
+            route(srcX = objX - 3, srcZ = objZ + z, rot = 2).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX - 1, objZ + z), route.last())
+            }
+            // Test coming from east tiles.
+            route(srcX = objX + width + 3, srcZ = objZ + z, rot = 0).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + width, objZ + z), route.last())
+            }
+            route(srcX = objX + width + 3, srcZ = objZ + z, rot = 2).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + width, objZ + z), route.last())
+            }
+            // Test coming from west tiles with access blocked.
+            route(srcX = objX - 3, srcZ = objZ + z, rot = 0, blockAccessFlags = BLOCK_WEST).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX - 1, objZ + z), route.last())
+            }
+            route(srcX = objX - 3, srcZ = objZ + z, rot = 2, blockAccessFlags = BLOCK_EAST).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX - 1, objZ + z), route.last())
+            }
+            // Test coming from east tiles with access blocked.
+            route(srcX = objX + width + 3, srcZ = objZ + z, rot = 0, blockAccessFlags = BLOCK_EAST).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + width, objZ + z), route.last())
+            }
+            route(srcX = objX + width + 3, srcZ = objZ + z, rot = 2, blockAccessFlags = BLOCK_WEST).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + width, objZ + z), route.last())
+            }
+        }
+    }
+
+    /**
+     * Test that object rotations are taken into account within [PathFinder.findPath]
+     * and do not rely on external modifications. For example, given the parameters of
+     * an object in coordinates (3203, 3203) with a dimension of 3 x 1 (width x height),
+     * the following test should pass:
+     *
+     * Object rotation of [1] or [3]. (flipped)
+     * ```
+     * --------
+     * ---o----
+     * --o o---
+     * --o o---
+     * --o o---
+     * ---o----
+     * --------
+     * --------
+     * ```
+     * Where:
+     * - Area starts from bottom-left and makes its way to top-right. (3200,3200 - 3207,3207)
+     * - ' ' (whitespace) are the tiles occupied by the rotated object.
+     * - 'o' are the valid tiles that the tail of [Route.waypoints] may return.
+     * - '-' represents every other tile in the area. (in this case a zone, or 8x8 tile area)
+     */
+    @ParameterizedTest
+    @ArgumentsSource(RotatedObjectProvider::class)
+    fun testRotatedObjectPathFlipped(objX: Int, objZ: Int, dimension: Dimension) {
+        val (width, height) = dimension
+        val (minX, minZ) = RouteCoordinates(objX - 16, objZ - 16)
+        val (maxX, maxZ) = RouteCoordinates(objX + 16, objZ + 16)
+        val map = buildCollisionMap(minX, minZ, maxX, maxZ)
+            .flag(objX, objZ, width = height, height = width, mask = CollisionFlag.OBJECT)
+        val pathFinder = PathFinder(map)
+        fun route(srcX: Int, srcZ: Int, rot: Int, blockAccessFlags: Int = 0): Route {
+            return pathFinder.findPath(
+                level = 0,
+                srcX = srcX,
+                srcZ = srcZ,
+                destX = objX,
+                destZ = objZ,
+                destWidth = width,
+                destHeight = height,
+                objRot = rot,
+                objShape = -2, //  Use rectangular exclusive strategy
+                blockAccessFlags = blockAccessFlags
+            )
+        }
+        for (x in 0 until height) {
+            // Test coming from south tiles.
+            route(srcX = objX + x, srcZ = objZ - 3, rot = 1).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + x, objZ - 1), route.last())
+            }
+            route(srcX = objX + x, srcZ = objZ - 3, rot = 3).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + x, objZ - 1), route.last())
+            }
+            // Test coming from north tiles.
+            route(srcX = objX + x, srcZ = objZ + width + 3, rot = 1).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + x, objZ + width), route.last())
+            }
+            route(srcX = objX + x, srcZ = objZ + width + 3, rot = 3).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + x, objZ + width), route.last())
+            }
+            // Test coming from south tiles with access blocked.
+            route(srcX = objX + x, srcZ = objZ - 3, rot = 1, blockAccessFlags = BLOCK_EAST).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + x, objZ - 1), route.last())
+            }
+            route(srcX = objX + x, srcZ = objZ - 3, rot = 3, blockAccessFlags = BLOCK_WEST).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + x, objZ - 1), route.last())
+            }
+            // Test coming from north tiles with access blocked.
+            route(srcX = objX + x, srcZ = objZ + height + 3, rot = 1, blockAccessFlags = BLOCK_WEST).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + x, objZ + height), route.last())
+            }
+            route(srcX = objX + x, srcZ = objZ + height + 3, rot = 3, blockAccessFlags = BLOCK_EAST).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + x, objZ + height), route.last())
+            }
+        }
+        for (z in 0 until width) {
+            // Test coming from west tiles.
+            route(srcX = objX - 3, srcZ = objZ + z, rot = 1).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX - 1, objZ + z), route.last())
+            }
+            route(srcX = objX - 3, srcZ = objZ + z, rot = 3).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX - 1, objZ + z), route.last())
+            }
+            // Test coming from east tiles.
+            route(srcX = objX + height + 3, srcZ = objZ + z, rot = 1).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + height, objZ + z), route.last())
+            }
+            route(srcX = objX + height + 3, srcZ = objZ + z, rot = 3).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertEquals(RouteCoordinates(objX + height, objZ + z), route.last())
+            }
+            // Test coming from west tiles with access blocked.
+            route(srcX = objX - 3, srcZ = objZ + z, rot = 1, blockAccessFlags = BLOCK_SOUTH).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX - 1, objZ + z), route.last())
+            }
+            route(srcX = objX - 3, srcZ = objZ + z, rot = 3, blockAccessFlags = BLOCK_NORTH).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX - 1, objZ + z), route.last())
+            }
+            // Test coming from east tiles with access blocked.
+            route(srcX = objX + height + 3, srcZ = objZ + z, rot = 1, blockAccessFlags = BLOCK_NORTH).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + height, objZ + z), route.last())
+            }
+            route(srcX = objX + height + 3, srcZ = objZ + z, rot = 3, blockAccessFlags = BLOCK_SOUTH).let { route ->
+                assertTrue(route.success)
+                assertFalse(route.alternative)
+                assertNotEquals(RouteCoordinates(objX + height, objZ + z), route.last())
+            }
         }
     }
 }
---
+++ game/pathfinder/src/test/kotlin/org/rsmod/game/pathfinder/RotatedObjectProvider.kt
@@ -0,0 +1,23 @@
+package org.rsmod.game.pathfinder
+
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import java.util.stream.Stream
+
+object RotatedObjectProvider : ArgumentsProvider {
+
+    override fun provideArguments(context: ExtensionContext): Stream<out Arguments> {
+        return Stream.of(
+            Arguments.of(3203, 3203, Dimension(1, 1)),
+            Arguments.of(3203, 3203, Dimension(1, 2)),
+            Arguments.of(3203, 3203, Dimension(1, 3)),
+            Arguments.of(3203, 3203, Dimension(2, 1)),
+            Arguments.of(3203, 3203, Dimension(2, 2)),
+            Arguments.of(3203, 3203, Dimension(2, 3)),
+            Arguments.of(3203, 3203, Dimension(3, 1)),
+            Arguments.of(3203, 3203, Dimension(3, 2)),
+            Arguments.of(3203, 3203, Dimension(3, 3))
+        )
+    }
+}
