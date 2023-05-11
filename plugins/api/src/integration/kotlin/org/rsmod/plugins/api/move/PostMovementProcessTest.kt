c6be26a10a3c73e8235a47cdc472e60d5fcd8cbc
Tomm0017 <rspsmods@gmail.com>
2023-05-11 15:29:58 -0400

    Add `DownstreamPacketScope`
    This allows for integration tests to be logically split up and also handles the "clean-up" of clearing the DownstreamList after its use.

--- plugins/api/src/integration/kotlin/org/rsmod/plugins/api/move/PostMovementProcessTest.kt
+++ plugins/api/src/integration/kotlin/org/rsmod/plugins/api/move/PostMovementProcessTest.kt
@@ -8,8 +8,7 @@ import org.rsmod.plugins.api.net.downstream.RebuildNormal
 import org.rsmod.plugins.api.refreshBuildArea
 import org.rsmod.plugins.api.toBuildArea
 import org.rsmod.plugins.testing.GameTestState
-import org.rsmod.plugins.testing.assertions.verify
-import org.rsmod.plugins.testing.assertions.verifyNull
+import org.rsmod.plugins.testing.assertions.verifyDownstream
 
 class PostMovementProcessTest {
 
@@ -21,21 +20,27 @@ class PostMovementProcessTest {
         withPlayer {
             coords = Coordinates(3200, 3200)
             refreshBuildArea(coords)
-            // Test player does not have REBUILD_NORMAL in downstream list.
-            verifyNull(RebuildNormal::class)
-            // Empty executes to make sure player isn't affected inappropriately.
-            repeat(8) { process.execute() }
-            // Test player still does not have this packet queued even after process executes.
-            verifyNull(RebuildNormal::class)
+            // Test player's build area is not rebuilt unnecessarily.
+            verifyDownstream {
+                // Test player does not have REBUILD_NORMAL in downstream list.
+                assertNull(RebuildNormal::class)
+                // Empty executes to make sure player isn't affected inappropriately.
+                repeat(8) { process.execute() }
+                // Test player still does not have this packet queued even after process executes.
+                assertNull(RebuildNormal::class)
+            }
             // Test player's build area is rebuilt when applicable.
-            coords = dest
-            process.execute()
-            verify(RebuildNormal::class) { it.zone == ZoneKey.from(dest) }
-            assertEquals(dest.toBuildArea(), buildArea)
-            // Test player's build area is _not_ rebuild further.
-            downstream.clear()
-            process.execute()
-            verifyNull(RebuildNormal::class)
+            verifyDownstream {
+                coords = dest
+                process.execute()
+                assert(RebuildNormal::class) { it.zone == ZoneKey.from(dest) }
+                assertEquals(dest.toBuildArea(), buildArea)
+            }
+            // Test player's build area is _not_ rebuilt further.
+            verifyDownstream {
+                process.execute()
+                assertNull(RebuildNormal::class)
+            }
         }
     }
 }
--- plugins/testing/src/main/kotlin/org/rsmod/plugins/testing/assertions/DownstreamPackets.kt
+++ plugins/testing/src/main/kotlin/org/rsmod/plugins/testing/assertions/DownstreamPackets.kt
@@ -2,19 +2,48 @@ package org.rsmod.plugins.testing.assertions
 
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.rsmod.game.model.DownstreamList
 import org.rsmod.game.model.mob.Player
 import org.rsmod.protocol.game.packet.DownstreamPacket
 import kotlin.reflect.KClass
 
 @Suppress("UNCHECKED_CAST")
-public fun <T : DownstreamPacket> Player.verify(type: KClass<T>, predicate: (T) -> Boolean) {
-    val valid = downstream.filter { it::class == type }
+public fun <T : DownstreamPacket> DownstreamList.verify(type: KClass<T>, predicate: (T) -> Boolean) {
+    val valid = filter { it::class == type }
     val pass = valid.any { predicate(it as T) }
     assertFalse(valid.isEmpty()) { "`${type.simpleName}` not found in downstream packet list." }
     assertTrue(pass) { "`${type.simpleName}` found but predicate failed. (${valid.size} tested packets)" }
 }
 
-public fun <T : DownstreamPacket> Player.verifyNull(type: KClass<T>) {
-    val contains = downstream.any { it::class == type }
+public fun <T : DownstreamPacket> DownstreamList.verifyNull(type: KClass<T>) {
+    val contains = any { it::class == type }
     assertFalse(contains)
+}
+
+public fun <T : DownstreamPacket> Player.verify(
+    type: KClass<T>,
+    predicate: (T) -> Boolean
+): Unit = downstream.verify(type, predicate)
+
+public fun <T : DownstreamPacket> Player.verifyNull(
+    type: KClass<T>
+): Unit = downstream.verifyNull(type)
+
+public fun Player.verifyDownstream(
+    downstream: DownstreamList = this.downstream,
+    init: DownstreamPacketScope.() -> Unit
+) {
+    DownstreamPacketScope(downstream).apply(init)
+    downstream.clear()
+}
+
+public class DownstreamPacketScope(private val downstream: DownstreamList) {
+
+    public fun <T : DownstreamPacket> assert(type: KClass<T>, predicate: (T) -> Boolean) {
+        downstream.verify(type, predicate)
+    }
+
+    public fun <T : DownstreamPacket> assertNull(type: KClass<T>) {
+        downstream.verifyNull(type)
+    }
 }
