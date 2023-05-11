f4040c3c262a59e7d7d219128f97385e7843bf36
Tomm0017 <rspsmods@gmail.com>
2023-05-11 13:37:00 -0400

    Add `MobList::parallelForEach` extension function

--- game/src/main/kotlin/org/rsmod/game/model/mob/list/MobListExtensions.kt
+++ game/src/main/kotlin/org/rsmod/game/model/mob/list/MobListExtensions.kt
@@ -2,6 +2,12 @@ package org.rsmod.game.model.mob.list
 
 import org.rsmod.game.model.mob.Mob
 
+public inline fun <T : Mob> MobList<T>.parallelForEachNotNull(action: (T) -> Unit) {
+    for (element in parallelStream()) {
+        action(element ?: continue)
+    }
+}
+
 public inline fun <T : Mob> MobList<T>.forEachNotNull(action: (T) -> Unit) {
     for (element in this) {
         action(element ?: continue)
