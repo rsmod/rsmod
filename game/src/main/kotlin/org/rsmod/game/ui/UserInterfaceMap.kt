cc955215630d931d30e18288b6e6a107627af42e
Tomm0017 <rspsmods@gmail.com>
2023-05-26 08:53:03 -0400

    Add `UserInterfaceMap::gameframe` map
    This map is responsible for storing the references from the "main gameframe" component targets to the current gameframe component targets.

--- game/src/main/kotlin/org/rsmod/game/ui/UserInterfaceMap.kt
+++ game/src/main/kotlin/org/rsmod/game/ui/UserInterfaceMap.kt
@@ -4,5 +4,6 @@ public data class UserInterfaceMap(
     public val topLevel: MutableSet<UserInterface> = mutableSetOf(),
     public val overlays: MutableMap<Component, UserInterface> = mutableMapOf(),
     public val modals: MutableMap<Component, UserInterface> = mutableMapOf(),
-    public val properties: MutableMap<Component, ComponentProperty> = mutableMapOf()
+    public val properties: MutableMap<Component, ComponentProperty> = mutableMapOf(),
+    public val gameframe: MutableMap<Component, Component> = mutableMapOf()
 )
