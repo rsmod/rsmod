89c31aaee756d072c738e4ac646def7613a3bbea
LazarVukicevic <106715659+LazarVukicevic@users.noreply.github.com>
2024-01-06 18:53:05 -0500

    Add files via upload

---
+++ miss_schism.plugin.kts
@@ -0,0 +1,6 @@
+package gg.rsmod.plugins.content.areas.draynor.spawns
+
+import gg.rsmod.game.model.Direction
+import gg.rsmod.plugins.api.cfg.Npcs
+
+spawn_npc(npc = Npcs.MISS_SCHISM, x = 3095, z = 3252, direction = Direction.SOUTH)
