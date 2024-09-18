db63ec00c362d36039881054c2cbd4b9b2d0eb06
Tomm0017 <rspsmods@gmail.com>
2024-09-18 06:05:12 -0400

    Replace builder type `InvTypeBuilder` -> `InvPluginBuilder`

--- api/type/type-editors/src/main/kotlin/org/rsmod/api/type/editors/inv/InvEditor.kt
+++ api/type/type-editors/src/main/kotlin/org/rsmod/api/type/editors/inv/InvEditor.kt
@@ -1,12 +1,12 @@
 package org.rsmod.api.type.editors.inv
 
 import org.rsmod.api.type.editors.TypeEditor
-import org.rsmod.game.type.inv.InvTypeBuilder
+import org.rsmod.api.type.script.dsl.InvPluginBuilder
 import org.rsmod.game.type.inv.UnpackedInvType
 
-public abstract class InvEditor : TypeEditor<InvTypeBuilder, UnpackedInvType>() {
-    override fun edit(internal: String, init: InvTypeBuilder.() -> Unit) {
-        val type = InvTypeBuilder(internal).apply(init).build(id = -1)
+public abstract class InvEditor : TypeEditor<InvPluginBuilder, UnpackedInvType>() {
+    override fun edit(internal: String, init: InvPluginBuilder.() -> Unit) {
+        val type = InvPluginBuilder(internal).apply(init).build(id = -1)
         cache += type
     }
 }
