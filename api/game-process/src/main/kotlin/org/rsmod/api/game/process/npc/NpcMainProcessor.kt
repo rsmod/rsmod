0fc0beedcf2fc6da99c7281bd88be7f994970e2d
Tomm0017 <rspsmods@gmail.com>
2024-09-19 07:31:53 -0400

    Resume npc paused processes

--- api/game-process/src/main/kotlin/org/rsmod/api/game/process/npc/NpcMainProcessor.kt
+++ api/game-process/src/main/kotlin/org/rsmod/api/game/process/npc/NpcMainProcessor.kt
@@ -41,9 +41,16 @@ constructor(
         for (npc in this@process) {
             npc.previousCoords = npc.coords
             npc.currentMapClock = mapClock.cycle
+            npc.resumePausedProcess()
             npc.modeProcess()
             npc.movementProcess()
             npc.faceSquareProcess()
+        }
+    }
+
+    private fun Npc.resumePausedProcess() {
+        if (isNotDelayed) {
+            advanceActiveCoroutine()
         }
     }
 
