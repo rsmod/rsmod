e61734ca76c218d51426b787eb4eeea5f5213b58
Tomm0017 <rspsmods@gmail.com>
2024-09-22 16:21:37 -0400

    Add windmill ladder script

---
+++ content/other/generic/src/main/kotlin/org/rsmod/content/other/generic/windmill/WindmillConfig.kt
@@ -0,0 +1,9 @@
+package org.rsmod.content.other.generic.windmill
+
+import org.rsmod.api.type.refs.loc.LocReferences
+
+internal object WindmillLocs : LocReferences() {
+    val ladder_up = find(1865137527552917217)
+    val ladder_option = find(1367500736205422751)
+    val ladder_down = find(7410446485423123414)
+}
---
+++ content/other/generic/src/main/kotlin/org/rsmod/content/other/generic/windmill/WindmillLadderScript.kt
@@ -0,0 +1,46 @@
+package org.rsmod.content.other.generic.windmill
+
+import jakarta.inject.Inject
+import org.rsmod.api.config.refs.seqs
+import org.rsmod.api.dialogue.Dialogues
+import org.rsmod.api.dialogue.startDialogue
+import org.rsmod.api.player.protect.ProtectedAccess
+import org.rsmod.api.script.onOpLoc1
+import org.rsmod.api.script.onOpLoc2
+import org.rsmod.api.script.onOpLoc3
+import org.rsmod.plugin.scripts.PluginScript
+import org.rsmod.plugin.scripts.ScriptContext
+
+class WindmillLadderScript @Inject constructor(private val dialogues: Dialogues) : PluginScript() {
+    override fun ScriptContext.startUp() {
+        onOpLoc1(WindmillLocs.ladder_down) { climbDown() }
+        onOpLoc1(WindmillLocs.ladder_up) { climbUp() }
+        onOpLoc1(WindmillLocs.ladder_option) { climbOption() }
+        onOpLoc2(WindmillLocs.ladder_option) { climbUp() }
+        onOpLoc3(WindmillLocs.ladder_option) { climbDown() }
+    }
+
+    private suspend fun ProtectedAccess.climbUp(): Unit = climb(1)
+
+    private suspend fun ProtectedAccess.climbDown(): Unit = climb(-1)
+
+    private suspend fun ProtectedAccess.climb(translateLevel: Int) {
+        arriveDelay()
+        val dest = player.coords.translateLevel(translateLevel)
+        anim(seqs.human_reachforladddertop)
+        delay(1)
+        telejump(dest)
+    }
+
+    private suspend fun ProtectedAccess.climbOption() {
+        arriveDelay()
+        startDialogue(dialogues) {
+            val translate =
+                choice2("Climb Up.", 1, "Climb Down.", -1, title = "Climb up or down the ladder?")
+            val dest = player.coords.translateLevel(translate)
+            anim(seqs.human_reachforladddertop)
+            delay(1)
+            telejump(dest)
+        }
+    }
+}
