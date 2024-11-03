466db369a5db8279b60241d68ebb9feb7febbcc9
Tomm0017 <rspsmods@gmail.com>
2024-11-03 05:11:21 -0500

    Add queues for duck overhead text

--- content/other/generic/src/main/kotlin/org/rsmod/content/other/generic/npcs/Ducks.kt
+++ content/other/generic/src/main/kotlin/org/rsmod/content/other/generic/npcs/Ducks.kt
@@ -4,6 +4,8 @@ import jakarta.inject.Inject
 import org.rsmod.api.config.refs.content
 import org.rsmod.api.random.GameRandom
 import org.rsmod.api.script.onAiTimer
+import org.rsmod.api.script.onNpcQueue
+import org.rsmod.api.type.refs.queue.QueueReferences
 import org.rsmod.game.entity.Npc
 import org.rsmod.game.entity.NpcList
 import org.rsmod.plugin.scripts.PluginScript
@@ -14,29 +16,43 @@ class Ducks @Inject constructor(private val random: GameRandom, private val npcL
     override fun ScriptContext.startUp() {
         onAiTimer(content.duck) { npc.duckTimer() }
         onAiTimer(content.duckling) { npc.ducklingTimer() }
+        onNpcQueue(DuckQueues.duck_say) { npc.duckSay() }
+        onNpcQueue(DuckQueues.duckling_say) { npc.ducklingSay() }
     }
 
     private fun Npc.duckTimer() {
         setNextTimer()
-        // TODO: say should be an npc_queue
+        queue(DuckQueues.duck_say, 0)
+    }
+
+    private fun Npc.duckSay() {
         say("Quack!")
         // TODO: sound_area
     }
 
     private fun Npc.ducklingTimer() {
         setNextTimer()
-        // NOTE: Ducklings should hunt for nearby ducks.
+        // TODO: Duckling huntmode for nearby ducks.
         val duck = facingTarget(npcList)
         if (duck != null && duck.matches(content.duckling)) {
             duck.say("Quack?")
-            // TODO: duck.sound_area
-            // TODO: npc_queue: after 2 cycles duckling should say "Eep!"
+            duck.queue(DuckQueues.duckling_say, 2)
             return
         }
+    }
+
+    private fun Npc.ducklingSay() {
+        say("Eep!")
+        // TODO: sound_area
     }
 
     private fun Npc.setNextTimer() {
         val next = random.of(50..100)
         aiTimer(next)
     }
+}
+
+internal object DuckQueues : QueueReferences() {
+    val duckling_say = find("duckling_say")
+    val duck_say = find("duck_say")
 }
