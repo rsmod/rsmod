d2bfb836d55eb0c1b3b556a378132dd620b7dbe6
Tomm0017 <rspsmods@gmail.com>
2024-12-16 17:17:25 -0500

    Mark constructor as `OptIn`

--- engine/game/src/main/kotlin/org/rsmod/game/obj/InvObj.kt
+++ engine/game/src/main/kotlin/org/rsmod/game/obj/InvObj.kt
@@ -1,5 +1,3 @@
-@file:Suppress("DEPRECATION")
-
 package org.rsmod.game.obj
 
 import org.rsmod.game.type.obj.ObjType
@@ -8,5 +6,6 @@ import org.rsmod.game.type.util.UncheckedType
 public data class InvObj
 @UncheckedType("Use the `ObjType` constructor instead for type-safety consistency.")
 constructor(public val id: Int, public val count: Int, public val vars: Int = 0) {
+    @OptIn(UncheckedType::class)
     public constructor(type: ObjType, count: Int = 1, vars: Int = 0) : this(type.id, count, vars)
 }
