6cac1fb17197163ea2bed2276f626ee0d713af78
Tomm0017 <rspsmods@gmail.com>
2025-01-08 17:09:14 -0500

    Move `OptIn` scope
    Just a matter of preference to avoid `InvObj` constructor doc pollution.

--- engine/game/src/main/kotlin/org/rsmod/game/obj/InvObj.kt
+++ engine/game/src/main/kotlin/org/rsmod/game/obj/InvObj.kt
@@ -3,9 +3,9 @@ package org.rsmod.game.obj
 import org.rsmod.game.type.obj.ObjType
 import org.rsmod.game.type.util.UncheckedType
 
+@OptIn(UncheckedType::class)
 public data class InvObj
 @UncheckedType("Use the `ObjType` constructor instead for type-safety consistency.")
 constructor(public val id: Int, public val count: Int, public val vars: Int = 0) {
-    @OptIn(UncheckedType::class)
     public constructor(type: ObjType, count: Int = 1, vars: Int = 0) : this(type.id, count, vars)
 }
