2655cb90498b57dc030fac7780f8808c82169c7b
Tomm0017 <rspsmods@gmail.com>
2025-01-09 06:06:57 -0500

    Enable Javadoc and sources JAR generation for Java plugin

--- build-logic/src/main/kotlin/kotlin-conventions.gradle.kts
+++ build-logic/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -2,6 +2,15 @@ plugins {
     kotlin("jvm")
 }
 
+plugins.withType<JavaPlugin> {
+    configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+        withJavadocJar()
+        withSourcesJar()
+    }
+}
+
 kotlin {
     jvmToolchain(11)
 
