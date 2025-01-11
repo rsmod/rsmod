532929e3313a314ea4581580b5a304964b079243
Tomm0017 <rspsmods@gmail.com>
2025-01-11 01:36:52 -0500

    Bind registry and repository instances

--- api/core/src/main/kotlin/org/rsmod/api/core/module/EntityRepoModule.kt
+++ api/core/src/main/kotlin/org/rsmod/api/core/module/EntityRepoModule.kt
@@ -5,6 +5,7 @@ import org.rsmod.api.repo.controller.ControllerRepository
 import org.rsmod.api.repo.loc.LocRepository
 import org.rsmod.api.repo.npc.NpcRepository
 import org.rsmod.api.repo.obj.ObjRepository
+import org.rsmod.api.repo.player.PlayerRepository
 import org.rsmod.module.ExtendedModule
 
 public object EntityRepoModule : ExtendedModule() {
@@ -13,6 +14,7 @@ public object EntityRepoModule : ExtendedModule() {
         bindInstance<LocRepository>()
         bindInstance<NpcRepository>()
         bindInstance<ObjRepository>()
+        bindInstance<PlayerRepository>()
         bindInstance<ControllerRepository>()
     }
 }
