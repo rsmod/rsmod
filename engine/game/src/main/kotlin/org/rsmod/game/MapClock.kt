321d771bc61eb4a33221184dea9e742c626c86b9
Tomm0017 <rspsmods@gmail.com>
2025-01-10 19:31:51 -0500

    Add `minus` operator function

--- engine/game/src/main/kotlin/org/rsmod/game/MapClock.kt
+++ engine/game/src/main/kotlin/org/rsmod/game/MapClock.kt
@@ -13,6 +13,8 @@ public class MapClock(public var cycle: Int = 0) {
     public operator fun plus(cycles: Int): Int =
         min(Int.MAX_VALUE.toLong(), cycle.toLong() + cycles).toInt()
 
+    public operator fun minus(cycles: Int): Int = cycle - cycles
+
     public operator fun compareTo(value: Int): Int = cycle.compareTo(value)
 
     override fun toString(): String = "MapClock(cycle=$cycle)"
