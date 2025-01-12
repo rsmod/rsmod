06b1015b8176bfecbce4bfb816afe847a081a391
Tomm0017 <rspsmods@gmail.com>
2025-01-12 06:04:48 -0500

    Generate `equals` and `hashCode` implementation

--- engine/game/src/main/kotlin/org/rsmod/game/type/stat/StatType.kt
+++ engine/game/src/main/kotlin/org/rsmod/game/type/stat/StatType.kt
@@ -13,4 +13,22 @@ public class StatType(internal var internalId: Int?, internal var internalName: 
 
     override fun toString(): String =
         "StatType(internalName='$internalName', internalId=$internalId)"
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as StatType
+
+        if (internalId != other.internalId) return false
+        if (internalName != other.internalName) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = internalId ?: 0
+        result = 31 * result + internalName.hashCode()
+        return result
+    }
 }
