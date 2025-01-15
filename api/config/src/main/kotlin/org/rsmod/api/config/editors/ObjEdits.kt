730905feca574656b7fdb06038546037ea34bfbe
Tomm0017 <rspsmods@gmail.com>
2025-01-15 02:43:23 -0500

    Add chinchompa release params

--- api/config/src/main/kotlin/org/rsmod/api/config/editors/ObjEdits.kt
+++ api/config/src/main/kotlin/org/rsmod/api/config/editors/ObjEdits.kt
@@ -7,5 +7,17 @@ public object ObjEdits : ObjEditor() {
     init {
         edit("coins") { param[params.shop_sale_restricted] = true }
         edit("platinum_token") { param[params.shop_sale_restricted] = true }
+        edit("chinchompa") {
+            param[params.release_note_title] = "Drop all of your chinchompas?"
+            param[params.release_note_message] = "You release the chinchompa and it bounds away."
+        }
+        edit("red_chinchompa") {
+            param[params.release_note_title] = "Drop all of your red chinchompas?"
+            param[params.release_note_message] = "You release the chinchompa and it bounds away."
+        }
+        edit("black_chinchompa") {
+            param[params.release_note_title] = "Drop all of your black chinchompas?"
+            param[params.release_note_message] = "You release the chinchompa and it bounds away."
+        }
     }
 }
