6ee31580648d84ac8af4d0f7af4649195bc6ab7c
Tomm0017 <rspsmods@gmail.com>
2025-01-16 01:28:28 -0500

    Decouple player zone registry from zone updates
    As of now, zone updates are handled async, and player zone registrations are not thread-safe. Though zone updates being async may not always be the case (will have to benchmark), it still makes sense for these two concepts to be decoupled.

--- api/game-process/src/main/kotlin/org/rsmod/api/game/process/player/PlayerPostTickProcess.kt
+++ api/game-process/src/main/kotlin/org/rsmod/api/game/process/player/PlayerPostTickProcess.kt
@@ -6,35 +6,58 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.supervisorScope
 import org.rsmod.api.player.forceDisconnect
+import org.rsmod.api.registry.player.PlayerRegistry
 import org.rsmod.game.entity.Player
 import org.rsmod.game.entity.PlayerList
 import org.rsmod.game.seq.EntitySeq
+import org.rsmod.map.zone.ZoneKey
 
 public class PlayerPostTickProcess
 @Inject
-constructor(private val players: PlayerList, private val zoneUpdates: PlayerZoneUpdateProcessor) {
+constructor(
+    private val playerList: PlayerList,
+    private val playerRegistry: PlayerRegistry,
+    private val zoneUpdates: PlayerZoneUpdateProcessor,
+) {
     private val logger = InlineLogger()
 
     public fun process() {
-        players.processZones()
-        players.forEach {
-            it.tryOrDisconnect {
-                flushClient()
-                cleanUpPendingUpdates()
+        updateZoneRegistry()
+        sendZoneUpdates()
+        finalizePostTick()
+    }
+
+    private fun updateZoneRegistry() {
+        for (player in playerList) {
+            player.tryOrDisconnect {
+                val currZone = ZoneKey.from(coords)
+                val prevZone = lastProcessedZone
+                if (currZone != prevZone) {
+                    playerRegistry.change(this, prevZone, currZone)
+                }
             }
         }
     }
 
     @Suppress("DeferredResultUnused")
-    private fun PlayerList.processZones() = runBlocking {
+    private fun sendZoneUpdates() = runBlocking {
         zoneUpdates.computeEnclosedBuffers()
         supervisorScope {
-            for (player in this@processZones) {
+            for (player in playerList) {
                 async { player.tryOrDisconnect { zoneUpdates.process(this) } }
             }
         }
         zoneUpdates.clearEnclosedBuffers()
         zoneUpdates.clearPendingZoneUpdates()
+    }
+
+    private fun finalizePostTick() {
+        for (player in playerList) {
+            player.tryOrDisconnect {
+                flushClient()
+                cleanUpPendingUpdates()
+            }
+        }
     }
 
     private fun Player.flushClient() {
--- api/game-process/src/main/kotlin/org/rsmod/api/game/process/player/PlayerZoneUpdateProcessor.kt
+++ api/game-process/src/main/kotlin/org/rsmod/api/game/process/player/PlayerZoneUpdateProcessor.kt
@@ -10,7 +10,6 @@ import net.rsprot.protocol.game.outgoing.zone.header.UpdateZonePartialFollows
 import net.rsprot.protocol.message.ZoneProt
 import org.rsmod.api.registry.loc.LocRegistry
 import org.rsmod.api.registry.obj.ObjRegistry
-import org.rsmod.api.registry.player.PlayerRegistry
 import org.rsmod.api.registry.zone.ZoneUpdateMap
 import org.rsmod.api.registry.zone.ZoneUpdateTransformer
 import org.rsmod.api.utils.map.BuildAreaUtils
@@ -27,7 +26,6 @@ constructor(
     private val updates: ZoneUpdateMap,
     private val locReg: LocRegistry,
     private val objReg: ObjRegistry,
-    private val playerReg: PlayerRegistry,
     private val enclosedBuffers: SharedZoneEnclosedBuffers,
 ) {
     public fun computeEnclosedBuffers() {
@@ -72,9 +70,6 @@ constructor(
             // ground the same cycle the zone becomes visible to the player.
             val oldZones = IntArrayList(currZones).apply { removeAll(newZones) }
             processVisibleZoneUpdates(buildArea, oldZones)
-
-            // Update player's zone in registry.
-            playerReg.change(this, prevZone, currZone)
         } else {
             // If the player hasn't moved to a new zone, process updates for currently visible
             // zones.
