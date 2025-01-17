b34f0a9d403615acd36ed75327fb6c76d1172f9c
Tomm0017 <rspsmods@gmail.com>
2025-01-17 10:28:23 -0500

    Replace explicit `ParamType` with typealias

--- api/config/src/main/kotlin/org/rsmod/api/config/refs/BaseParams.kt
+++ api/config/src/main/kotlin/org/rsmod/api/config/refs/BaseParams.kt
@@ -11,8 +11,6 @@ import org.rsmod.api.config.aliases.ParamStat
 import org.rsmod.api.config.aliases.ParamStr
 import org.rsmod.api.config.aliases.ParamSynth
 import org.rsmod.api.type.refs.param.ParamReferences
-import org.rsmod.game.type.param.ParamType
-import org.rsmod.game.type.synth.SynthType
 
 typealias params = BaseParams
 
@@ -33,7 +31,7 @@ object BaseParams : ParamReferences() {
     val no_alchemy: ParamInt = find("no_alchemy")
 
     /* Server-side only types */
-    val equipment_sound: ParamType<SynthType> = find("equipment_sound")
+    val equipment_sound: ParamSynth = find("equipment_sound")
     val player_op5_text: ParamStr = find("player_op5_text")
     val release_note_title: ParamStr = find("release_note_title")
     val release_note_message: ParamStr = find("release_note_message")
