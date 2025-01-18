3347fc1ffa265ca993faa7dafb857f4c4cb46520
Tomm0017 <rspsmods@gmail.com>
2025-01-18 00:10:21 -0500

    Bind default cache enrichers

--- api/cache-enricher/src/main/kotlin/org/rsmod/api/cache/enricher/CacheEnricherModule.kt
+++ api/cache-enricher/src/main/kotlin/org/rsmod/api/cache/enricher/CacheEnricherModule.kt
@@ -1,9 +1,18 @@
 package org.rsmod.api.cache.enricher
 
+import org.rsmod.api.cache.enricher.loc.DefaultLocCacheEnricher
+import org.rsmod.api.cache.enricher.loc.LocCacheEnricher
+import org.rsmod.api.cache.enricher.npc.DefaultNpcCacheEnricher
+import org.rsmod.api.cache.enricher.npc.NpcCacheEnricher
+import org.rsmod.api.cache.enricher.obj.DefaultObjCacheEnricher
+import org.rsmod.api.cache.enricher.obj.ObjCacheEnricher
 import org.rsmod.module.ExtendedModule
 
 public object CacheEnricherModule : ExtendedModule() {
     override fun bind() {
+        addSetBinding<LocCacheEnricher>(DefaultLocCacheEnricher::class.java)
+        addSetBinding<NpcCacheEnricher>(DefaultNpcCacheEnricher::class.java)
+        addSetBinding<ObjCacheEnricher>(DefaultObjCacheEnricher::class.java)
         bindInstance<CacheEnrichment>()
     }
 }
