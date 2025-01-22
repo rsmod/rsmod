eab94340f46f46b34b71c93bbc81c4592cab84be
Tomm0017 <rspsmods@gmail.com>
2025-01-22 05:44:36 -0500

    Add `InvObj` copy constructor

--- engine/game/src/main/kotlin/org/rsmod/game/obj/InvObj.kt
+++ engine/game/src/main/kotlin/org/rsmod/game/obj/InvObj.kt
@@ -8,6 +8,8 @@ import org.rsmod.game.type.util.UncheckedType
 public data class InvObj
 @UncheckedType("Use the `ObjType` constructor instead for type-safety consistency.")
 constructor(public val id: Int, public val count: Int, public val vars: Int = 0) {
+    public constructor(copy: InvObj) : this(copy.id, copy.count, copy.vars)
+
     public constructor(type: ObjType, count: Int = 1, vars: Int = 0) : this(type.id, count, vars)
 }
 
