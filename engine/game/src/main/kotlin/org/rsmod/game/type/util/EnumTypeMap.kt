5464699dbbdbaddc79afc6e97769eb5212e2c4de
Tomm0017 <rspsmods@gmail.com>
2025-01-23 02:11:12 -0500

    Add `EnumTypeNonNullMap` concept
    Can be created through `EnumTypeMap.filterValuesNotNull`. This is an easy way to avoid boilerplate of checking if values are null for enum type maps.

--- engine/game/src/main/kotlin/org/rsmod/game/type/util/EnumTypeMap.kt
+++ engine/game/src/main/kotlin/org/rsmod/game/type/util/EnumTypeMap.kt
@@ -5,7 +5,7 @@ import org.rsmod.game.type.enums.UnpackedEnumType
 public class EnumTypeMap<K : Any, V : Any>(type: UnpackedEnumType<K, V>) :
     Iterable<Map.Entry<K, V?>> {
     private val entries: Map<K, V?> = type.typedMap
-    private val default: V? = type.default
+    internal val default: V? = type.default
 
     public val isEmpty: Boolean
         get() = entries.isEmpty()
@@ -29,4 +29,53 @@ public class EnumTypeMap<K : Any, V : Any>(type: UnpackedEnumType<K, V>) :
     public operator fun contains(key: K): Boolean = entries.containsKey(key)
 
     override fun iterator(): Iterator<Map.Entry<K, V?>> = entries.iterator()
+
+    /**
+     * Creates a new [EnumTypeNonNullMap] containing only the entries with non-null values.
+     *
+     * _Note: This function does not consider the [UnpackedEnumType.default] value of the associated
+     * enum type. Even if the `default` is non-null, any key-value entry with a null value will
+     * still be excluded from the result._
+     */
+    public fun filterValuesNotNull(): EnumTypeNonNullMap<K, V> {
+        return EnumTypeNonNullMap.from(this)
+    }
+}
+
+public class EnumTypeNonNullMap<K : Any, V : Any>(
+    private val entries: Map<K, V>,
+    private val default: V?,
+) : Iterable<Map.Entry<K, V>> {
+    public val isEmpty: Boolean
+        get() = entries.isEmpty()
+
+    public val isNotEmpty: Boolean
+        get() = entries.isNotEmpty()
+
+    public val keys: Set<K>
+        get() = entries.keys
+
+    public val values: Collection<V>
+        get() = entries.values
+
+    public fun getOrNull(key: K): V? = entries[key]
+
+    public operator fun get(key: K): V? =
+        entries[key] ?: default ?: throw NoSuchElementException("Key $key is missing in the map.")
+
+    public operator fun contains(key: K): Boolean = entries.containsKey(key)
+
+    override fun iterator(): Iterator<Map.Entry<K, V>> = entries.iterator()
+
+    public companion object {
+        public fun <K : Any, V : Any> from(other: EnumTypeMap<K, V>): EnumTypeNonNullMap<K, V> {
+            val filteredEntries = mutableMapOf<K, V>()
+            for ((key, value) in other) {
+                if (value != null) {
+                    filteredEntries[key] = value
+                }
+            }
+            return EnumTypeNonNullMap(filteredEntries, other.default)
+        }
+    }
 }
