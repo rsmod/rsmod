5ba528dbd926e5038a8b520267a5165e68b56f96
Tomm0017 <rspsmods@gmail.com>
2025-01-23 00:18:51 -0500

    Re-order inv iteration order on `startInvTransmit`

--- api/player/src/main/kotlin/org/rsmod/api/player/PlayerExtensions.kt
+++ api/player/src/main/kotlin/org/rsmod/api/player/PlayerExtensions.kt
@@ -22,6 +22,19 @@ public fun Player.startInvTransmit(inv: Inventory) {
     check(inv.type.scope != InvScope.Shared || !invMap.contains(inv.type)) {
         "`inv` should have previously been removed from cached inv map: $inv"
     }
+    /*
+     * Reorders the given `inv` in the list of transmitted inventories. This ensures that updates
+     * for inventories are sent in the order they were added when this function was called, even if
+     * they were first added during log in (e.g., `worn` and `inv`).
+     *
+     * This is done to emulate the behavior observed in os, where the transmitted inventory order
+     * can change dynamically. For example, equipping an item will have the update order of `inv`
+     * and `worn`. If you open a shop and then equip an item, the new order will be `worn` -> `inv`.
+     *
+     * This logic guarantees that updates sent from this point onward respect the new order.
+     */
+    transmittedInvs.remove(inv.type.id)
+
     invMap[inv.type] = inv
     transmittedInvs.add(inv.type.id)
     UpdateInventory.updateInvFull(this, inv)
