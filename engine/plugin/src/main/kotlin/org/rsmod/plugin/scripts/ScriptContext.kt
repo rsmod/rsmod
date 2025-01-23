18ff9e742d54aa76191f8c14ec8215f229ea6667
Tomm0017 <rspsmods@gmail.com>
2025-01-23 05:47:02 -0500

    Remove `data` modifier from `ScriptContext`

--- engine/plugin/src/main/kotlin/org/rsmod/plugin/scripts/ScriptContext.kt
+++ engine/plugin/src/main/kotlin/org/rsmod/plugin/scripts/ScriptContext.kt
@@ -4,6 +4,6 @@ import jakarta.inject.Inject
 import org.rsmod.events.EventBus
 import org.rsmod.game.cheat.CheatCommandMap
 
-public data class ScriptContext
+public class ScriptContext
 @Inject
 constructor(public val eventBus: EventBus, public val cheatCommandMap: CheatCommandMap)
