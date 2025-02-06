456c5780da3a5399f573ea4cc89d8389f44d68bc
Tomm0017 <rspsmods@gmail.com>
2025-02-05 22:34:59 -0500

    Remove `stockmarket` market price condition
    Some objs such as platinum tokens have `stockmarket` as false, but have
    a cost of 1000 coins. This should be taken into account.

--- api/market/src/main/kotlin/org/rsmod/api/market/DefaultMarketPrices.kt
+++ api/market/src/main/kotlin/org/rsmod/api/market/DefaultMarketPrices.kt
@@ -3,10 +3,5 @@ package org.rsmod.api.market
 import org.rsmod.game.type.obj.UnpackedObjType
 
 public object DefaultMarketPrices : MarketPrices {
-    override fun get(type: UnpackedObjType): Int? =
-        if (type.stockmarket) {
-            type.cost
-        } else {
-            null
-        }
+    override fun get(type: UnpackedObjType): Int = type.cost
 }
