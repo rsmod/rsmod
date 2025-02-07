f6429634a703ddf0d19b694f52c4bc097904851d
Tomm0017 <rspsmods@gmail.com>
2025-02-07 02:38:05 -0500

    Decouple `op` from `Interaction`
    Not all interactions will have an `InteractionOp` associated with them.
    (e.g., opnpct)

--- engine/game/src/main/kotlin/org/rsmod/game/interact/Interaction.kt
+++ engine/game/src/main/kotlin/org/rsmod/game/interact/Interaction.kt
@@ -6,7 +6,6 @@ import org.rsmod.game.loc.BoundLocInfo
 import org.rsmod.game.obj.Obj
 
 public sealed class Interaction(
-    public val op: InteractionOp,
     public val hasOpTrigger: Boolean,
     public val hasApTrigger: Boolean,
     public var apRange: Int,
@@ -16,7 +15,6 @@ public sealed class Interaction(
 ) {
     override fun toString(): String =
         "Interaction(" +
-            "op=$op, " +
             "hasOpTrigger=$hasOpTrigger, " +
             "hasApTrigger=$hasApTrigger, " +
             "apRange=$apRange, " +
@@ -28,27 +26,29 @@ public sealed class Interaction(
 
 public class InteractionLoc(
     public val target: BoundLocInfo,
-    op: InteractionOp,
+    public val op: InteractionOp,
     hasOpTrigger: Boolean,
     hasApTrigger: Boolean,
     startApRange: Int = PathingEntity.DEFAULT_AP_RANGE,
     persistent: Boolean = false,
-) : Interaction(op, hasOpTrigger, hasApTrigger, startApRange, persistent)
+) : Interaction(hasOpTrigger, hasApTrigger, startApRange, persistent)
 
 public class InteractionNpc(
     public val target: Npc,
-    op: InteractionOp,
+    public val op: InteractionOp,
     hasOpTrigger: Boolean,
     hasApTrigger: Boolean,
     startApRange: Int = PathingEntity.DEFAULT_AP_RANGE,
     persistent: Boolean = false,
-) : Interaction(op, hasOpTrigger, hasApTrigger, startApRange, persistent)
+) : Interaction(hasOpTrigger, hasApTrigger, startApRange, persistent) {
+    public val type: Int = target.visType.id
+}
 
 public class InteractionObj(
     public val target: Obj,
-    op: InteractionOp,
+    public val op: InteractionOp,
     hasOpTrigger: Boolean,
     hasApTrigger: Boolean,
     startApRange: Int = PathingEntity.DEFAULT_AP_RANGE,
     persistent: Boolean = false,
-) : Interaction(op, hasOpTrigger, hasApTrigger, startApRange, persistent)
+) : Interaction(hasOpTrigger, hasApTrigger, startApRange, persistent)
