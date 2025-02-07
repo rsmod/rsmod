f6ef69e0e062d6e4e25bab9adb174bac841bdb95
Tomm0017 <rspsmods@gmail.com>
2025-02-07 01:40:12 -0500

    Cancel npc interaction on transmog change

--- api/game-process/src/main/kotlin/org/rsmod/api/game/process/player/PlayerInteractionProcessor.kt
+++ api/game-process/src/main/kotlin/org/rsmod/api/game/process/player/PlayerInteractionProcessor.kt
@@ -312,18 +312,22 @@ constructor(
 
     private fun Player.shouldCancelInteraction(interaction: Interaction): Boolean =
         when (interaction) {
-            is InteractionLoc -> {
-                locRegistry.isInvalid(interaction.target.coords, interaction.target.id)
-            }
-
-            is InteractionNpc -> {
-                !interaction.target.isValidTarget || interaction.target.isDelayed
-            }
-
-            is InteractionObj -> {
-                objRegistry.isInvalid(this, interaction.target)
-            }
+            is InteractionLoc -> interaction.shouldCancelInteraction()
+            is InteractionNpc -> interaction.shouldCancelInteraction()
+            is InteractionObj -> interaction.shouldCancelInteraction(this)
         }
+
+    private fun InteractionLoc.shouldCancelInteraction(): Boolean {
+        return locRegistry.isInvalid(target.coords, target.id)
+    }
+
+    private fun InteractionNpc.shouldCancelInteraction(): Boolean {
+        return !target.isValidTarget || target.isDelayed || type != target.currentType.id
+    }
+
+    private fun InteractionObj.shouldCancelInteraction(observer: Player): Boolean {
+        return objRegistry.isInvalid(observer, target)
+    }
 
     /* Interaction event launch functions */
     public fun triggerOp(player: Player, interaction: InteractionLoc) {
