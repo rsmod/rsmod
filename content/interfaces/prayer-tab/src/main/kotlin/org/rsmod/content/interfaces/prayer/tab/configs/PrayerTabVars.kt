2205ec5f4414613a8abbf34a6d102fe152760d0d
Tomm0017 <rspsmods@gmail.com>
2025-02-24 14:58:53 -0500

    Update varbit type identity hashes

--- content/interfaces/prayer-tab/src/main/kotlin/org/rsmod/content/interfaces/prayer/tab/configs/PrayerTabVars.kt
+++ content/interfaces/prayer-tab/src/main/kotlin/org/rsmod/content/interfaces/prayer/tab/configs/PrayerTabVars.kt
@@ -5,13 +5,14 @@ import org.rsmod.api.type.refs.varbit.VarBitReferences
 internal typealias prayer_varbits = PrayerTabVarBits
 
 object PrayerTabVarBits : VarBitReferences() {
-    val filter_show_lower_tiers = find("prayer_filter_show_lower_tiers", 1492222260)
-    val filter_show_tiered_prayers = find("prayer_filter_show_tiered_prayers", 1492453023)
-    val filter_show_rapid_healing = find("prayer_filter_show_rapid_healing", 1492683786)
-    val filter_show_prayers_fail_lvl = find("prayer_filter_show_prayers_without_level", 1492914549)
+    val filter_show_lower_tiers = find("prayer_filter_show_lower_tiers", 153119424717634)
+    val filter_show_tiered_prayers = find("prayer_filter_show_tiered_prayers", 153119424721417)
+    val filter_show_rapid_healing = find("prayer_filter_show_rapid_healing", 153119424725200)
+    val filter_show_prayers_fail_lvl =
+        find("prayer_filter_show_prayers_without_level", 153119424728983)
     val filter_show_prayers_fail_req =
-        find("prayer_filter_show_prayers_without_requirements", 1493145312)
-    val enabled_prayers = find("prayer_enabled_full", 930850789)
-    val selected_quick_prayers = find("quickprayer_enabled_full", 931077770)
-    val using_quick_prayers = find("using_quick_prayers", 931303043)
+        find("prayer_filter_show_prayers_without_requirements", 153119424732766)
+    val enabled_prayers = find("prayer_enabled_full", 4277063138774)
+    val selected_quick_prayers = find("quickprayer_enabled_full", 4328583513136)
+    val using_quick_prayers = find("using_quick_prayers", 19321012450480)
 }
