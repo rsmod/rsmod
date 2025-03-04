0f940604b691d5bf5a2fffafa6ca5894a423d40f
Tomm0017 <rspsmods@gmail.com>
2025-03-04 08:22:06 -0500

    Change `Hide` and `Spawn` to `KeyedEvent`s
    `onNpcSpawn` scripts will require a key, so `Spawn` should not be
    an `UnboundEvent`.
    
    As for `Hide`, its use case is uncertain. If no valid need for
    `onNpcHide` scripts arises, we may remove it.

--- engine/game/src/main/kotlin/org/rsmod/game/entity/npc/NpcStateEvents.kt
+++ engine/game/src/main/kotlin/org/rsmod/game/entity/npc/NpcStateEvents.kt
@@ -1,16 +1,21 @@
 package org.rsmod.game.entity.npc
 
+import org.rsmod.events.KeyedEvent
 import org.rsmod.events.UnboundEvent
 import org.rsmod.game.entity.Npc
 
 public class NpcStateEvents {
     public data class Create(val npc: Npc) : UnboundEvent
 
-    public data class Spawn(val npc: Npc) : UnboundEvent
-
     public data class Delete(val npc: Npc) : UnboundEvent
 
-    public data class Hide(val npc: Npc) : UnboundEvent
-
     public data class Reveal(val npc: Npc) : UnboundEvent
+
+    public data class Hide(val npc: Npc) : KeyedEvent {
+        override val id: Long = npc.id.toLong()
+    }
+
+    public data class Spawn(val npc: Npc) : KeyedEvent {
+        override val id: Long = npc.id.toLong()
+    }
 }
