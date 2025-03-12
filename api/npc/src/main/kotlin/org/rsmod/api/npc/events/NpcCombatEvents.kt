031b7d1c3ba455ddfba503089069684516463f82
Tomm0017 <rspsmods@gmail.com>
2025-03-12 08:31:28 -0400

    Change `NpcHitEvent` keys to `Npc.visType.id`
    Improves flexibility by using the current npc transmog of the npc being
    hit. This means `onModifyNpcHit` scripts should bind to the visual
    npc type instead of the base npc type.

--- api/npc/src/main/kotlin/org/rsmod/api/npc/events/NpcCombatEvents.kt
+++ api/npc/src/main/kotlin/org/rsmod/api/npc/events/NpcCombatEvents.kt
@@ -9,12 +9,12 @@ public class NpcHitEvents {
     public data class Modify(
         public val npc: Npc,
         public val hit: HitBuilder,
-        override val id: Long = npc.id.toLong(),
+        override val id: Long = npc.visType.id.toLong(),
     ) : KeyedEvent
 
     public data class Impact(
         public val npc: Npc,
         public val hit: Hit,
-        override val id: Long = npc.id.toLong(),
+        override val id: Long = npc.visType.id.toLong(),
     ) : KeyedEvent
 }
