6c0f79b716c0030bb4db91972892d434f3e69b46
Tomm0017 <rspsmods@gmail.com>
2025-03-15 16:09:58 -0400

    Add documentation for `getMeleeHitChance`

--- api/combat/combat-formulas/src/main/kotlin/org/rsmod/api/combat/formulas/AccuracyFormulae.kt
+++ api/combat/combat-formulas/src/main/kotlin/org/rsmod/api/combat/formulas/AccuracyFormulae.kt
@@ -23,6 +23,21 @@ public class AccuracyFormulae @Inject constructor(private val pvNMeleeAccuracy: 
         return pvNMeleeAccuracy.isSuccessfulHit(hitChance, random)
     }
 
+    /**
+     * Calculates the melee hit chance based on the [player]'s attack roll and the [target]'s
+     * defence roll.
+     *
+     * @param attackType The [MeleeAttackType] used for the attack roll, typically based on the
+     *   [player]'s current combat stance.
+     * @param attackStyle The [MeleeAttackStyle] used for the attack roll, usually derived from the
+     *   [player]'s current stance.
+     * @param blockType The [MeleeAttackType] used for the defense roll. In most cases, this matches
+     *   [attackType], but certain special attacks may use a different type. For example, the Dragon
+     *   Longsword special attack applies the player's current attack type for the attack roll but
+     *   always uses `Slash` for the defense roll.
+     * @return An integer between `0` and `10,000`, where `0` represents a `0%` hit chance, `1`
+     *   represents a `0.01%` hit chance, and `10,000` represents a `100%` hit chance.
+     */
     public fun getMeleeHitChance(
         player: Player,
         target: Npc,
