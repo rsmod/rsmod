34863b105706e16d79b7ba360aac1dd051a0b789
Tomm0017 <rspsmods@gmail.com>
2025-03-16 17:39:15 -0400

    Add documentation for `rollMeleeAccuracy`

--- api/combat/combat-formulas/src/main/kotlin/org/rsmod/api/combat/formulas/AccuracyFormulae.kt
+++ api/combat/combat-formulas/src/main/kotlin/org/rsmod/api/combat/formulas/AccuracyFormulae.kt
@@ -15,6 +15,26 @@ constructor(
     private val nvpMeleeAccuracy: NvPMeleeAccuracy,
     private val pvnMeleeAccuracy: PvNMeleeAccuracy,
 ) {
+    /**
+     * Rolls for melee accuracy to determine if an attack from a [player] against a [target] npc is
+     * successful.
+     *
+     * This function calculates the hit chance based on the player's attack roll and the npc's
+     * defence roll, then uses a value from the random number generator ([random]) to determine if
+     * the attack hits.
+     *
+     * @param attackType The [MeleeAttackType] used for the attack roll, usually derived from the
+     *   player's current stance.
+     * @param attackStyle The [MeleeAttackStyle] used for the attack roll, usually derived from the
+     *   player's current stance.
+     * @param blockType The [MeleeAttackType] used for the defense roll. In most cases, this matches
+     *   [attackType], but certain special attacks may use a different type.
+     * @param specMultiplier A multiplier applied to the hit chance, typically used for special
+     *   attacks.
+     * @param random A [GameRandom] instance used to generate a random number for the hit roll.
+     * @return `true` if the attack is successful (i.e., the hit chance exceeds the random roll),
+     *   `false` otherwise.
+     */
     public fun rollMeleeAccuracy(
         player: Player,
         target: Npc,
@@ -33,14 +53,16 @@ constructor(
      * Calculates the melee hit chance based on the [player]'s attack roll and the [target]'s
      * defence roll.
      *
-     * @param attackType The [MeleeAttackType] used for the attack roll, typically based on the
-     *   [player]'s current combat stance.
+     * @param attackType The [MeleeAttackType] used for the attack roll, usually derived from the
+     *   [player]'s current stance.
      * @param attackStyle The [MeleeAttackStyle] used for the attack roll, usually derived from the
      *   [player]'s current stance.
      * @param blockType The [MeleeAttackType] used for the defense roll. In most cases, this matches
      *   [attackType], but certain special attacks may use a different type. For example, the Dragon
      *   Longsword special attack applies the player's current attack type for the attack roll but
      *   always uses `Slash` for the defense roll.
+     * @param specMultiplier A multiplier applied to the hit chance, typically used for special
+     *   attacks.
      * @return An integer between `0` and `10,000`, where `0` represents a `0%` hit chance, `1`
      *   represents a `0.01%` hit chance, and `10,000` represents a `100%` hit chance.
      */
@@ -61,6 +83,20 @@ constructor(
             specMultiplier,
         )
 
+    /**
+     * Rolls for melee accuracy to determine if an attack from an [npc] against a [target] player is
+     * successful.
+     *
+     * This function calculates the hit chance based on the npc's attack roll and the player's
+     * defence roll, then uses a value from the random number generator ([random]) to determine if
+     * the attack hits.
+     *
+     * @param attackType The [MeleeAttackType] used for the attack roll, typically based on the
+     *   npc's current type of attack.
+     * @param random A [GameRandom] instance used to generate a random number for the hit roll.
+     * @return `true` if the attack is successful (i.e., the hit chance exceeds the random roll),
+     *   `false` otherwise.
+     */
     public fun rollMeleeAccuracy(
         npc: Npc,
         target: Player,
