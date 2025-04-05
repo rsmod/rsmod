c7a67b0d7f92347889f1cb73d840a29c9234cd06
Tomm0017 <rspsmods@gmail.com>
2025-04-05 17:27:28 -0400

    Bind missing manager instances

--- api/combat/combat-manager/src/main/kotlin/org/rsmod/api/combat/manager/CombatManagerModule.kt
+++ api/combat/combat-manager/src/main/kotlin/org/rsmod/api/combat/manager/CombatManagerModule.kt
@@ -4,6 +4,8 @@ import org.rsmod.plugin.module.PluginModule
 
 public class CombatManagerModule : PluginModule() {
     override fun bind() {
+        bindInstance<CombatChargeManager>()
+        bindInstance<MagicRuneManager>()
         bindInstance<PlayerAttackManager>()
         bindInstance<RangedAmmoManager>()
     }
