d0aed9f8b49127f8e1ae884a0eb1f5b5bcc61960
Tomm0017 <rspsmods@gmail.com>
2025-04-06 04:44:40 -0400

    Remove default `onAp_T` scripts for inv component

--- api/inv-plugin/src/main/kotlin/org/rsmod/api/inv/LocUOpScript.kt
+++ api/inv-plugin/src/main/kotlin/org/rsmod/api/inv/LocUOpScript.kt
@@ -6,7 +6,6 @@ import org.rsmod.api.player.events.interact.LocTDefaultEvents
 import org.rsmod.api.player.interact.LocUInteractions
 import org.rsmod.api.player.output.UpdateInventory.resendSlot
 import org.rsmod.api.player.protect.ProtectedAccess
-import org.rsmod.api.script.onApLocT
 import org.rsmod.api.script.onOpLocT
 import org.rsmod.game.type.obj.ObjTypeList
 import org.rsmod.plugin.scripts.PluginScript
@@ -17,13 +16,7 @@ public class LocUOpScript
 private constructor(private val interactions: LocUInteractions, private val objTypes: ObjTypeList) :
     PluginScript() {
     override fun ScriptContext.startUp() {
-        onApLocT(components.inv_inv) { apLocU(it) }
         onOpLocT(components.inv_inv) { opLocU(it) }
-    }
-
-    private suspend fun ProtectedAccess.apLocU(ap: LocTDefaultEvents.Ap) {
-        val objType = ap.objType?.let(objTypes::get) ?: return resendSlot(inv, 0)
-        interactions.interactAp(this, ap.loc, ap.base, ap.type, objType, inv, ap.comsub)
     }
 
     private suspend fun ProtectedAccess.opLocU(op: LocTDefaultEvents.Op) {
--- api/inv-plugin/src/main/kotlin/org/rsmod/api/inv/NpcUOpScript.kt
+++ api/inv-plugin/src/main/kotlin/org/rsmod/api/inv/NpcUOpScript.kt
@@ -6,7 +6,6 @@ import org.rsmod.api.player.events.interact.NpcTDefaultEvents
 import org.rsmod.api.player.interact.NpcUInteractions
 import org.rsmod.api.player.output.UpdateInventory.resendSlot
 import org.rsmod.api.player.protect.ProtectedAccess
-import org.rsmod.api.script.onApNpcT
 import org.rsmod.api.script.onOpNpcT
 import org.rsmod.game.type.obj.ObjTypeList
 import org.rsmod.plugin.scripts.PluginScript
@@ -17,13 +16,7 @@ public class NpcUOpScript
 private constructor(private val interactions: NpcUInteractions, private val objTypes: ObjTypeList) :
     PluginScript() {
     override fun ScriptContext.startUp() {
-        onApNpcT(components.inv_inv) { apNpcU(it) }
         onOpNpcT(components.inv_inv) { opNpcU(it) }
-    }
-
-    private suspend fun ProtectedAccess.apNpcU(ap: NpcTDefaultEvents.Ap) {
-        val objType = ap.objType?.let(objTypes::get) ?: return resendSlot(inv, 0)
-        interactions.interactAp(this, ap.npc, inv, ap.comsub, objType)
     }
 
     private suspend fun ProtectedAccess.opNpcU(op: NpcTDefaultEvents.Op) {
