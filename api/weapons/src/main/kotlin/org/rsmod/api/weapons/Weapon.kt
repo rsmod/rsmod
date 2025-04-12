19c110d5b340e8c18145c117413c4e1cb76da4b7
Tomm0017 <rspsmods@gmail.com>
2025-04-12 15:23:25 -0400

    Add `Weapon.Attack` kdoc

--- api/weapons/src/main/kotlin/org/rsmod/api/weapons/Weapon.kt
+++ api/weapons/src/main/kotlin/org/rsmod/api/weapons/Weapon.kt
@@ -6,8 +6,30 @@ import org.rsmod.game.entity.Npc
 import org.rsmod.game.entity.Player
 
 public interface Weapon<T : CombatAttack> {
+    /**
+     * Executes this weapon's attack against an [Npc] target.
+     *
+     * **Note:** If you intend to cancel the combat interaction, you **must** do so explicitly -
+     * call [WeaponAttackManager.stopCombat]. Likewise, to continue the interaction, call
+     * [WeaponAttackManager.continueCombat].
+     *
+     * @return `true` if this weapon handled the attack and the default combat flow should stop;
+     *   `false` to resume the previously interrupted default combat attack. This should usually
+     *   return `true`.
+     */
     public suspend fun ProtectedAccess.attack(target: Npc, attack: T): Boolean
 
+    /**
+     * Executes this weapon's attack against a [Player] target.
+     *
+     * **Note:** If you intend to cancel the combat interaction, you **must** do so explicitly -
+     * call [WeaponAttackManager.stopCombat]. Likewise, to continue the interaction, call
+     * [WeaponAttackManager.continueCombat].
+     *
+     * @return `true` if this weapon handled the attack and the default combat flow should stop;
+     *   `false` to resume the previously interrupted default combat attack. This should usually
+     *   return `true`.
+     */
     public suspend fun ProtectedAccess.attack(target: Player, attack: T): Boolean
 }
 
