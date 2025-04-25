1295907db1a98f864be5301d0038db5d9d908243
Tomm0017 <rspsmods@gmail.com>
2025-04-25 06:31:39 -0400

    Set special attack energy for new accounts

--- api/specials/src/main/kotlin/org/rsmod/api/specials/scripts/SpecialEnergyScript.kt
+++ api/specials/src/main/kotlin/org/rsmod/api/specials/scripts/SpecialEnergyScript.kt
@@ -3,7 +3,9 @@ package org.rsmod.api.specials.scripts
 import kotlin.math.min
 import org.rsmod.api.config.constants
 import org.rsmod.api.config.refs.timers
+import org.rsmod.api.config.refs.varbits
 import org.rsmod.api.config.refs.varps
+import org.rsmod.api.player.vars.boolVarBit
 import org.rsmod.api.player.vars.intVarp
 import org.rsmod.api.script.onPlayerLogIn
 import org.rsmod.api.script.onPlayerSoftTimer
@@ -11,7 +13,8 @@ import org.rsmod.game.entity.Player
 import org.rsmod.plugin.scripts.PluginScript
 import org.rsmod.plugin.scripts.ScriptContext
 
-public class EnergyRegenScript : PluginScript() {
+public class SpecialEnergyScript : PluginScript() {
+    private val Player.newAccount by boolVarBit(varbits.new_player_account)
     private var Player.specialAttackEnergy by intVarp(varps.sa_energy)
 
     override fun ScriptContext.startUp() {
@@ -20,6 +23,9 @@ public class EnergyRegenScript : PluginScript() {
     }
 
     private fun Player.initRegenTimer() {
+        if (newAccount) {
+            specialAttackEnergy = constants.sa_max_energy
+        }
         softTimer(timers.spec_regen, constants.spec_regen_interval)
     }
 
