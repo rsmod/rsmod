614a8e2906d6ce9c6266091bb614395f71700bf5
Tomm0017 <rspsmods@gmail.com>
2025-05-18 11:34:26 -0400

    Add `ResumePauseButtonInput` helper functions

--- api/player/src/main/kotlin/org/rsmod/api/player/input/ResumePauseButtonInput.kt
+++ api/player/src/main/kotlin/org/rsmod/api/player/input/ResumePauseButtonInput.kt
@@ -5,4 +5,8 @@ import org.rsmod.game.type.comp.ComponentType
 public data class ResumePauseButtonInput(
     public val component: ComponentType,
     public val subcomponent: Int,
-)
+) {
+    public fun isComponentType(type: ComponentType): Boolean = type.isType(component)
+
+    public fun isSubcomponent(comsub: Int): Boolean = subcomponent == comsub
+}
