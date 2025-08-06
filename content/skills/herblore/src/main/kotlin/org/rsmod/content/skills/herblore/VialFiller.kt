7080610395da06d2b774014c9776bb512b7325f4
CombatBorn <Minecraftisreality@gmail.com>
2025-08-05 20:36:08 -0400

    started herblore skill

---
+++ content/skills/herblore/src/main/kotlin/org/rsmod/content/skills/herblore/VialFiller.kt
@@ -0,0 +1,15 @@
+package org.rsmod.content.skills.herblore
+
+import jakarta.inject.Inject
+import org.rsmod.api.script.onOpHeld1
+import org.rsmod.plugin.scripts.PluginScript
+import org.rsmod.plugin.scripts.ScriptContext
+
+class VialFiller
+@Inject
+constructor() : PluginScript() {
+
+    override fun ScriptContext.startup() {
+    }
+
+}
